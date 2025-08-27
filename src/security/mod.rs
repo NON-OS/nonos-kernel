0cd5642498e6680ebe22744b7094b7dfe074e5e1
ekisanon-anyone <ekisanon@proton.me>
2025-08-27 10:43:06 -0400

    Create mod.rs

---
+++ src/security/mod.rs
@@ -0,0 +1,21 @@
+// This file is part of the NONOS Operating Systems Kernel.
+// 
+//  Copyright (C) [2025] [NONOS]
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+
+pub mod capability;
+pub mod nonos_capability;
+pub mod security;
+
+pub use capability::*;
+pub use nonos_capability::*;
+pub use security::*;
