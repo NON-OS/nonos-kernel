f105a45169f81804e9985ba475562120af003ea3
ekisanon-anyone <ekisanon@proton.me>
2025-08-27 10:29:52 -0400

    License Identifier system_monitor.rs

--- src/system_monitor.rs
+++ src/system_monitor.rs
@@ -1,3 +1,17 @@
+// This file is part of the NONOS Operating Systems Kernel.
+// 
+//  Copyright (C) [2025] [NONOS]
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+
 //! System Health Monitoring
 //! 
 //! Simple, reliable system monitoring 
