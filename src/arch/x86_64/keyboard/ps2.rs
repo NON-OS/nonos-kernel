b91fd9adce8c81864cfbcfad68c1a6123b698675
ekisanon-anyone <ekisanon@proton.me>
2025-10-09 15:52:51 -0400

    Add PS/2 keyboard controller support

---
+++ src/arch/x86_64/keyboard/ps2.rs
@@ -0,0 +1,9 @@
+//! PS/2 Keyboard Controller Support
+
+pub fn init_ps2() {
+    // TODO: Initialize PS/2 controller, enable IRQ
+}
+
+pub fn handle_ps2_interrupt() {
+    // TODO: Read scan code from 0x60, map to input event
+}
