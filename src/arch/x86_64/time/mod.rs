bd99976a8429a1c243d7a7ef8ea1ad368dae2f01
ekisanon-anyone <ekisanon@proton.me>
2025-10-09 16:15:32 -0400

    Add time module with timer submodules

---
+++ src/arch/x86_64/time/mod.rs
@@ -0,0 +1,8 @@
+pub mod nonos_timer;
+pub mod tsc;
+pub mod hpet;
+pub mod pit;
+pub mod rtc;
+// pub mod test; // Uncomment until timer unit tests
+
+// Shared constants, types, or traits for the time subsystem can be defined here.
