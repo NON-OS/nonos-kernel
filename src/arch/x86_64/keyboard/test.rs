c2e05b71ccc510dab3d7020a4fcfcbe671228ace
ekisanon-anyone <ekisanon@proton.me>
2025-10-13 12:35:17 -0400

    Update test.rs
    Added unit tests for AZERTY and Colemak keyboard layouts.

--- src/arch/x86_64/keyboard/test.rs
+++ src/arch/x86_64/keyboard/test.rs
@@ -2,13 +2,15 @@
 
 #[cfg(test)]
 mod tests {
-    use super::super::layouts::{get_ascii_mapping, Layout};
+    use super::super::layout::{get_ascii_mapping, Layout};
 
     #[test]
     fn test_us_qwerty_mapping() {
         let map = get_ascii_mapping(Layout::UsQwerty);
         assert_eq!(map[2], b'1');
         assert_eq!(map[16], b'q');
+        assert_eq!(map[30], b'a');
+        assert_eq!(map[57], b' ');
     }
 
     #[test]
@@ -16,7 +18,22 @@ mod tests {
         let map = get_ascii_mapping(Layout::Dvorak);
         assert_eq!(map[16], b'\'');
         assert_eq!(map[30], b'a');
+        assert_eq!(map[18], b',');
     }
 
-    // Add more tests later for AZERTY, Colemak, etc.
+    #[test]
+    fn test_azerty_mapping() {
+        let map = get_ascii_mapping(Layout::Azerty);
+        assert_eq!(map[2], b'&');
+        assert_eq!(map[16], b'a');
+        assert_eq!(map[44], b'!');
+    }
+
+    #[test]
+    fn test_colemak_mapping() {
+        let map = get_ascii_mapping(Layout::Colemak);
+        assert_eq!(map[2], b'1');
+        assert_eq!(map[30], b'a');
+        assert_eq!(map[18], b'f');
+    }
 }
