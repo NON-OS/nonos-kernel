c4bf2b7f3da53966577318ae5fa9601d39776d2e
A. Erik <ekisanon@proton.me>
2026-01-06 17:30:19 +0100

    Create mod.rs

---
+++ src/memory/secure_memory/mod.rs
@@ -0,0 +1,13 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+extern crate alloc;
+pub mod constants;
+pub mod error;
+pub mod manager;
+pub mod types;
+#[cfg(test)]
+mod tests;
+pub use constants::*;
+pub use error::{SecureMemoryError, SecureMemoryResult};
+pub use manager::*;
+pub use types::*;
