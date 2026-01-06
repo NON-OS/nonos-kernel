76b8985e211e51f18aa6f5f15bbfa1229428b7a9
A. Erik <ekisanon@proton.me>
2026-01-06 15:37:43 +0100

    mod.rs for paging module
    page table management and address translation.

---
+++ src/memory/paging/mod.rs
@@ -0,0 +1,30 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//! Page table management and address translation.
+
+extern crate alloc;
+pub mod constants;
+pub mod error;
+pub mod manager;
+mod stats;
+pub mod tlb;
+pub mod types;
+#[cfg(test)]
+mod tests;
+pub use constants::*;
+pub use error::{PageFaultInfo, PagingError, PagingResult};
+pub use manager::*;
+pub use types::{PageMapping, PagePermissions, PageSize, PagingStats};
