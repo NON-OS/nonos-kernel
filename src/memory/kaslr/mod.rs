34a0043e7b922ce4bccdde08c7932b482fa7fc19
A. Erik <ekisanon@proton.me>
2026-01-06 13:11:01 +0100

    Create mod.rs

---
+++ src/memory/kaslr/mod.rs
@@ -0,0 +1,14 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+
+extern crate alloc;
+pub mod constants;
+pub mod error;
+pub mod manager;
+pub mod types;
+#[cfg(test)]
+mod tests;
+pub use constants::*;
+pub use error::{KaslrError, KaslrResult};
+pub use manager::*;
+pub use types::*;
