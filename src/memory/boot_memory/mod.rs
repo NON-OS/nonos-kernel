3860774ffddb12fda82479adc9717f8d67136ad3
A. Erik <ekisanon@proton.me>
2026-01-06 11:05:44 +0100

    boot memory management module
    NØNOS Operating System
    Copyright (C) 2026 NØNOS Contributors

---
+++ src/memory/boot_memory/mod.rs
@@ -0,0 +1,31 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//! Early boot-time physical memory allocator.
+
+extern crate alloc;
+
+pub mod constants;
+pub mod error;
+pub mod manager;
+pub mod types;
+
+#[cfg(test)]
+mod tests;
+
+pub use constants::*;
+pub use error::{BootMemoryError, BootMemoryResult};
+pub use manager::*;
+pub use types::*;
