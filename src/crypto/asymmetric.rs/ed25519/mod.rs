ba6310f08fbbe0e3ffd08b6e8246912040dad55c
A. Erik <ekisanon@proton.me>
2026-01-09 11:31:17 +0100

    Ed25519 module with public API exports
    #![allow(non_snake_case)]
    Ed25519 uses math notation where uppercase = points (A, B, R, S), lowercase = scalars (a, k, r). Following RFC 8032 naming makes code match the spec and easier to audit
    
    #![allow(clippy::many_single_char_names)]
    Crypto formulas use single chars: x, y, u, v, k, r, s
    fe_mul(a, b) is clearer than fe_mul(field_element_left, field_element_right) to when interacting with a known formula

---
+++ src/crypto/asymmetric.rs/ed25519/mod.rs
@@ -0,0 +1,28 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#![allow(non_snake_case)]
+#![allow(clippy::many_single_char_names)]
+
+mod field;
+mod point;
+mod scalar;
+mod signature;
+
+#[cfg(test)]
+mod tests;
+
+pub use signature::{sign, verify, verify_batch, KeyPair, Signature};
