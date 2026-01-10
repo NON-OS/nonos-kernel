781e57e0f35b83f14607a88a34ae20ffcb044048
A. Erik <ekisanon@proton.me>
2026-01-10 12:45:14 +0100

    Add new modules

--- src/elf/mod.rs
+++ src/elf/mod.rs
@@ -15,15 +15,25 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 pub mod aslr;
+pub mod auxv;
+pub mod cache;
 pub mod dynlink;
+pub mod embedded;
 pub mod errors;
+pub mod fini;
+pub mod got;
+pub mod hash;
+pub mod init;
 pub mod interpreter;
+pub mod libmgr;
 pub mod loader;
 pub mod minimal;
+pub mod process;
 pub mod reloc;
+pub mod stack;
+pub mod symbol;
 pub mod tls;
 pub mod types;
-
 pub use types::{
     DynamicEntry, ElfHeader, ProgramHeader, RelaEntry, SectionHeader, Symbol,
 };
@@ -34,15 +44,25 @@ pub use types::{
 };
 
 pub use errors::{ElfError, ElfResult};
-
 pub use loader::{
     get_elf_loader, init_elf_loader, is_initialized, load_elf_executable,
     DynamicInfo, ElfImage, ElfLoader, LoadedSegment,
 };
 
 pub use aslr::AslrManager;
+pub use auxv::{aux_type, AuxEntry, AuxvBuilder};
+pub use cache::{CacheEntryState, CachedImage, ImageCache};
 pub use dynlink::DynLinkInfo;
+pub use embedded::{EmbeddedLibrary, EmbeddedLibraryLoader, EmbeddedLibraryRegistry, LibraryVersion};
+pub use fini::{FiniArrayInfo, FiniArrayRunner, FiniFn};
+pub use got::{GlobalOffsetTable, GotEntry, RelocationProcessor};
+pub use hash::{gnu_hash, sysv_hash, DualHashLookup, GnuHashTable, HashTable, SysvHashTable};
+pub use init::{InitArrayInfo, InitArrayRunner, InitFn, PreInitArrayInfo};
 pub use interpreter::InterpreterInfo;
+pub use libmgr::{LibraryManager, LibraryState, LinkMap, LoadedLibrary};
+pub use process::{create_process, create_process_with_args, ProcessBuilder, ProcessConfig, ProcessImage};
 pub use reloc::process_relocations;
+pub use stack::{setup_user_stack, StackConfig, StackLayout};
+pub use symbol::{ResolvedSymbol, SymbolLookup, SymbolResolver};
 pub use tls::TlsInfo;
 pub use minimal::{entry_from_bytes, validate_elf, validate_elf_detailed};
