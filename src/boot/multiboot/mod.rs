b9d02fccd09b2645efc9e07d0c6a8494160499b6
A. Erik <ekisanon@proton.me>
2026-01-12 08:56:01 +0100

    Multiboot2 protocol support mod

---
+++ src/boot/multiboot/mod.rs
@@ -0,0 +1,30 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod parser;
+mod platform;
+mod types;
+#[cfg(test)]
+mod tests;
+
+pub use parser::parse_multiboot_info;
+pub use platform::{detect_platform, get_safe_memory_regions, init_platform_features};
+pub use platform::{ConsoleType, Platform};
+pub use types::memory_type;
+pub use types::MultibootError;
+pub use types::{
+    FramebufferInfo, MemoryMapEntry, ModuleInfo, Multiboot2Header, Multiboot2Info, MultibootInfo,
+};
