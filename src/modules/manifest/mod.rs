6643c720eaf0fde10a18fcd5ea45c5a1a2e7a056
A. Erik <ekisanon@proton.me>
2026-01-12 15:45:27 +0100

    initial manifest module

---
+++ src/modules/manifest/mod.rs
@@ -0,0 +1,29 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+
+pub mod constants;
+pub mod error;
+pub mod types;
+pub mod builder;
+#[cfg(test)]
+mod tests;
+
+pub use constants::*;
+pub use error::{ManifestError, ManifestResult};
+pub use types::{ModuleManifest, ModuleType, PrivacyPolicy, MemoryRequirements, AttestationEntry};
+pub use builder::ManifestBuilder;
