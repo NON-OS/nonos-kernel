205de3219736cf30549a1fa87666974b6b9a952d
A. Erik <ekisanon@proton.me>
2026-01-13 09:57:29 +0100

    Utility functions for zero-knowledge proofs.

---
+++ src/crypto/zk_kernel/utils.rs
@@ -0,0 +1,36 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use core::ptr;
+use crate::crypto::util::constant_time::{compiler_fence, memory_fence};
+
+#[inline]
+pub fn zeroize(buf: &mut [u8]) {
+    for b in buf {
+        unsafe { ptr::write_volatile(b, 0) };
+    }
+    compiler_fence();
+    memory_fence();
+}
+
+#[inline]
+pub fn constant_time_eq(a: &[u8; 32], b: &[u8; 32]) -> bool {
+    let mut diff = 0u8;
+    for i in 0..32 {
+        diff |= a[i] ^ b[i];
+    }
+    diff == 0
+}
