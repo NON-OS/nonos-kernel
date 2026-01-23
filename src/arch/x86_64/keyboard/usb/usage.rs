68edd492fdc10226f0b24ed105ec662eae3684ce
A. Erik <ekisanon@proton.me>
2026-01-23 17:38:24 +0100

    USB keyboard usage constants and conversion

---
+++ src/arch/x86_64/keyboard/usb/usage.rs
@@ -0,0 +1,195 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub const KEY_A: u8 = 0x04;
+pub const KEY_B: u8 = 0x05;
+pub const KEY_C: u8 = 0x06;
+pub const KEY_D: u8 = 0x07;
+pub const KEY_E: u8 = 0x08;
+pub const KEY_F: u8 = 0x09;
+pub const KEY_G: u8 = 0x0A;
+pub const KEY_H: u8 = 0x0B;
+pub const KEY_I: u8 = 0x0C;
+pub const KEY_J: u8 = 0x0D;
+pub const KEY_K: u8 = 0x0E;
+pub const KEY_L: u8 = 0x0F;
+pub const KEY_M: u8 = 0x10;
+pub const KEY_N: u8 = 0x11;
+pub const KEY_O: u8 = 0x12;
+pub const KEY_P: u8 = 0x13;
+pub const KEY_Q: u8 = 0x14;
+pub const KEY_R: u8 = 0x15;
+pub const KEY_S: u8 = 0x16;
+pub const KEY_T: u8 = 0x17;
+pub const KEY_U: u8 = 0x18;
+pub const KEY_V: u8 = 0x19;
+pub const KEY_W: u8 = 0x1A;
+pub const KEY_X: u8 = 0x1B;
+pub const KEY_Y: u8 = 0x1C;
+pub const KEY_Z: u8 = 0x1D;
+
+pub const KEY_1: u8 = 0x1E;
+pub const KEY_2: u8 = 0x1F;
+pub const KEY_3: u8 = 0x20;
+pub const KEY_4: u8 = 0x21;
+pub const KEY_5: u8 = 0x22;
+pub const KEY_6: u8 = 0x23;
+pub const KEY_7: u8 = 0x24;
+pub const KEY_8: u8 = 0x25;
+pub const KEY_9: u8 = 0x26;
+pub const KEY_0: u8 = 0x27;
+
+pub const KEY_ENTER: u8 = 0x28;
+pub const KEY_ESCAPE: u8 = 0x29;
+pub const KEY_BACKSPACE: u8 = 0x2A;
+pub const KEY_TAB: u8 = 0x2B;
+pub const KEY_SPACE: u8 = 0x2C;
+pub const KEY_MINUS: u8 = 0x2D;
+pub const KEY_EQUALS: u8 = 0x2E;
+pub const KEY_LEFT_BRACKET: u8 = 0x2F;
+pub const KEY_RIGHT_BRACKET: u8 = 0x30;
+pub const KEY_BACKSLASH: u8 = 0x31;
+pub const KEY_HASH: u8 = 0x32;
+pub const KEY_SEMICOLON: u8 = 0x33;
+pub const KEY_APOSTROPHE: u8 = 0x34;
+pub const KEY_GRAVE: u8 = 0x35;
+pub const KEY_COMMA: u8 = 0x36;
+pub const KEY_PERIOD: u8 = 0x37;
+pub const KEY_SLASH: u8 = 0x38;
+pub const KEY_CAPS_LOCK: u8 = 0x39;
+
+pub const KEY_F1: u8 = 0x3A;
+pub const KEY_F2: u8 = 0x3B;
+pub const KEY_F3: u8 = 0x3C;
+pub const KEY_F4: u8 = 0x3D;
+pub const KEY_F5: u8 = 0x3E;
+pub const KEY_F6: u8 = 0x3F;
+pub const KEY_F7: u8 = 0x40;
+pub const KEY_F8: u8 = 0x41;
+pub const KEY_F9: u8 = 0x42;
+pub const KEY_F10: u8 = 0x43;
+pub const KEY_F11: u8 = 0x44;
+pub const KEY_F12: u8 = 0x45;
+
+pub const KEY_PRINT_SCREEN: u8 = 0x46;
+pub const KEY_SCROLL_LOCK: u8 = 0x47;
+pub const KEY_PAUSE: u8 = 0x48;
+pub const KEY_INSERT: u8 = 0x49;
+pub const KEY_HOME: u8 = 0x4A;
+pub const KEY_PAGE_UP: u8 = 0x4B;
+pub const KEY_DELETE: u8 = 0x4C;
+pub const KEY_END: u8 = 0x4D;
+pub const KEY_PAGE_DOWN: u8 = 0x4E;
+pub const KEY_RIGHT_ARROW: u8 = 0x4F;
+pub const KEY_LEFT_ARROW: u8 = 0x50;
+pub const KEY_DOWN_ARROW: u8 = 0x51;
+pub const KEY_UP_ARROW: u8 = 0x52;
+
+pub const KEY_NUM_LOCK: u8 = 0x53;
+pub const KEY_KP_DIVIDE: u8 = 0x54;
+pub const KEY_KP_MULTIPLY: u8 = 0x55;
+pub const KEY_KP_MINUS: u8 = 0x56;
+pub const KEY_KP_PLUS: u8 = 0x57;
+pub const KEY_KP_ENTER: u8 = 0x58;
+pub const KEY_KP_1: u8 = 0x59;
+pub const KEY_KP_2: u8 = 0x5A;
+pub const KEY_KP_3: u8 = 0x5B;
+pub const KEY_KP_4: u8 = 0x5C;
+pub const KEY_KP_5: u8 = 0x5D;
+pub const KEY_KP_6: u8 = 0x5E;
+pub const KEY_KP_7: u8 = 0x5F;
+pub const KEY_KP_8: u8 = 0x60;
+pub const KEY_KP_9: u8 = 0x61;
+pub const KEY_KP_0: u8 = 0x62;
+pub const KEY_KP_DECIMAL: u8 = 0x63;
+
+pub const KEY_LEFT_CTRL: u8 = 0xE0;
+pub const KEY_LEFT_SHIFT: u8 = 0xE1;
+pub const KEY_LEFT_ALT: u8 = 0xE2;
+pub const KEY_LEFT_GUI: u8 = 0xE3;
+pub const KEY_RIGHT_CTRL: u8 = 0xE4;
+pub const KEY_RIGHT_SHIFT: u8 = 0xE5;
+pub const KEY_RIGHT_ALT: u8 = 0xE6;
+pub const KEY_RIGHT_GUI: u8 = 0xE7;
+
+pub const ERR_ROLLOVER: u8 = 0x01;
+pub const ERR_POST_FAIL: u8 = 0x02;
+pub const ERR_UNDEFINED: u8 = 0x03;
+
+pub fn hid_to_scancode(hid_usage: u8) -> u8 {
+    match hid_usage {
+        0x04..=0x1D => {
+            const MAP: [u8; 26] = [
+                0x1E, 0x30, 0x2E, 0x20, 0x12, 0x21, 0x22, 0x23, 0x17, 0x24,
+                0x25, 0x26, 0x32, 0x31, 0x18, 0x19, 0x10, 0x13, 0x1F, 0x14,
+                0x16, 0x2F, 0x11, 0x2D, 0x15, 0x2C,
+            ];
+            MAP[(hid_usage - 0x04) as usize]
+        }
+        0x1E..=0x27 => {
+            const MAP: [u8; 10] = [0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B];
+            MAP[(hid_usage - 0x1E) as usize]
+        }
+        0x28 => 0x1C,
+        0x29 => 0x01,
+        0x2A => 0x0E,
+        0x2B => 0x0F,
+        0x2C => 0x39,
+        0x2D => 0x0C,
+        0x2E => 0x0D,
+        0x2F => 0x1A,
+        0x30 => 0x1B,
+        0x31 => 0x2B,
+        0x33 => 0x27,
+        0x34 => 0x28,
+        0x35 => 0x29,
+        0x36 => 0x33,
+        0x37 => 0x34,
+        0x38 => 0x35,
+        0x39 => 0x3A,
+        0x3A..=0x45 => 0x3B + (hid_usage - 0x3A),
+        0x49 => 0x52,
+        0x4A => 0x47,
+        0x4B => 0x49,
+        0x4C => 0x53,
+        0x4D => 0x4F,
+        0x4E => 0x51,
+        0x4F => 0x4D,
+        0x50 => 0x4B,
+        0x51 => 0x50,
+        0x52 => 0x48,
+        0x53 => 0x45,
+        0x54 => 0x35,
+        0x55 => 0x37,
+        0x56 => 0x4A,
+        0x57 => 0x4E,
+        0x58 => 0x1C,
+        0x59..=0x62 => {
+            const MAP: [u8; 10] = [0x4F, 0x50, 0x51, 0x4B, 0x4C, 0x4D, 0x47, 0x48, 0x49, 0x52];
+            MAP[(hid_usage - 0x59) as usize]
+        }
+        0x63 => 0x53,
+        0xE0 => 0x1D,
+        0xE1 => 0x2A,
+        0xE2 => 0x38,
+        0xE3 => 0x5B,
+        0xE4 => 0x1D,
+        0xE5 => 0x36,
+        0xE6 => 0x38,
+        0xE7 => 0x5C,
+        _ => 0,
+    }
+}
