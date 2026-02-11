a888791325b152479560a5d95585db90833ec472
A. Erik <ekisanon@proton.me>
2026-02-11 14:18:04 +0100

    kernel_verify module for kernel verification
    imports and public API.

---
+++ nonos-bootloader/src/kernel_verify/mod.rs
@@ -0,0 +1,25 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod display;
+mod types;
+mod verify;
+
+pub use types::{CryptoVerifyResult, MIN_KERNEL_SIZE, SIGNATURE_SIZE};
+
+pub use verify::verify_kernel_crypto;
+
+pub use display::{byte_to_hex, mini_delay, print, print_hex_bytes, print_hex_char};
