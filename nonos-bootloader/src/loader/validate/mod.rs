a12a6946e7fb234d8f6c825067dab1869ccd47ad
A. Erik <ekisanon@proton.me>
2026-02-13 17:21:49 +0100

    loader validation module

---
+++ nonos-bootloader/src/loader/validate/mod.rs
@@ -0,0 +1,25 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod context;
+mod header;
+mod program;
+pub mod validate;
+
+pub use validate::{
+    validate_elf, validate_elf_strict, validate_entry_point, validate_header, validate_ident,
+    validate_magic, validate_program_header, validate_program_headers, ValidationContext,
+};
