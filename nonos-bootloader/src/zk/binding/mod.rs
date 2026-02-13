e20839fcce38f3337685905a7ca2f6af59598774
A. Erik <ekisanon@proton.me>
2026-02-13 18:58:53 +0100

    zk binding module with exports

---
+++ nonos-bootloader/src/zk/binding/mod.rs
@@ -0,0 +1,23 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod commit;
+mod select;
+mod types;
+
+pub use commit::{compute_capsule_commitment, compute_commit, verify_commitment};
+pub use select::{is_manifest_binding_enabled, select_binding};
+pub use types::{BindingInput, DS_COMMITMENT, MAX_MANIFEST_SIZE};
