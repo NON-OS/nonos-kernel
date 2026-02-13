6eded30dac311d310c86bb033410da96c61e1b53
A. Erik <ekisanon@proton.me>
2026-02-13 17:08:38 +0100

    Add segment module to loader

---
+++ nonos-bootloader/src/loader/segment/mod.rs
@@ -0,0 +1,23 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub mod segment;
+
+pub use segment::{
+    calculate_memory_bounds, check_segment_overlaps, count_wx_violations, load_all_segments,
+    load_segment, total_file_size, total_memory_size, validate_segment_addresses, SegmentLoadInfo,
+    SegmentPermissions,
+};
