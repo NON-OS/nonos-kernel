3c2989e7939f0cc0423117781c2bbf3e9964908e
A. Erik <ekisanon@proton.me>
2026-02-23 13:34:46 +0100

    Add initial VFS module with necessary submodules

---
+++ src/fs/vfs/mod.rs
@@ -0,0 +1,25 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod error;
+mod types;
+mod fd_table;
+mod vfs_impl;
+
+pub use error::{VfsError, VfsResult};
+pub use types::*;
+pub use fd_table::*;
+pub use vfs_impl::*;
