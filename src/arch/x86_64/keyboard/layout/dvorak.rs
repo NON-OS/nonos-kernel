ce66dd08bc6d492ce0d7195bd13f10d5dadb7d34
A.Erik <ekisanon@proton.me>
2026-02-28 13:20:33 +0100

    dvorak keyboard layout

--- src/arch/x86_64/keyboard/layout/dvorak.rs
+++ src/arch/x86_64/keyboard/layout/dvorak.rs
@@ -1,8 +1,5 @@
-// **
-//Dr. Dvorak (Univ. of Washington, Seattle; he lived 1894–1975) used his research to design two other keyboards specifically for people with only one hand
-// **
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -19,7 +16,7 @@
 
 use super::types::{Layout, LayoutInfo};
 
-pub static BASE: [u8; 128] = [
+pub(super) static BASE: [u8; 128] = [
     0,    27,   b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'0', b'[', b']', 8,    b'\t',
     b'\'',b',', b'.', b'p', b'y', b'f', b'g', b'c', b'r', b'l', b'/', b'=', b'\n', 0,   b'a', b'o',
     b'e', b'u', b'i', b'd', b'h', b't', b'n', b's', b'-', b'`', 0,    b'\\',b';', b'q', b'j', b'k',
@@ -30,7 +27,7 @@ pub static BASE: [u8; 128] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ];
 
-pub static SHIFT: [u8; 128] = [
+pub(super) static SHIFT: [u8; 128] = [
     0,    27,   b'!', b'@', b'#', b'$', b'%', b'^', b'&', b'*', b'(', b')', b'{', b'}', 8,    b'\t',
     b'"', b'<', b'>', b'P', b'Y', b'F', b'G', b'C', b'R', b'L', b'?', b'+', b'\n', 0,   b'A', b'O',
     b'E', b'U', b'I', b'D', b'H', b'T', b'N', b'S', b'_', b'~', 0,    b'|', b':', b'Q', b'J', b'K',
@@ -43,4 +40,4 @@ pub static SHIFT: [u8; 128] = [
 
 static ALTGR: [u8; 128] = [0; 128];
 
-pub static LAYOUT_INFO: LayoutInfo = LayoutInfo::new(Layout::Dvorak, &BASE, &SHIFT, &ALTGR);
+pub(super) static LAYOUT_INFO: LayoutInfo = LayoutInfo::new(Layout::Dvorak, &BASE, &SHIFT, &ALTGR);
