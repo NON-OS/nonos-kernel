a6df6e5ccdce9663b4fbb898dc984acd30289223
A.Erik <ekisanon@proton.me>
2026-02-28 16:39:55 +0100

    boot/stage1: simplify BSS clearing safety documentation
    Condense safety comments to essential requirements.

--- src/boot/stage1/bss.rs
+++ src/boot/stage1/bss.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -13,25 +13,26 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-//
-/// # Safety {
-/// Must be called exactly once at boot before using any static variables
-/// Must be called before any Rust code that accesses static variables
-/// The linker symbols __bss_start and __bss_end must be properly defined
-/// }
-pub unsafe fn clear_bss() {
-    extern "C" {
-        static mut __bss_start: u8;
-        static mut __bss_end: u8;
-    }
-    // # SAFETY: These are linker-provided addresses, not actual values
-    let start = unsafe { &__bss_start as *const u8 as usize };
-    let end = unsafe { &__bss_end as *const u8 as usize };
-    let len = end.saturating_sub(start);
 
-    if len > 0 {
-        // # SAFETY: BSS section is valid writable memory, caller guarantees single call
-        unsafe {
+/// # Safety
+///
+/// Must be called once at boot before using static variables.
+pub unsafe fn clear_bss() {
+    // SAFETY: Linker-provided symbols for BSS section bounds
+    extern "C" {
+        static __bss_start: u8;
+        static __bss_end: u8;
+    }
+
+    // SAFETY: These are linker-provided addresses, not actual values
+    // Using addr_of! to avoid creating references to linker symbols
+    unsafe {
+        let start = core::ptr::addr_of!(__bss_start) as usize;
+        let end = core::ptr::addr_of!(__bss_end) as usize;
+        let len = end.saturating_sub(start);
+
+        if len > 0 {
+            // SAFETY: BSS section is valid writable memory, caller guarantees single call
             core::ptr::write_bytes(start as *mut u8, 0, len);
         }
     }
