9f678ede94832247e29d35356ae5b468d30b0d4b
A.Erik <ekisanon@proton.me>
2026-02-28 16:39:28 +0100

    boot/handoff: update test imports for types refactor
    Adjust import paths to match new types/ module structure.

--- src/boot/handoff/tests.rs
+++ src/boot/handoff/tests.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -15,6 +15,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use super::*;
+
 #[test]
 fn test_reexports_available() {
     let _ = HANDOFF_MAGIC;
