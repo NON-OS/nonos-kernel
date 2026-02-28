6d5f8b85f7e7b2cfaf45242b2762f248b70f1056
A.Erik <ekisanon@proton.me>
2026-02-28 13:20:02 +0100

    interrupt module exports for x86_64

--- src/arch/x86_64/interrupt/mod.rs
+++ src/arch/x86_64/interrupt/mod.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2024 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,32 +14,14 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! NØNOS x86_64 Interrupt Controllers
-
-pub mod nonos_apic;
-pub mod nonos_ioapic;
-pub mod nonos_pic_legacy;
-
-/// Prelude for ergonomic import of all interrupt controller APIs
-pub mod prelude {
-    pub use super::nonos_apic::*;
-    pub use super::nonos_ioapic::*;
-    pub use super::nonos_pic_legacy::*;
-}
-
+pub mod apic;
+pub mod ioapic;
+pub mod pic;
 #[cfg(test)]
-mod tests {
-    use super::prelude::*;
-    #[test]
-    fn test_apic_feature_detection() {
-        // Only tests logic, does not touch hardware.
-        assert!(has_xapic() || !has_xapic());
-        assert!(has_x2apic() || !has_x2apic());
-    }
-    #[test]
-    fn test_ioapic_vector_alloc() {
-        // Only tests vector allocator logic, not actual hardware.
-        let (_vec, rte) = alloc_route(5, 1).expect("vector alloc failed");
-        assert!(rte.masked);
-    }
-}
+mod test;
+
+pub use apic::{ApicError, ApicResult, ApicStatus};
+pub use ioapic::{IoApicError, IoApicResult, IoApicStatus};
+pub use pic::{PicError, PicResult, PicStatus};
+
+pub use ioapic as nonos_ioapic;
