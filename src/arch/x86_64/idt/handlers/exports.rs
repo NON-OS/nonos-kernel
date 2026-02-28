d0a2d3ba39d6af13616a2761eb8dcca2798a3209
A.Erik <ekisanon@proton.me>
2026-02-28 15:43:26 +0100

    add isr exports

---
+++ src/arch/x86_64/idt/handlers/exports.rs
@@ -0,0 +1,64 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub(crate) use super::isr::isr_divide_error as isr_0;
+pub(crate) use super::isr::isr_debug as isr_1;
+pub(crate) use super::isr::isr_nmi as isr_2;
+pub(crate) use super::isr::isr_breakpoint as isr_3;
+pub(crate) use super::isr::isr_overflow as isr_4;
+pub(crate) use super::isr::isr_bound_range as isr_5;
+pub(crate) use super::isr::isr_invalid_opcode as isr_6;
+pub(crate) use super::isr::isr_device_not_available as isr_7;
+pub(crate) use super::isr::isr_double_fault as isr_8;
+pub(crate) use super::isr::isr_coprocessor_segment as isr_9;
+pub(crate) use super::isr::isr_invalid_tss as isr_10;
+pub(crate) use super::isr::isr_segment_not_present as isr_11;
+pub(crate) use super::isr::isr_stack_segment as isr_12;
+pub(crate) use super::isr::isr_general_protection as isr_13;
+pub(crate) use super::isr::isr_page_fault as isr_14;
+pub(crate) use super::isr::isr_reserved_15 as isr_15;
+pub(crate) use super::isr::isr_x87_fp as isr_16;
+pub(crate) use super::isr::isr_alignment_check as isr_17;
+pub(crate) use super::isr::isr_machine_check as isr_18;
+pub(crate) use super::isr::isr_simd_fp as isr_19;
+pub(crate) use super::isr::isr_virtualization as isr_20;
+pub(crate) use super::isr::isr_control_protection as isr_21;
+pub(crate) use super::isr::isr_reserved_22 as isr_22;
+pub(crate) use super::isr::isr_reserved_23 as isr_23;
+pub(crate) use super::isr::isr_reserved_24 as isr_24;
+pub(crate) use super::isr::isr_reserved_25 as isr_25;
+pub(crate) use super::isr::isr_reserved_26 as isr_26;
+pub(crate) use super::isr::isr_reserved_27 as isr_27;
+pub(crate) use super::isr::isr_reserved_28 as isr_28;
+pub(crate) use super::isr::isr_reserved_29 as isr_29;
+pub(crate) use super::isr::isr_reserved_30 as isr_30;
+pub(crate) use super::isr::isr_reserved_31 as isr_31;
+pub(crate) use super::isr::isr_irq0 as isr_32;
+pub(crate) use super::isr::isr_irq1 as isr_33;
+pub(crate) use super::isr::isr_irq2 as isr_34;
+pub(crate) use super::isr::isr_irq3 as isr_35;
+pub(crate) use super::isr::isr_irq4 as isr_36;
+pub(crate) use super::isr::isr_irq5 as isr_37;
+pub(crate) use super::isr::isr_irq6 as isr_38;
+pub(crate) use super::isr::isr_irq7 as isr_39;
+pub(crate) use super::isr::isr_irq8 as isr_40;
+pub(crate) use super::isr::isr_irq9 as isr_41;
+pub(crate) use super::isr::isr_irq10 as isr_42;
+pub(crate) use super::isr::isr_irq11 as isr_43;
+pub(crate) use super::isr::isr_irq12 as isr_44;
+pub(crate) use super::isr::isr_irq13 as isr_45;
+pub(crate) use super::isr::isr_irq14 as isr_46;
+pub(crate) use super::isr::isr_irq15 as isr_47;
