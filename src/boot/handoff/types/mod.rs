14f5c87c866d9ddc2a099001228e812630d3cdf2
A.Erik <ekisanon@proton.me>
2026-02-28 16:37:17 +0100

    boot/handoff: add types module exports
    Module declaration and public exports for all handoff type structures
    and constants.

---
+++ src/boot/handoff/types/mod.rs
@@ -0,0 +1,31 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod constants;
+mod framebuffer;
+mod memory;
+mod info;
+mod security;
+mod handoff;
+#[cfg(test)]
+mod tests;
+
+pub use constants::{HANDOFF_MAGIC, HANDOFF_VERSION, MAX_CMDLINE_LEN, flags, pixel_format};
+pub use framebuffer::FramebufferInfo;
+pub use memory::{memory_type, MemoryMapEntry, MemoryMap};
+pub use info::{AcpiInfo, SmbiosInfo, Module, Modules, Timing};
+pub use security::{Measurements, ZkAttestation, RngSeed};
+pub use handoff::BootHandoffV1;
