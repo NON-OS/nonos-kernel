bd40f9cca155497b9166afa4a555c693c35a81e8
A.Erik <ekisanon@proton.me>
2026-02-28 15:55:20 +0100

    add uefi revisions

---
+++ src/arch/x86_64/uefi/constants/revisions.rs
@@ -0,0 +1,51 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub const UEFI_REVISION_2_0: u32 = 0x00020000;
+
+pub const UEFI_REVISION_2_1: u32 = 0x00020100;
+
+pub const UEFI_REVISION_2_3: u32 = 0x00020300;
+
+pub const UEFI_REVISION_2_3_1: u32 = 0x0002001F;
+
+pub const UEFI_REVISION_2_4: u32 = 0x00020400;
+
+pub const UEFI_REVISION_2_5: u32 = 0x00020500;
+
+pub const UEFI_REVISION_2_6: u32 = 0x00020600;
+
+pub const UEFI_REVISION_2_7: u32 = 0x00020700;
+
+pub const UEFI_REVISION_2_8: u32 = 0x00020800;
+
+pub const UEFI_REVISION_2_9: u32 = 0x00020900;
+
+pub const UEFI_REVISION_2_10: u32 = 0x00020A00;
+
+pub const RESET_TYPE_COLD: u32 = 0;
+
+pub const RESET_TYPE_WARM: u32 = 1;
+
+pub const RESET_TYPE_SHUTDOWN: u32 = 2;
+
+pub const RESET_TYPE_PLATFORM_SPECIFIC: u32 = 3;
+
+pub const EFI_UNSPECIFIED_TIMEZONE: i16 = 0x07FF;
+
+pub const EFI_TIME_ADJUST_DAYLIGHT: u8 = 0x01;
+
+pub const EFI_TIME_IN_DAYLIGHT: u8 = 0x02;
