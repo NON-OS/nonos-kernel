e72bdb7a85f09004fe059203a176db1bf08e666e
A.Erik <ekisanon@proton.me>
2026-02-28 13:15:36 +0100

    idt module structure

--- src/arch/x86_64/idt/mod.rs
+++ src/arch/x86_64/idt/mod.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-//! x86_64 Interrupt Descriptor Table.
 
 mod api;
 pub mod constants;
