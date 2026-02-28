654979ee71530aeee6b9a483f1a20028394368fc
A.Erik <ekisanon@proton.me>
2026-02-28 21:21:50 +0100

    add blake3 constants

---
+++ src/crypto/hash/blake3/constants.rs
@@ -0,0 +1,46 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub const OUT_LEN: usize = 32;
+pub const KEY_LEN: usize = 32;
+
+pub(crate) const IV: [u32; 8] = [
+    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+    0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
+];
+
+pub(crate) const BLOCK_LEN: usize = 64;
+pub(crate) const CHUNK_LEN: usize = 1024;
+pub(crate) const ROUNDS: usize = 7;
+pub(crate) const MAX_DEPTH: usize = 54;
+
+pub(crate) const CHUNK_START: u8         = 1 << 0;
+pub(crate) const CHUNK_END: u8           = 1 << 1;
+pub(crate) const PARENT: u8              = 1 << 2;
+pub(crate) const ROOT: u8                = 1 << 3;
+pub(crate) const KEYED_HASH: u8          = 1 << 4;
+pub(crate) const DERIVE_KEY_CONTEXT: u8  = 1 << 5;
+pub(crate) const DERIVE_KEY_MATERIAL: u8 = 1 << 6;
+
+pub(crate) const MSG_SCHEDULE: [[usize; 16]; 7] = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    [2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8],
+    [3, 4, 10, 12, 13, 2, 7, 14, 6, 5, 9, 0, 11, 15, 8, 1],
+    [10, 7, 12, 9, 14, 3, 13, 15, 4, 0, 11, 2, 5, 8, 1, 6],
+    [12, 13, 9, 11, 15, 10, 14, 8, 7, 2, 5, 3, 0, 1, 6, 4],
+    [9, 14, 11, 5, 8, 12, 15, 1, 13, 3, 0, 10, 2, 6, 4, 7],
+    [11, 15, 5, 0, 1, 9, 8, 6, 14, 10, 2, 12, 3, 4, 7, 13],
+];
