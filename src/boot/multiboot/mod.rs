a0254bb9493a892f5f1b9e45ada5f52b71cad959
A.Erik <ekisanon@proton.me>
2026-02-28 16:39:34 +0100

    boot/multiboot: update module exports for refactor
    Point module declarations to types/, platform/, and parser/
    directories with updated public exports.

--- src/boot/multiboot/mod.rs
+++ src/boot/multiboot/mod.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -17,6 +17,7 @@
 mod parser;
 mod platform;
 mod types;
+
 #[cfg(test)]
 mod tests;
 
