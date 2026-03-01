6dfb6d667610afecbc7dc46048f0ba0dfd5a8fe6
A.Erik <ekisanon@proton.me>
2026-03-01 13:23:40 +0100

    drivers/usb: update class driver framework
    Remove comments from driver registration.

--- src/drivers/usb/class_driver.rs
+++ src/drivers/usb/class_driver.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! USB class driver registry.
+
 extern crate alloc;
 
 use alloc::sync::Arc;
@@ -27,7 +29,9 @@ pub trait UsbClassDriver: Send + Sync + 'static {
     fn matches(&self, dev: &UsbDevice, cfg: &UsbConfiguration, iface: &UsbInterfaceInfo) -> bool;
     fn bind(&self, dev: &UsbDevice, cfg: &UsbConfiguration, iface: &UsbInterfaceInfo) -> Result<(), &'static str>;
     fn name(&self) -> &'static str;
+
     fn unbind(&self, _dev: &UsbDevice, _iface: &UsbInterfaceInfo) {}
+
     fn priority(&self) -> u8 {
         0
     }
@@ -54,31 +58,50 @@ pub fn get_class_drivers() -> Vec<Arc<dyn UsbClassDriver>> {
 
 pub fn bind_drivers_to_device(dev: &UsbDevice) {
     let drivers = get_class_drivers();
+
+    crate::log::logger::log_critical(&alloc::format!(
+        "[USB] Binding drivers for device {:04x}:{:04x} slot {}",
+        dev.vendor_id(), dev.product_id(), dev.slot_id
+    ));
+
     if let Some(cfg) = &dev.active_config {
+        crate::log::logger::log_critical(&alloc::format!(
+            "[USB] Device has {} interface(s), {} driver(s) registered",
+            cfg.interfaces.len(), drivers.len()
+        ));
+
         for iface in &cfg.interfaces {
+            crate::log::logger::log_critical(&alloc::format!(
+                "[USB] Interface {}: class={:02x} subclass={:02x} proto={:02x}",
+                iface.iface.b_interface_number,
+                iface.iface.b_interface_class,
+                iface.iface.b_interface_sub_class,
+                iface.iface.b_interface_protocol
+            ));
+
             for driver in &drivers {
                 if driver.matches(dev, cfg, iface) {
+                    crate::log::logger::log_critical(&alloc::format!(
+                        "[USB] Driver '{}' matched!", driver.name()
+                    ));
                     match driver.bind(dev, cfg, iface) {
                         Ok(()) => {
-                            crate::log_info!(
-                                "[USB] Bound {} to interface {}",
-                                driver.name(),
-                                iface.iface.b_interface_number
-                            );
+                            crate::log::logger::log_critical(&alloc::format!(
+                                "[USB] {} bound successfully", driver.name()
+                            ));
                             break;
                         }
                         Err(e) => {
-                            crate::log_warn!(
-                                "[USB] Failed to bind {} to interface {}: {}",
-                                driver.name(),
-                                iface.iface.b_interface_number,
-                                e
-                            );
+                            crate::log::logger::log_critical(&alloc::format!(
+                                "[USB] {} bind failed: {}", driver.name(), e
+                            ));
                         }
                     }
                 }
             }
         }
+    } else {
+        crate::log::logger::log_critical("[USB] Device has no active configuration!");
     }
 }
 
