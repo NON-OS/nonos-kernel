60565b5841c29636bbbbae4a4caf66979c8df87e
A.Erik <ekisanon@proton.me>
2026-03-01 13:27:56 +0100

    drivers/wifi: update constants
    Refactor WiFi feature flags and channels.

--- src/drivers/wifi/constants.rs
+++ src/drivers/wifi/constants.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -15,7 +15,9 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 //! Intel WiFi constants and device IDs.
+
 pub const INTEL_VENDOR_ID: u16 = 0x8086;
+
 pub const SUPPORTED_DEVICE_IDS: &[u16] = &[
     0x2723, 0x2725, 0x34F0, 0x3DF0, 0x4DF0, 0x2729, 0x272B, 0x51F0, 0x51F1, 0x54F0, 0x2526, 0x9DF0,
     0xA370, 0x31DC, 0x30DC, 0x24F3, 0x24F4, 0x24F5, 0x24F6, 0x24FD, 0x08B1, 0x08B2, 0x08B3, 0x08B4,
@@ -62,11 +64,13 @@ pub mod csr_bits {
     pub const GP_CNTRL_REG_FLAG_INIT_DONE: u32 = 0x00000004;
     pub const GP_CNTRL_REG_FLAG_MAC_CLOCK_READY: u32 = 0x00000001;
     pub const GP_CNTRL_REG_FLAG_GOING_TO_SLEEP: u32 = 0x00000010;
+
     pub const RESET_REG_FLAG_NEVO_RESET: u32 = 0x00000001;
     pub const RESET_REG_FLAG_FORCE_NMI: u32 = 0x00000002;
     pub const RESET_REG_FLAG_SW_RESET: u32 = 0x00000080;
     pub const RESET_REG_FLAG_MASTER_DISABLED: u32 = 0x00000100;
     pub const RESET_REG_FLAG_STOP_MASTER: u32 = 0x00000200;
+
     pub const INT_BIT_FH_RX: u32 = 1 << 26;
     pub const INT_BIT_HW_ERR: u32 = 1 << 29;
     pub const INT_BIT_FH_TX: u32 = 1 << 27;
@@ -82,12 +86,14 @@ pub mod fh {
     pub const RSCSR_CHNL0_RBDCB_BASE_REG: u32 = 0x1BC8;
     pub const RSCSR_CHNL0_WPTR: u32 = 0x1BC8;
     pub const RSCSR_CHNL0_RBDCB_WPTR_REG: u32 = 0x1BC8;
+
     pub const RCSR_CHNL0_CONFIG_REG: u32 = 0x1F40;
     pub const RCSR_RX_CONFIG_REG_IRQ_DEST_HOST: u32 = 1 << 12;
     pub const RCSR_RX_CONFIG_REG_SINGLE_FRAME: u32 = 1 << 11;
     pub const RCSR_RX_CONFIG_REG_RB_SIZE_4K: u32 = 0 << 8;
     pub const RCSR_RX_CONFIG_REG_RBDCB_SIZE_8: u32 = 0 << 4;
     pub const RCSR_RX_CONFIG_REG_RB_TIMEOUT_IMMEDIATE: u32 = 1 << 0;
+
     pub const TCSR_CHNL_TX_CONFIG_REG: u32 = 0x1D00;
     pub const TCSR_TX_CONFIG_REG_VAL_DMA_CHNL_PAUSE: u32 = 0x00000000;
     pub const TCSR_TX_CONFIG_REG_VAL_DMA_CHNL_ENABLE: u32 = 0x80000000;
@@ -170,20 +176,26 @@ pub const NUM_TFD_QUEUES: usize = 31;
 pub const TFD_QUEUE_SIZE_LOG: u32 = 8;
 pub const TFD_QUEUE_SIZE: usize = 1 << TFD_QUEUE_SIZE_LOG;
 pub const TFD_QUEUE_SIZE_MASK: usize = TFD_QUEUE_SIZE - 1;
+
 pub const RX_QUEUE_SIZE_LOG: u32 = 8;
 pub const RX_QUEUE_SIZE: usize = 1 << RX_QUEUE_SIZE_LOG;
 pub const RX_QUEUE_SIZE_MASK: usize = RX_QUEUE_SIZE - 1;
+
 pub const RX_BUFFER_SIZE: usize = 4096;
 pub const TX_BUFFER_SIZE: usize = 4096;
+
 pub const MAX_CMD_PAYLOAD_SIZE: usize = 320;
+
 pub const ALIVE_TIMEOUT_MS: u64 = 2000;
 pub const INIT_TIMEOUT_MS: u64 = 5000;
 pub const SCAN_TIMEOUT_MS: u64 = 10000;
 pub const CONNECT_TIMEOUT_MS: u64 = 5000;
+
 pub const APM_INIT_TIMEOUT_US: u64 = 25000;
 pub const NIC_ACCESS_TIMEOUT_US: u64 = 15000;
 pub const STOP_MASTER_TIMEOUT_US: u64 = 100000;
 pub const INT_COALESCING_TIMEOUT: u32 = 64;
+
 pub const TFD_ALIGNMENT: usize = 256;
 pub const TB_ALIGNMENT: usize = 64;
 pub const BC_TBL_ALIGNMENT: usize = 16;
@@ -191,14 +203,19 @@ pub const RX_BD_ALIGNMENT: usize = 256;
 pub const RX_STATUS_ALIGNMENT: usize = 16;
 pub const RX_BUFFER_ALIGNMENT: usize = 4096;
 pub const DMA_ALIGNMENT: usize = 4096;
+
 pub const SCAN_CMD_SIZE: usize = 256;
 pub const MAC_CONTEXT_CMD_SIZE: usize = 128;
 pub const AUTH_CMD_SIZE: usize = 256;
+
 pub const RSSI_INVALID: i8 = -100;
+
 pub const FW_MEM_EXTENDED_START: u32 = 0x40000;
 pub const FW_MEM_EXTENDED_END: u32 = 0x57FFF;
+
 pub const MIN_FW_API_VERSION: u16 = 22;
 pub const MAX_FW_API_VERSION: u16 = 77;
+
 pub const PRPH_BASE: u32 = 0x44000;
 pub const PRPH_DWORD: u32 = PRPH_BASE + 0x0C;
 pub const PRPH_DATA: u32 = PRPH_BASE + 0x10;
@@ -207,15 +224,19 @@ pub const HBUS_TARG_MEM_WDAT: u32 = 0x40004;
 pub const TX_QUEUE_WRITE_PTR_BASE: u32 = 0x1C08;
 pub const CSR_UCODE_BASE: u32 = 0x044000;
 pub const NVM_MAC_ADDR: u32 = 0x00A01020;
+
 pub const DMA_BLOCK_SIZE: usize = 0x1000;
 pub const KERNEL_PHYS_MASK: u64 = 0xFFFF_FFFF;
 pub const KERNEL_RESERVED_SIZE: u64 = 0x0200_0000;
+
 pub const IWL_FW_MAGIC: u32 = 0x0a4c5749;
 pub const FW_API_VERSION_MASK: u32 = 0xFFFF;
+
 pub const ALL_INTS_MASK: u32 = 0xFFFF_FFFF;
 pub const INT_MASK_DISABLED: u32 = 0x0000_0000;
 pub const RX_STATUS_PTR_MASK: u32 = 0xFFF;
 pub const PRPH_READ_FLAG: u32 = 3 << 24;
+
 pub const IEEE80211_FC_TODS: u16 = 0x0100;
 pub const FRAME_TYPE_MASK: u16 = 0x000C;
 pub const FRAME_SUBTYPE_MASK: u16 = 0x00F0;
