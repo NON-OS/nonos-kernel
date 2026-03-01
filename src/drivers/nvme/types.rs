fe30ae25c14cde4c577cee24b9c50f582b179617
A.Erik <ekisanon@proton.me>
2026-03-01 13:18:43 +0100

    drivers/nvme: update type definitions
    Remove comments from NVMe data structures.

--- src/drivers/nvme/types.rs
+++ src/drivers/nvme/types.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,7 +14,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! NVMe data types.
+
 use core::mem;
+
 #[repr(C, align(64))]
 #[derive(Clone, Copy)]
 pub struct SubmissionEntry {
@@ -35,6 +38,7 @@ pub struct SubmissionEntry {
 
 impl SubmissionEntry {
     pub const SIZE: usize = mem::size_of::<Self>();
+
     #[inline]
     pub const fn new() -> Self {
         Self {
