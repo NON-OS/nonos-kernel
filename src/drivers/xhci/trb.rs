3f6b928238b30a6cbd9775cd6640c3f744a75f3a
A.Erik <ekisanon@proton.me>
2026-03-01 13:32:27 +0100

    drivers/xhci: update TRB definitions
    Refactor transfer request block structure.

--- src/drivers/xhci/trb.rs
+++ src/drivers/xhci/trb.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,8 +14,11 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! Transfer Request Block (TRB) definitions and operations.
+
 use super::constants::*;
 use super::error::{XhciError, XhciResult};
+
 #[repr(C, align(16))]
 #[derive(Clone, Copy, Debug)]
 pub struct Trb {
