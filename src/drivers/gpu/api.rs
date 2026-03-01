2d3a84278c283edb7f6601c4fbd75823bbed9587
A.Erik <ekisanon@proton.me>
2026-03-01 03:35:52 +0100

    drivers/gpu: add api module for GPU operations
    Move init_gpu and driver access functions to dedicated module.

---
+++ src/drivers/gpu/api.rs
@@ -0,0 +1,78 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use spin::Mutex;
+
+use super::driver::{GpuDriver, GpuStats, GPU_ONCE};
+use super::surface::{DisplayMode, GpuSurface};
+
+static GPU_HANDLE: spin::Once<&'static Mutex<GpuDriver>> = spin::Once::new();
+
+pub fn init_gpu() -> Result<(), &'static str> {
+    let handle = GpuDriver::init()?;
+    GPU_HANDLE.call_once(|| handle);
+    crate::log::logger::log_critical("GPU: Bochs VBE initialized");
+    Ok(())
+}
+
+pub fn with_driver<T, F>(f: F) -> Option<T>
+where
+    F: FnOnce(&GpuDriver) -> T,
+{
+    GPU_HANDLE.get().map(|m| f(&m.lock()))
+}
+
+pub fn with_driver_mut<T, F>(f: F) -> Option<T>
+where
+    F: FnOnce(&mut GpuDriver) -> T,
+{
+    GPU_HANDLE.get().map(|m| f(&mut m.lock()))
+}
+
+pub fn set_mode_32bpp(width: u16, height: u16) -> Result<DisplayMode, &'static str> {
+    GpuDriver::set_mode_32bpp(width, height)
+}
+
+pub fn disable_gpu() -> Result<(), &'static str> {
+    GpuDriver::disable()
+}
+
+pub fn get_surface() -> Option<GpuSurface> {
+    GpuDriver::get_surface()
+}
+
+pub fn is_initialized() -> bool {
+    GPU_ONCE.get().is_some()
+}
+
+pub fn get_stats() -> Option<GpuStats> {
+    with_driver(|drv| drv.get_stats())
+}
+
+pub fn get_mode() -> Option<DisplayMode> {
+    with_driver(|drv| drv.surface.mode)
+}
+
+pub fn get_dimensions() -> Option<(u16, u16)> {
+    with_driver(|drv| (drv.surface.mode.width, drv.surface.mode.height))
+}
+
+pub fn clear_screen(color: u32) {
+    if let Some(surface) = get_surface() {
+        surface.clear(color);
+        surface.present(None);
+    }
+}
