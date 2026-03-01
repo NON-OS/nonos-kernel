99cf673353e5fa110c371a794178a56c25d7cecd
A.Erik <ekisanon@proton.me>
2026-03-01 13:25:00 +0100

    drivers/vga: update constants
    Refactor VGA dimension constants.

--- src/drivers/vga/constants.rs
+++ src/drivers/vga/constants.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,23 +14,44 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! VGA text mode constants.
+
 pub const VGA_BUFFER_ADDR: usize = 0xB8000;
+
 pub const VGA_WIDTH: usize = 80;
+
 pub const VGA_HEIGHT: usize = 25;
+
 pub const VGA_TOTAL_CELLS: usize = VGA_WIDTH * VGA_HEIGHT;
+
 pub const VGA_BUFFER_SIZE: usize = VGA_TOTAL_CELLS * 2;
+
 pub const CRT_INDEX_PORT: u16 = 0x3D4;
+
 pub const CRT_DATA_PORT: u16 = 0x3D5;
+
 pub const CRT_CURSOR_START: u8 = 0x0A;
+
 pub const CRT_CURSOR_END: u8 = 0x0B;
+
 pub const CRT_CURSOR_LOC_HIGH: u8 = 0x0E;
+
 pub const CRT_CURSOR_LOC_LOW: u8 = 0x0F;
+
 pub const CURSOR_DISABLE_BIT: u8 = 0x20;
+
 pub const CURSOR_START_MASK: u8 = 0x1F;
+
 pub const CURSOR_END_MASK: u8 = 0x1F;
+
 pub const DEFAULT_CURSOR_START: u8 = 0;
+
 pub const DEFAULT_CURSOR_END: u8 = 15;
+
 pub const SPACE_CHAR: u8 = b' ';
+
 pub const BACKSPACE_CHAR: u8 = 0x08;
+
 pub const PRINTABLE_START: u8 = 0x20;
+
 pub const PRINTABLE_END: u8 = 0x7E;
