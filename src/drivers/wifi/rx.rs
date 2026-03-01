09fcfd5135fc6afd4b4351edde84d68a4eba1ddc
A.Erik <ekisanon@proton.me>
2026-03-01 13:28:33 +0100

    drivers/wifi: update receive path
    Refactor RX descriptor handling.

--- src/drivers/wifi/rx.rs
+++ src/drivers/wifi/rx.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,267 +14,303 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use super::error::WifiError;
-use super::tx::{IEEE80211_FTYPE_DATA, IEEE80211_FTYPE_MGMT};
-use alloc::string::ToString;
+//! WiFi receive path with frame filtering and processing.
+
+use alloc::collections::VecDeque;
 use alloc::vec::Vec;
 
-pub const RX_RES_STATUS_DECRYPT_OK: u32 = 1 << 3;
-pub const RX_RES_STATUS_MIC_OK: u32 = 1 << 5;
-pub const RX_RES_STATUS_CRCF32_OK: u32 = 1 << 6;
-pub const RX_RES_STATUS_OVERRUN: u32 = 1 << 24;
+const _RX_BUFFER_SIZE: usize = 64;
 
-#[repr(C, packed)]
-pub struct RxPhyInfo {
-    pub non_cfg_phy_cnt: u8,
-    pub cfg_phy_cnt: u8,
-    pub stat_id: u8,
-    pub reserved: u8,
-    pub timestamp: u32,
-    pub beacon_time_stamp: u32,
-    pub phy_flags: u16,
-    pub channel: u16,
-    pub non_cfg_phy: [u32; 8],
-    pub rate_n_flags: u32,
-    pub byte_count: u16,
-    pub frame_time: u16,
-}
+const _FRAME_TYPE_MGMT: u8 = 0;
+const _FRAME_TYPE_CTRL: u8 = 1;
+const _FRAME_TYPE_DATA: u8 = 2;
 
-#[repr(C, packed)]
-pub struct RxMpduRes {
-    pub byte_count: u16,
-    pub reserved: u16,
-    pub status: u32,
-}
+const _MGMT_SUBTYPE_BEACON: u8 = 8;
+const _MGMT_SUBTYPE_PROBE_RESP: u8 = 5;
+const _MGMT_SUBTYPE_AUTH: u8 = 11;
+const _MGMT_SUBTYPE_DEAUTH: u8 = 12;
+const _MGMT_SUBTYPE_ASSOC_RESP: u8 = 1;
+const _MGMT_SUBTYPE_DISASSOC: u8 = 10;
+
+const _DATA_SUBTYPE_DATA: u8 = 0;
+const _DATA_SUBTYPE_QOS_DATA: u8 = 8;
 
 #[derive(Debug, Clone)]
-pub struct RxFrame {
-    pub frame_type: FrameType,
-    pub src_addr: [u8; 6],
-    pub dst_addr: [u8; 6],
-    pub bssid: [u8; 6],
-    pub rssi: i8,
-    pub channel: u8,
-    pub payload: Vec<u8>,
+pub(super) struct _RxFrameInfo {
+    pub frame_type: _FrameType,
+    pub subtype: u8,
+    pub to_ds: bool,
+    pub from_ds: bool,
+    pub protected: bool,
+    pub addr1: [u8; 6],
+    pub addr2: [u8; 6],
+    pub addr3: [u8; 6],
+    pub seq_num: u16,
+    pub payload_offset: usize,
+    pub payload_len: usize,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum FrameType {
-    Beacon,
-    ProbeResponse,
-    AssocResponse,
-    ReassocResponse,
-    Auth,
-    Deauth,
-    Disassoc,
+pub(super) enum _FrameType {
+    Management,
+    Control,
     Data,
-    QosData,
-    Action,
     Unknown,
 }
 
-pub struct RxProcessor {
-    bssid_filter: Option<[u8; 6]>,
-    pub promiscuous: bool,
+#[derive(Debug, Clone)]
+pub(super) struct _RxFrame {
+    pub info: _RxFrameInfo,
+    pub data: Vec<u8>,
+    pub rssi: i8,
+    pub channel: u8,
+    pub timestamp: u64,
 }
 
-impl RxProcessor {
-    pub fn new() -> Self {
+pub(super) struct _RxProcessor {
+    bssid_filter: Option<[u8; 6]>,
+    our_mac: [u8; 6],
+    pub promiscuous: bool,
+    data_queue: VecDeque<_RxFrame>,
+    mgmt_queue: VecDeque<_RxFrame>,
+    frames_received: u64,
+    frames_filtered: u64,
+    frames_malformed: u64,
+}
+
+impl _RxProcessor {
+    pub(super) fn new() -> Self {
         Self {
             bssid_filter: None,
+            our_mac: [0; 6],
             promiscuous: false,
+            data_queue: VecDeque::with_capacity(_RX_BUFFER_SIZE),
+            mgmt_queue: VecDeque::with_capacity(16),
+            frames_received: 0,
+            frames_filtered: 0,
+            frames_malformed: 0,
         }
     }
 
-    pub fn set_bssid_filter(&mut self, bssid: Option<[u8; 6]>) {
+    pub(super) fn set_bssid_filter(&mut self, bssid: Option<[u8; 6]>) {
         self.bssid_filter = bssid;
     }
 
-    pub fn set_promiscuous(&mut self, enable: bool) {
+    pub(super) fn set_our_mac(&mut self, mac: [u8; 6]) {
+        self.our_mac = mac;
+    }
+
+    pub(super) fn set_promiscuous(&mut self, enable: bool) {
         self.promiscuous = enable;
     }
 
-    pub fn process_mpdu(
-        &self,
-        data: &[u8],
-        phy_info: &RxPhyInfo,
-    ) -> Result<Option<RxFrame>, WifiError> {
-        if data.len() < 24 {
-            return Ok(None);
-        }
+    pub(super) fn process_frame(&mut self, raw_frame: &[u8], rssi: i8, channel: u8) -> Option<_RxFrameInfo> {
+        self.frames_received += 1;
 
-        let fc = u16::from_le_bytes([data[0], data[1]]);
-        let frame_type = fc & super::constants::FRAME_TYPE_MASK;
-        let frame_subtype = fc & super::constants::FRAME_SUBTYPE_MASK;
-
-        let mut src_addr = [0u8; 6];
-        let mut dst_addr = [0u8; 6];
-        let mut bssid = [0u8; 6];
-
-        dst_addr.copy_from_slice(&data[4..10]);
-        src_addr.copy_from_slice(&data[10..16]);
-        bssid.copy_from_slice(&data[16..22]);
-
-        if let Some(filter_bssid) = self.bssid_filter {
-            if !self.promiscuous && bssid != filter_bssid {
-                return Ok(None);
+        let info = match self.parse_frame(raw_frame) {
+            Some(info) => info,
+            None => {
+                self.frames_malformed += 1;
+                return None;
             }
+        };
+
+        if !self.promiscuous && !self.should_accept(&info) {
+            self.frames_filtered += 1;
+            return None;
         }
 
-        let ftype = match (frame_type, frame_subtype) {
-            (IEEE80211_FTYPE_MGMT, 0x80) => FrameType::Beacon,
-            (IEEE80211_FTYPE_MGMT, 0x50) => FrameType::ProbeResponse,
-            (IEEE80211_FTYPE_MGMT, 0x10) => FrameType::AssocResponse,
-            (IEEE80211_FTYPE_MGMT, 0x30) => FrameType::ReassocResponse,
-            (IEEE80211_FTYPE_MGMT, 0xB0) => FrameType::Auth,
-            (IEEE80211_FTYPE_MGMT, 0xC0) => FrameType::Deauth,
-            (IEEE80211_FTYPE_MGMT, 0xA0) => FrameType::Disassoc,
-            (IEEE80211_FTYPE_MGMT, 0xD0) => FrameType::Action,
-            (IEEE80211_FTYPE_DATA, 0x00) => FrameType::Data,
-            (IEEE80211_FTYPE_DATA, 0x80) => FrameType::QosData,
-            _ => FrameType::Unknown,
-        };
-
-        let payload_offset = match ftype {
-            FrameType::Data => 24,
-            FrameType::QosData => 26,
-            _ => 24,
-        };
-
-        let payload = if data.len() > payload_offset {
-            data[payload_offset..].to_vec()
-        } else {
-            Vec::new()
-        };
-
-        let rssi = self.calculate_rssi(phy_info);
-        let channel = (phy_info.channel & 0xFF) as u8;
-
-        Ok(Some(RxFrame {
-            frame_type: ftype,
-            src_addr,
-            dst_addr,
-            bssid,
+        let frame = _RxFrame {
+            info: info.clone(),
+            data: raw_frame.to_vec(),
             rssi,
             channel,
-            payload,
-        }))
-    }
+            timestamp: crate::arch::x86_64::time::tsc::elapsed_us(),
+        };
 
-    fn calculate_rssi(&self, phy_info: &RxPhyInfo) -> i8 {
-        let agc = (phy_info.non_cfg_phy[1] >> 24) as i8;
-        -44 - agc
-    }
-
-    pub fn extract_data_payload(&self, frame: &RxFrame) -> Option<Vec<u8>> {
-        if !matches!(frame.frame_type, FrameType::Data | FrameType::QosData) {
-            return None;
-        }
-
-        if frame.payload.len() < 8 {
-            return None;
-        }
-
-        if frame.payload[0..3] == [0xAA, 0xAA, 0x03] {
-            Some(frame.payload[8..].to_vec())
-        } else {
-            Some(frame.payload.clone())
-        }
-    }
-}
-
-pub fn parse_beacon(frame: &RxFrame) -> Option<BeaconInfo> {
-    if frame.frame_type != FrameType::Beacon && frame.frame_type != FrameType::ProbeResponse {
-        return None;
-    }
-
-    if frame.payload.len() < 12 {
-        return None;
-    }
-
-    let timestamp = u64::from_le_bytes([
-        frame.payload[0],
-        frame.payload[1],
-        frame.payload[2],
-        frame.payload[3],
-        frame.payload[4],
-        frame.payload[5],
-        frame.payload[6],
-        frame.payload[7],
-    ]);
-    let beacon_interval = u16::from_le_bytes([frame.payload[8], frame.payload[9]]);
-    let capability = u16::from_le_bytes([frame.payload[10], frame.payload[11]]);
-    let mut ssid = None;
-    let mut channel = None;
-    let mut supported_rates = Vec::new();
-    let mut is_wpa = false;
-    let mut is_wpa2 = false;
-    let mut is_wpa3 = false;
-    let mut ie_offset = 12;
-    while ie_offset + 2 <= frame.payload.len() {
-        let ie_id = frame.payload[ie_offset];
-        let ie_len = frame.payload[ie_offset + 1] as usize;
-        if ie_offset + 2 + ie_len > frame.payload.len() {
-            break;
-        }
-
-        let ie_data = &frame.payload[ie_offset + 2..ie_offset + 2 + ie_len];
-
-        match ie_id {
-            0 => {
-                if let Ok(s) = core::str::from_utf8(ie_data) {
-                    ssid = Some(s.to_string());
+        match info.frame_type {
+            _FrameType::Data => {
+                if self.data_queue.len() >= _RX_BUFFER_SIZE {
+                    self.data_queue.pop_front();
                 }
+                self.data_queue.push_back(frame);
             }
-            1 => {
-                supported_rates.extend_from_slice(ie_data);
-            }
-            3 => {
-                if !ie_data.is_empty() {
-                    channel = Some(ie_data[0]);
+            _FrameType::Management => {
+                if self.mgmt_queue.len() >= 16 {
+                    self.mgmt_queue.pop_front();
                 }
-            }
-            48 => {
-                is_wpa2 = true;
-                if ie_data.len() >= 4 && ie_data[2..4] == [0x00, 0x0F, 0xAC, 0x08] {
-                    is_wpa3 = true;
-                }
-            }
-            221 => {
-                if ie_data.len() >= 4 && ie_data[0..4] == [0x00, 0x50, 0xF2, 0x01] {
-                    is_wpa = true;
-                }
+                self.mgmt_queue.push_back(frame);
             }
             _ => {}
         }
 
-        ie_offset += 2 + ie_len;
+        Some(info)
     }
 
-    Some(BeaconInfo {
-        bssid: frame.bssid,
-        ssid: ssid.unwrap_or_default(),
-        channel: channel.unwrap_or(0),
-        rssi: frame.rssi,
-        timestamp,
-        beacon_interval,
-        capability,
-        is_wpa,
-        is_wpa2,
-        is_wpa3,
-    })
-}
+    fn parse_frame(&self, data: &[u8]) -> Option<_RxFrameInfo> {
+        if data.len() < 24 {
+            return None;
+        }
 
-#[derive(Debug, Clone)]
-pub struct BeaconInfo {
-    pub bssid: [u8; 6],
-    pub ssid: alloc::string::String,
-    pub channel: u8,
-    pub rssi: i8,
-    pub timestamp: u64,
-    pub beacon_interval: u16,
-    pub capability: u16,
-    pub is_wpa: bool,
-    pub is_wpa2: bool,
-    pub is_wpa3: bool,
+        let frame_control = u16::from_le_bytes([data[0], data[1]]);
+
+        let frame_type_bits = ((frame_control >> 2) & 0x03) as u8;
+        let subtype = ((frame_control >> 4) & 0x0F) as u8;
+        let to_ds = (frame_control & 0x0100) != 0;
+        let from_ds = (frame_control & 0x0200) != 0;
+        let protected = (frame_control & 0x4000) != 0;
+
+        let frame_type = match frame_type_bits {
+            _FRAME_TYPE_MGMT => _FrameType::Management,
+            _FRAME_TYPE_CTRL => _FrameType::Control,
+            _FRAME_TYPE_DATA => _FrameType::Data,
+            _ => _FrameType::Unknown,
+        };
+
+        let mut addr1 = [0u8; 6];
+        let mut addr2 = [0u8; 6];
+        let mut addr3 = [0u8; 6];
+        addr1.copy_from_slice(&data[4..10]);
+        addr2.copy_from_slice(&data[10..16]);
+        addr3.copy_from_slice(&data[16..22]);
+
+        let seq_ctrl = u16::from_le_bytes([data[22], data[23]]);
+        let seq_num = seq_ctrl >> 4;
+
+        let mut payload_offset = 24;
+
+        if frame_type == _FrameType::Data && (subtype & 0x08) != 0 {
+            payload_offset += 2; // QoS control field
+        }
+
+        if (frame_control & 0x8000) != 0 {
+            payload_offset += 4;
+        }
+
+        if to_ds && from_ds {
+            payload_offset += 6;
+        }
+
+        let payload_len = if data.len() > payload_offset {
+            data.len() - payload_offset
+        } else {
+            0
+        };
+
+        Some(_RxFrameInfo {
+            frame_type,
+            subtype,
+            to_ds,
+            from_ds,
+            protected,
+            addr1,
+            addr2,
+            addr3,
+            seq_num,
+            payload_offset,
+            payload_len,
+        })
+    }
+
+    fn should_accept(&self, info: &_RxFrameInfo) -> bool {
+        if info.addr1[0] & 0x01 != 0 {
+            if let Some(bssid) = self.bssid_filter {
+                return info.addr3 == bssid || info.addr2 == bssid;
+            }
+            return true;
+        }
+
+        if info.addr1 == self.our_mac {
+            if let Some(bssid) = self.bssid_filter {
+                return info.addr2 == bssid || info.addr3 == bssid;
+            }
+            return true;
+        }
+
+        if info.frame_type == _FrameType::Management {
+            match info.subtype {
+                _MGMT_SUBTYPE_BEACON | _MGMT_SUBTYPE_PROBE_RESP => {
+                    return true;
+                }
+                _ => {}
+            }
+        }
+
+        false
+    }
+
+    pub(super) fn dequeue_data(&mut self) -> Option<_RxFrame> {
+        self.data_queue.pop_front()
+    }
+
+    pub(super) fn dequeue_mgmt(&mut self) -> Option<_RxFrame> {
+        self.mgmt_queue.pop_front()
+    }
+
+    pub(super) fn has_data(&self) -> bool {
+        !self.data_queue.is_empty()
+    }
+
+    pub(super) fn has_mgmt(&self) -> bool {
+        !self.mgmt_queue.is_empty()
+    }
+
+    pub(super) fn data_queue_len(&self) -> usize {
+        self.data_queue.len()
+    }
+
+    pub(super) fn stats(&self) -> (u64, u64, u64) {
+        (self.frames_received, self.frames_filtered, self.frames_malformed)
+    }
+
+    pub(super) fn clear(&mut self) {
+        self.data_queue.clear();
+        self.mgmt_queue.clear();
+    }
+
+    pub(super) fn get_source_mac(info: &_RxFrameInfo) -> [u8; 6] {
+        match (info.to_ds, info.from_ds) {
+            (false, false) => info.addr2, // IBSS: SA = Addr2
+            (true, false) => info.addr2,  // To AP: SA = Addr2
+            (false, true) => info.addr3,  // From AP: SA = Addr3
+            (true, true) => info.addr2,   // WDS: SA = Addr4 (not handled here)
+        }
+    }
+
+    pub(super) fn get_dest_mac(info: &_RxFrameInfo) -> [u8; 6] {
+        match (info.to_ds, info.from_ds) {
+            (false, false) => info.addr1, // IBSS: DA = Addr1
+            (true, false) => info.addr3,  // To AP: DA = Addr3
+            (false, true) => info.addr1,  // From AP: DA = Addr1
+            (true, true) => info.addr3,   // WDS: DA = Addr3
+        }
+    }
+
+    pub(super) fn get_bssid(info: &_RxFrameInfo) -> [u8; 6] {
+        match (info.to_ds, info.from_ds) {
+            (false, false) => info.addr3, // IBSS: BSSID = Addr3
+            (true, false) => info.addr1,  // To AP: BSSID = Addr1
+            (false, true) => info.addr2,  // From AP: BSSID = Addr2
+            (true, true) => [0; 6],       // WDS: No single BSSID
+        }
+    }
+
+    pub(super) fn is_eapol_frame(data: &[u8], info: &_RxFrameInfo) -> bool {
+        if info.payload_len < 8 {
+            return false;
+        }
+
+        let payload = &data[info.payload_offset..];
+
+        if payload.len() >= 8
+            && payload[0] == 0xAA
+            && payload[1] == 0xAA
+            && payload[2] == 0x03
+        {
+            let ether_type = u16::from_be_bytes([payload[6], payload[7]]);
+            return ether_type == 0x888E;
+        }
+
+        false
+    }
 }
