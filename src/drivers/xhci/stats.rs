e052059bf4658fc3f2b3e7215e92dc5da36f4dd1
A.Erik <ekisanon@proton.me>
2026-03-01 13:32:22 +0100

    drivers/xhci: update statistics
    Refactor controller stats tracking.

--- src/drivers/xhci/stats.rs
+++ src/drivers/xhci/stats.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,7 +14,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! xHCI statistics tracking.
+
 use core::sync::atomic::{AtomicU64, Ordering};
+
 #[derive(Default)]
 pub struct XhciStatistics {
     pub interrupts: AtomicU64,
