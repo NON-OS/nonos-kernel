8549e7378f5da66e4ab38a41f1f1491ed7fa0e43
A.Erik <ekisanon@proton.me>
2026-03-01 13:26:57 +0100

    drivers/virtio_net: update unit tests
    Adapt tests for refactored structure.

--- src/drivers/virtio_net/tests.rs
+++ src/drivers/virtio_net/tests.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,10 +14,14 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! VirtIO network driver tests.
+
 use super::*;
+
 #[cfg(test)]
 mod error_tests {
     use super::error::*;
+
     #[test]
     fn test_error_messages() {
         assert_eq!(VirtioNetError::PacketTooSmall.as_str(), "packet too small");
