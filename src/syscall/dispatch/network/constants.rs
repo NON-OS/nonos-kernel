005e2efe9e067579f796741d54fd6e7879c6da7f
A.Erik <ekisanon@proton.me>
2026-03-01 15:33:05 +0100

    Add network constants

---
+++ src/syscall/dispatch/network/constants.rs
@@ -0,0 +1,24 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub(super) const AF_UNIX: u64 = 1;
+pub(super) const AF_INET: u64 = 2;
+pub(super) const AF_INET6: u64 = 10;
+
+pub(super) const SOCK_STREAM: u64 = 1;
+pub(super) const SOCK_DGRAM: u64 = 2;
+
+pub(super) const MMSGHDR_SIZE: usize = 64;
