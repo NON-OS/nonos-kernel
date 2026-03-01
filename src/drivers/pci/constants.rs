3a85bc4277be059c43d5889d015847d4c9354d06
A.Erik <ekisanon@proton.me>
2026-03-01 13:19:20 +0100

    drivers/pci: update constants module
    Remove comments from PCI register offsets.

--- src/drivers/pci/constants.rs
+++ src/drivers/pci/constants.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,13 +14,17 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! PCI specification constants.
+
 pub const PCI_CONFIG_ADDRESS: u16 = 0xCF8;
 pub const PCI_CONFIG_DATA: u16 = 0xCFC;
+
 pub const PCI_MAX_BUS: u8 = 255;
 pub const PCI_MAX_DEVICE: u8 = 31;
 pub const PCI_MAX_FUNCTION: u8 = 7;
 pub const PCI_CONFIG_SPACE_SIZE: u16 = 256;
 pub const PCIE_CONFIG_SPACE_SIZE: u16 = 4096;
+
 pub const CFG_VENDOR_ID: u16 = 0x00;
 pub const CFG_DEVICE_ID: u16 = 0x02;
 pub const CFG_COMMAND: u16 = 0x04;
@@ -48,6 +52,7 @@ pub const CFG_INTERRUPT_LINE: u16 = 0x3C;
 pub const CFG_INTERRUPT_PIN: u16 = 0x3D;
 pub const CFG_MIN_GRANT: u16 = 0x3E;
 pub const CFG_MAX_LATENCY: u16 = 0x3F;
+
 pub const CFG_PRIMARY_BUS: u16 = 0x18;
 pub const CFG_SECONDARY_BUS: u16 = 0x19;
 pub const CFG_SUBORDINATE_BUS: u16 = 0x1A;
@@ -64,6 +69,7 @@ pub const CFG_PREFETCH_LIMIT_UPPER: u16 = 0x2C;
 pub const CFG_IO_BASE_UPPER: u16 = 0x30;
 pub const CFG_IO_LIMIT_UPPER: u16 = 0x32;
 pub const CFG_BRIDGE_CONTROL: u16 = 0x3E;
+
 pub const CMD_IO_SPACE: u16 = 1 << 0;
 pub const CMD_MEMORY_SPACE: u16 = 1 << 1;
 pub const CMD_BUS_MASTER: u16 = 1 << 2;
@@ -74,6 +80,7 @@ pub const CMD_PARITY_ERROR_RESPONSE: u16 = 1 << 6;
 pub const CMD_SERR_ENABLE: u16 = 1 << 8;
 pub const CMD_FAST_B2B_ENABLE: u16 = 1 << 9;
 pub const CMD_INTERRUPT_DISABLE: u16 = 1 << 10;
+
 pub const STS_INTERRUPT_STATUS: u16 = 1 << 3;
 pub const STS_CAPABILITIES_LIST: u16 = 1 << 4;
 pub const STS_66MHZ_CAPABLE: u16 = 1 << 5;
@@ -85,10 +92,12 @@ pub const STS_RECEIVED_TARGET_ABORT: u16 = 1 << 12;
 pub const STS_RECEIVED_MASTER_ABORT: u16 = 1 << 13;
 pub const STS_SIGNALED_SYSTEM_ERROR: u16 = 1 << 14;
 pub const STS_DETECTED_PARITY_ERROR: u16 = 1 << 15;
+
 pub const HDR_TYPE_STANDARD: u8 = 0x00;
 pub const HDR_TYPE_BRIDGE: u8 = 0x01;
 pub const HDR_TYPE_CARDBUS: u8 = 0x02;
 pub const HDR_TYPE_MULTIFUNCTION: u8 = 0x80;
+
 pub const BAR_TYPE_MASK: u32 = 0x01;
 pub const BAR_TYPE_MEMORY: u32 = 0x00;
 pub const BAR_TYPE_IO: u32 = 0x01;
@@ -98,6 +107,7 @@ pub const BAR_MEMORY_TYPE_64: u32 = 0x04;
 pub const BAR_MEMORY_PREFETCHABLE: u32 = 0x08;
 pub const BAR_MEMORY_ADDR_MASK: u32 = 0xFFFF_FFF0;
 pub const BAR_IO_ADDR_MASK: u32 = 0xFFFF_FFFC;
+
 pub const CAP_ID_PM: u8 = 0x01;
 pub const CAP_ID_AGP: u8 = 0x02;
 pub const CAP_ID_VPD: u8 = 0x03;
@@ -119,6 +129,7 @@ pub const CAP_ID_SATA: u8 = 0x12;
 pub const CAP_ID_AF: u8 = 0x13;
 pub const CAP_ID_EA: u8 = 0x14;
 pub const CAP_ID_FPB: u8 = 0x15;
+
 pub const PCIE_CAP_ID_NULL: u16 = 0x0000;
 pub const PCIE_CAP_ID_AER: u16 = 0x0001;
 pub const PCIE_CAP_ID_VC: u16 = 0x0002;
@@ -161,6 +172,7 @@ pub const PCIE_CAP_ID_LMR: u16 = 0x0027;
 pub const PCIE_CAP_ID_HIER_ID: u16 = 0x0028;
 pub const PCIE_CAP_ID_NPEM: u16 = 0x0029;
 pub const PCIE_CAP_ID_32GT: u16 = 0x002A;
+
 pub const MSI_CTRL_ENABLE: u16 = 1 << 0;
 pub const MSI_CTRL_MMC_MASK: u16 = 0x7 << 1;
 pub const MSI_CTRL_MME_MASK: u16 = 0x7 << 4;
@@ -168,6 +180,7 @@ pub const MSI_CTRL_64BIT: u16 = 1 << 7;
 pub const MSI_CTRL_PVM: u16 = 1 << 8;
 pub const MSI_CTRL_EXT_MSG_DATA: u16 = 1 << 9;
 pub const MSI_CTRL_EXT_MSG_DATA_CAP: u16 = 1 << 10;
+
 pub const MSIX_CTRL_ENABLE: u16 = 1 << 15;
 pub const MSIX_CTRL_FUNCTION_MASK: u16 = 1 << 14;
 pub const MSIX_CTRL_TABLE_SIZE_MASK: u16 = 0x07FF;
@@ -177,6 +190,7 @@ pub const MSIX_ENTRY_ADDR_HI: u32 = 4;
 pub const MSIX_ENTRY_DATA: u32 = 8;
 pub const MSIX_ENTRY_VECTOR_CTRL: u32 = 12;
 pub const MSIX_ENTRY_MASKED: u32 = 1 << 0;
+
 pub const PM_CAP_VER_MASK: u16 = 0x7;
 pub const PM_CAP_PME_CLOCK: u16 = 1 << 3;
 pub const PM_CAP_DSI: u16 = 1 << 5;
@@ -188,16 +202,19 @@ pub const PM_CAP_PME_D1: u16 = 1 << 12;
 pub const PM_CAP_PME_D2: u16 = 1 << 13;
 pub const PM_CAP_PME_D3_HOT: u16 = 1 << 14;
 pub const PM_CAP_PME_D3_COLD: u16 = 1 << 15;
+
 pub const PM_CTRL_STATE_MASK: u16 = 0x3;
 pub const PM_CTRL_NO_SOFT_RESET: u16 = 1 << 3;
 pub const PM_CTRL_PME_ENABLE: u16 = 1 << 8;
 pub const PM_CTRL_DATA_SEL_MASK: u16 = 0xF << 9;
 pub const PM_CTRL_DATA_SCALE_MASK: u16 = 0x3 << 13;
 pub const PM_CTRL_PME_STATUS: u16 = 1 << 15;
+
 pub const PM_STATE_D0: u16 = 0;
 pub const PM_STATE_D1: u16 = 1;
 pub const PM_STATE_D2: u16 = 2;
 pub const PM_STATE_D3_HOT: u16 = 3;
+
 pub const PCIE_TYPE_ENDPOINT: u8 = 0x0;
 pub const PCIE_TYPE_LEGACY_ENDPOINT: u8 = 0x1;
 pub const PCIE_TYPE_ROOT_PORT: u8 = 0x4;
@@ -207,12 +224,14 @@ pub const PCIE_TYPE_PCIE_TO_PCI_BRIDGE: u8 = 0x7;
 pub const PCIE_TYPE_PCI_TO_PCIE_BRIDGE: u8 = 0x8;
 pub const PCIE_TYPE_ROOT_COMPLEX_ENDPOINT: u8 = 0x9;
 pub const PCIE_TYPE_ROOT_COMPLEX_EVENT_COLLECTOR: u8 = 0xA;
+
 pub const PCIE_LINK_SPEED_2_5GT: u8 = 0x1;
 pub const PCIE_LINK_SPEED_5GT: u8 = 0x2;
 pub const PCIE_LINK_SPEED_8GT: u8 = 0x3;
 pub const PCIE_LINK_SPEED_16GT: u8 = 0x4;
 pub const PCIE_LINK_SPEED_32GT: u8 = 0x5;
 pub const PCIE_LINK_SPEED_64GT: u8 = 0x6;
+
 pub const PCIE_LINK_WIDTH_X1: u8 = 0x01;
 pub const PCIE_LINK_WIDTH_X2: u8 = 0x02;
 pub const PCIE_LINK_WIDTH_X4: u8 = 0x04;
@@ -220,6 +239,7 @@ pub const PCIE_LINK_WIDTH_X8: u8 = 0x08;
 pub const PCIE_LINK_WIDTH_X12: u8 = 0x0C;
 pub const PCIE_LINK_WIDTH_X16: u8 = 0x10;
 pub const PCIE_LINK_WIDTH_X32: u8 = 0x20;
+
 pub const CLASS_UNCLASSIFIED: u8 = 0x00;
 pub const CLASS_MASS_STORAGE: u8 = 0x01;
 pub const CLASS_NETWORK: u8 = 0x02;
@@ -242,6 +262,7 @@ pub const CLASS_PROCESSING_ACCELERATOR: u8 = 0x12;
 pub const CLASS_NON_ESSENTIAL: u8 = 0x13;
 pub const CLASS_COPROCESSOR: u8 = 0x40;
 pub const CLASS_UNASSIGNED: u8 = 0xFF;
+
 pub const SUBCLASS_STORAGE_SCSI: u8 = 0x00;
 pub const SUBCLASS_STORAGE_IDE: u8 = 0x01;
 pub const SUBCLASS_STORAGE_FLOPPY: u8 = 0x02;
@@ -253,6 +274,7 @@ pub const SUBCLASS_STORAGE_SAS: u8 = 0x07;
 pub const SUBCLASS_STORAGE_NVM: u8 = 0x08;
 pub const SUBCLASS_STORAGE_UFS: u8 = 0x09;
 pub const SUBCLASS_STORAGE_OTHER: u8 = 0x80;
+
 pub const SUBCLASS_NETWORK_ETHERNET: u8 = 0x00;
 pub const SUBCLASS_NETWORK_TOKEN_RING: u8 = 0x01;
 pub const SUBCLASS_NETWORK_FDDI: u8 = 0x02;
@@ -263,10 +285,12 @@ pub const SUBCLASS_NETWORK_PICMG: u8 = 0x06;
 pub const SUBCLASS_NETWORK_INFINIBAND: u8 = 0x07;
 pub const SUBCLASS_NETWORK_FABRIC: u8 = 0x08;
 pub const SUBCLASS_NETWORK_OTHER: u8 = 0x80;
+
 pub const SUBCLASS_DISPLAY_VGA: u8 = 0x00;
 pub const SUBCLASS_DISPLAY_XGA: u8 = 0x01;
 pub const SUBCLASS_DISPLAY_3D: u8 = 0x02;
 pub const SUBCLASS_DISPLAY_OTHER: u8 = 0x80;
+
 pub const SUBCLASS_SERIAL_FIREWIRE: u8 = 0x00;
 pub const SUBCLASS_SERIAL_ACCESS_BUS: u8 = 0x01;
 pub const SUBCLASS_SERIAL_SSA: u8 = 0x02;
@@ -278,21 +302,27 @@ pub const SUBCLASS_SERIAL_IPMI: u8 = 0x07;
 pub const SUBCLASS_SERIAL_SERCOS: u8 = 0x08;
 pub const SUBCLASS_SERIAL_CANBUS: u8 = 0x09;
 pub const SUBCLASS_SERIAL_OTHER: u8 = 0x80;
+
 pub const PROGIF_OHCI: u8 = 0x10;
 pub const PROGIF_EHCI: u8 = 0x20;
 pub const PROGIF_XHCI: u8 = 0x30;
 pub const PROGIF_UHCI: u8 = 0x00;
 pub const PROGIF_USB4: u8 = 0x40;
 pub const PROGIF_DEVICE: u8 = 0xFE;
+
 pub const PROGIF_NVME: u8 = 0x02;
 pub const PROGIF_NVME_ADMIN: u8 = 0x03;
+
 pub const PROGIF_AHCI: u8 = 0x01;
 pub const PROGIF_AHCI_RAID: u8 = 0x04;
+
 pub const MAX_BAR_SIZE: u64 = 256 * 1024 * 1024 * 1024;
 pub const MIN_MMIO_ADDRESS: u64 = 0x100000;
 pub const MAX_PHYSICAL_ADDRESS: u64 = 0x0000_FFFF_FFFF_FFFF;
+
 pub const MSI_ADDRESS_BASE: u32 = 0xFEE0_0000;
 pub const MSI_ADDRESS_DEST_ID_SHIFT: u32 = 12;
+
 pub const MSI_DATA_VECTOR_MASK: u32 = 0xFF;
 pub const MSI_DATA_DELIVERY_FIXED: u32 = 0x000;
 pub const MSI_DATA_DELIVERY_LOWEST: u32 = 0x100;
@@ -300,6 +330,7 @@ pub const MSI_DATA_TRIGGER_EDGE: u32 = 0x000;
 pub const MSI_DATA_TRIGGER_LEVEL: u32 = 0x8000;
 pub const MSI_DATA_LEVEL_ASSERT: u32 = 0x4000;
 pub const MSI_DATA_LEVEL_DEASSERT: u32 = 0x0000;
+
 pub const BRIDGE_CTL_PARITY_ERROR_RESPONSE: u16 = 1 << 0;
 pub const BRIDGE_CTL_SERR_ENABLE: u16 = 1 << 1;
 pub const BRIDGE_CTL_ISA_ENABLE: u16 = 1 << 2;
@@ -312,6 +343,7 @@ pub const BRIDGE_CTL_PRIMARY_DISCARD_TIMER: u16 = 1 << 8;
 pub const BRIDGE_CTL_SECONDARY_DISCARD_TIMER: u16 = 1 << 9;
 pub const BRIDGE_CTL_DISCARD_TIMER_STATUS: u16 = 1 << 10;
 pub const BRIDGE_CTL_DISCARD_TIMER_SERR_ENABLE: u16 = 1 << 11;
+
 pub const VENDOR_INTEL: u16 = 0x8086;
 pub const VENDOR_AMD: u16 = 0x1022;
 pub const VENDOR_NVIDIA: u16 = 0x10DE;
@@ -320,6 +352,7 @@ pub const VENDOR_VIRTIO: u16 = 0x1AF4;
 pub const VENDOR_REALTEK: u16 = 0x10EC;
 pub const VENDOR_BROADCOM: u16 = 0x14E4;
 pub const VENDOR_RED_HAT: u16 = 0x1B36;
+
 #[inline]
 pub const fn pci_config_address(bus: u8, device: u8, function: u8, offset: u8) -> u32 {
     (1u32 << 31)
