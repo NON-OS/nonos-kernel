5fd95fda968d8b1acdaa97b2ffcbd0571fc3eab9
A.Erik <ekisanon@proton.me>
2026-03-01 08:05:07 +0100

    drivers/keyboard: update constants module
    Remove comments from scancode and LED constants.

--- src/drivers/keyboard/constants.rs
+++ src/drivers/keyboard/constants.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,31 +14,58 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! PS/2 keyboard constants.
+
 pub const KBD_DATA: u16 = 0x60;
+
 pub const KBD_STATUS: u16 = 0x64;
+
 pub const KBD_CMD: u16 = 0x64;
+
 pub const CMD_READ_CFG: u8 = 0x20;
+
 pub const CMD_WRITE_CFG: u8 = 0x60;
+
 pub const CMD_DISABLE_PORT1: u8 = 0xAD;
+
 pub const CMD_ENABLE_PORT1: u8 = 0xAE;
+
 pub const CMD_SELF_TEST: u8 = 0xAA;
+
 pub const KBD_ENABLE_SCANNING: u8 = 0xF4;
+
 pub const KBD_DISABLE_SCANNING: u8 = 0xF5;
+
 pub const KBD_SET_LEDS: u8 = 0xED;
+
 pub const KBD_RESET: u8 = 0xFF;
+
 pub const KBD_ACK: u8 = 0xFA;
+
 pub const KBD_RESEND: u8 = 0xFE;
+
 pub const STATUS_OUTPUT_FULL: u8 = 0x01;
+
 pub const STATUS_INPUT_FULL: u8 = 0x02;
+
 pub const STATUS_SYSTEM_FLAG: u8 = 0x04;
+
 pub const STATUS_CMD_DATA: u8 = 0x08;
+
 pub const STATUS_KEYBOARD_LOCK: u8 = 0x10;
+
 pub const STATUS_TIMEOUT: u8 = 0x40;
+
 pub const STATUS_PARITY: u8 = 0x80;
+
 pub const KBD_VECTOR: u8 = 0x21;
+
 pub const SC_EXT_E0: u8 = 0xE0;
+
 pub const SC_EXT_E1: u8 = 0xE1;
+
 pub const SC_BREAK_BIT: u8 = 0x80;
+
 pub const NORMAL: [Option<u8>; 0x60] = {
     let mut t: [Option<u8>; 0x60] = [None; 0x60];
     t[0x02] = Some(b'1');
@@ -152,26 +179,49 @@ pub const SHIFTED: [Option<u8>; 0x60] = {
 };
 
 pub const SC_LSHIFT: u8 = 0x2A;
+
 pub const SC_RSHIFT: u8 = 0x36;
+
 pub const SC_LCTRL: u8 = 0x1D;
+
 pub const SC_LALT: u8 = 0x38;
+
 pub const SC_CAPSLOCK: u8 = 0x3A;
+
 pub const SC_NUMLOCK: u8 = 0x45;
+
 pub const SC_SCROLLLOCK: u8 = 0x46;
+
 pub const SC_EXT_UP: u8 = 0x48;
+
 pub const SC_EXT_DOWN: u8 = 0x50;
+
 pub const SC_EXT_LEFT: u8 = 0x4B;
+
 pub const SC_EXT_RIGHT: u8 = 0x4D;
+
 pub const SC_EXT_HOME: u8 = 0x47;
+
 pub const SC_EXT_END: u8 = 0x4F;
+
 pub const SC_EXT_PGUP: u8 = 0x49;
+
 pub const SC_EXT_PGDN: u8 = 0x51;
+
 pub const SC_EXT_INSERT: u8 = 0x52;
+
 pub const SC_EXT_DELETE: u8 = 0x53;
+
 pub const LED_SCROLL_LOCK: u8 = 0b001;
+
 pub const LED_NUM_LOCK: u8 = 0b010;
+
 pub const LED_CAPS_LOCK: u8 = 0b100;
+
 pub const CHAR_RING_SIZE: usize = 1024;
+
 pub const EVT_RING_SIZE: usize = 64;
-pub const KBD_MAX_INTERRUPTS_PER_SEC: u64 = 1000; /// Maximum keyboard interrupts per second (protects against malfunctioning hardware).
+
+pub const KBD_MAX_INTERRUPTS_PER_SEC: u64 = 1000;
+
 pub const KBD_RATE_LIMIT_WINDOW_US: u64 = 1_000_000;
