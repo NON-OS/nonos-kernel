534c885ed058c800db1287382eee0fffb74840c3
A.Erik <ekisanon@proton.me>
2026-03-01 13:23:43 +0100

    drivers/usb: update constants module
    Remove comments from USB class codes.

--- src/drivers/usb/constants.rs
+++ src/drivers/usb/constants.rs
@@ -1,5 +1,5 @@
-// NØNOS Operating System
-// Copyright (C) 2026 NØNOS Contributors
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+//! USB protocol constants.
+
 pub const REQ_GET_STATUS: u8 = 0x00;
 pub const REQ_CLEAR_FEATURE: u8 = 0x01;
 pub const REQ_SET_FEATURE: u8 = 0x03;
@@ -25,15 +27,19 @@ pub const REQ_SET_CONFIGURATION: u8 = 0x09;
 pub const REQ_GET_INTERFACE: u8 = 0x0A;
 pub const REQ_SET_INTERFACE: u8 = 0x0B;
 pub const REQ_SYNCH_FRAME: u8 = 0x0C;
+
 pub const RT_DEV: u8 = 0x00;
 pub const RT_INTF: u8 = 0x01;
 pub const RT_EP: u8 = 0x02;
 pub const RT_OTHER: u8 = 0x03;
+
 pub const DIR_OUT: u8 = 0x00;
 pub const DIR_IN: u8 = 0x80;
+
 pub const TYPE_STD: u8 = 0x00 << 5;
 pub const TYPE_CLASS: u8 = 0x01 << 5;
 pub const TYPE_VENDOR: u8 = 0x02 << 5;
+
 pub const DT_DEVICE: u8 = 1;
 pub const DT_CONFIG: u8 = 2;
 pub const DT_STRING: u8 = 3;
@@ -49,20 +55,24 @@ pub const DT_BOS: u8 = 15;
 pub const DT_DEVICE_CAPABILITY: u8 = 16;
 pub const DT_SS_EP_COMPANION: u8 = 48;
 pub const DT_SSP_ISOCH_EP_COMPANION: u8 = 49;
+
 pub const EP_TRANSFER_TYPE_MASK: u8 = 0x03;
 pub const EP_TYPE_CONTROL: u8 = 0x00;
 pub const EP_TYPE_ISOCHRONOUS: u8 = 0x01;
 pub const EP_TYPE_BULK: u8 = 0x02;
 pub const EP_TYPE_INTERRUPT: u8 = 0x03;
+
 pub const EP_SYNC_TYPE_MASK: u8 = 0x0C;
 pub const EP_SYNC_NONE: u8 = 0x00;
 pub const EP_SYNC_ASYNC: u8 = 0x04;
 pub const EP_SYNC_ADAPTIVE: u8 = 0x08;
 pub const EP_SYNC_SYNC: u8 = 0x0C;
+
 pub const EP_USAGE_TYPE_MASK: u8 = 0x30;
 pub const EP_USAGE_DATA: u8 = 0x00;
 pub const EP_USAGE_FEEDBACK: u8 = 0x10;
 pub const EP_USAGE_IMPLICIT_FB: u8 = 0x20;
+
 pub const CLASS_DEVICE: u8 = 0x00;
 pub const CLASS_AUDIO: u8 = 0x01;
 pub const CLASS_CDC: u8 = 0x02;
@@ -85,12 +95,16 @@ pub const CLASS_WIRELESS: u8 = 0xE0;
 pub const CLASS_MISC: u8 = 0xEF;
 pub const CLASS_APPLICATION: u8 = 0xFE;
 pub const CLASS_VENDOR: u8 = 0xFF;
+
 pub const FEATURE_ENDPOINT_HALT: u16 = 0;
 pub const FEATURE_DEVICE_REMOTE_WAKEUP: u16 = 1;
 pub const FEATURE_TEST_MODE: u16 = 2;
+
 pub const DEFAULT_CONTROL_TIMEOUT_US: u32 = 5_000_000;
 pub const DEFAULT_BULK_TIMEOUT_US: u32 = 5_000_000;
 pub const DEFAULT_INTERRUPT_TIMEOUT_US: u32 = 1_000_000;
+
 pub const USB2_MAX_CONTROL_PACKET: u16 = 64;
 pub const USB3_MAX_CONTROL_PACKET: u16 = 512;
+
 pub const DEFAULT_LANG_ID: u16 = 0x0409;
