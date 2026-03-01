b290b660ca593007bd425a220f2dfe20bd9567c0
A.Erik <ekisanon@proton.me>
2026-03-01 14:10:01 +0100

    Implement token verification functions

---
+++ src/capabilities/token/verify.rs
@@ -0,0 +1,35 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::capabilities::bits::caps_to_bits;
+
+use super::material::{mac64, token_material};
+use super::signing_key::signing_key;
+use super::types::CapabilityToken;
+
+pub fn verify_token(tok: &CapabilityToken) -> bool {
+    let Some(key) = signing_key() else {
+        return false;
+    };
+
+    let mat = token_material(
+        tok.owner_module,
+        caps_to_bits(&tok.permissions),
+        tok.expires_at_ms.unwrap_or(0),
+        tok.nonce,
+    );
+    mac64(key, &mat) == tok.signature
+}
