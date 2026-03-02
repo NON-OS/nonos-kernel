af81ccea11c9116efd09a423beeb2056a0548167
A.Erik <ekisanon@proton.me>
2026-03-02 12:56:30 +0100

    block: module exports

---
+++ src/storage/block/mod.rs
@@ -0,0 +1,28 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub mod device;
+pub mod types;
+
+pub use device::{
+    init, register_device, unregister_device, get_device, device_count,
+    find_device, is_init, list_devices,
+};
+
+pub use types::{
+    BLOCK_SIZE, MAX_BLOCK_DEVICES, BlockDeviceType, BlockDevice,
+    BlockError, BlockResult, BlockOps,
+};
