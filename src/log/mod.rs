0038a95c560034fc89003e7d98da349088fb8467
A.Erik <ekisanon@proton.me>
2026-03-02 15:30:26 +0100

    log: restructure module with subdirectory organization

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -1,92 +1,51 @@
-//! NØNOS Logging Subsystem
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-pub mod nonos_logger;
+pub mod types;
+pub mod backend;
+pub mod manager;
+mod helpers;
+mod macros;
+mod compat;
 
-// Re-export for backward compatibility  
-pub use nonos_logger as simple_logger;
-
-pub use nonos_logger::{
-    Severity,
-    init as init_logger,
-    log, enter_panic_mode, log_critical
+pub use types::{Severity, LogEntry};
+pub use backend::{LogBackend, VgaBackend, RamBufferBackend, RAM_BUF_SIZE};
+pub use manager::{
+    LogManager, LOGGER, PANIC_MODE,
+    init, log, enter_panic_mode, log_critical, try_get_logger,
+    get_log_entries, get_recent_logs, log_entry_count, clear_log_buffer,
 };
+pub use helpers::{debug_simple, info_simple, warn_simple, log_error_simple};
 
+pub use init as init_logger;
 
-// Logging functions
-
-
-pub fn debug(msg: &str) {
-    log(Severity::Debug, msg);
-}
-
-pub fn log_err(msg: &str) {
-    log(Severity::Err, msg);
-}
-
-
-// Security logging macro
-#[macro_export]
-macro_rules! security_log {
-    ($($arg:tt)*) => {
-        $crate::log::log(crate::log::Severity::Fatal, &alloc::format!($($arg)*));
-    };
-}
-
-/// Debug logging macro
-#[macro_export]
-macro_rules! log_debug {
-    ($($arg:tt)*) => {
-        $crate::log::debug(&alloc::format!($($arg)*));
-    };
-}
-
-/// Warning logging macro
-#[macro_export]
-macro_rules! warn {
-    ($($arg:tt)*) => {
-        $crate::log::log(crate::log::Severity::Warn, &alloc::format!($($arg)*));
-    };
-}
-
-/// Error logging macro
-#[macro_export]
-macro_rules! error {
-    ($($arg:tt)*) => {
-        $crate::log::log(crate::log::Severity::Err, &alloc::format!($($arg)*));
-    };
-}
-
-/// Warning logging function macro
-#[macro_export]
-macro_rules! log_warning {
-    ($($arg:tt)*) => {
-        $crate::log::log(crate::log::Severity::Warn, &alloc::format!($($arg)*));
-    };
-}
-
-
-// Re-export the macros
-pub use {security_log, log_debug, error, log_warning};
-// Re-export macros from crate root that are exported by #[macro_export]
 pub use crate::info;
+pub use crate::log_info;
+pub use crate::log_warn;
+pub use crate::log_err;
+pub use crate::log_dbg;
+pub use crate::log_fatal;
+pub use crate::log_error;
+pub use crate::log_debug;
+pub use crate::log_warning;
+pub use crate::debug;
+pub use crate::error;
+pub use crate::security_log;
+pub use crate::warn;
 
-// Make logger submodule that re-exports macros for compatibility
-pub mod logger {
-    pub use crate::{log_warn, log_err, log_dbg, log_fatal, log_info};
-    pub use super::{log, error, log_warning, enter_panic_mode, log_critical}; // The logging function and macros
-    
-    // Re-export with alt names for compatibility
-    pub use crate::log_err as log_error;
-    pub use crate::info;
-    pub use super::nonos_logger::try_get_logger;
-    pub use super::init_logger as init;
-    
-    // Create log macro for compatibility
-    #[macro_export]
-    macro_rules! logger_log {
-        ($msg:expr) => {
-            $crate::log::log($crate::log::Severity::Info, $msg);
-        };
-    }
-    pub use logger_log as log;
-}
+pub use compat::logger;
+pub use compat::nonos_logger;
+pub use compat::simple_logger;
