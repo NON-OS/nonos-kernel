27a4f350dcf1bd3add25fb7ac6a78c975bcec5e3
A.Erik <ekisanon@proton.me>
2026-03-02 22:03:47 +0100

    shell: Add module exports

---
+++ src/shell/mod.rs
@@ -0,0 +1,26 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub mod commands;
+pub mod editor;
+pub mod output;
+pub mod terminal;
+mod init;
+
+pub use terminal::*;
+pub use commands::*;
+pub use output::{print_line as unified_print, enable_gui_output, disable_gui_output};
+pub use init::init;
