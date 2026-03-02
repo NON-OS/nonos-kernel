062dc44ab72da70cd33db339f154999cc6a1dee6
A.Erik <ekisanon@proton.me>
2026-03-02 22:05:09 +0100

    shell/editor/input: Add module exports

---
+++ src/shell/editor/input/mod.rs
@@ -0,0 +1,27 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod types;
+mod dispatch;
+mod normal;
+mod normal_ops;
+mod operators;
+mod insert;
+mod visual;
+mod command;
+
+pub use self::types::{Key, InputResult};
+pub use self::dispatch::handle_input;
