5685875bf55c617da6224f8130c249b3a706b42d
A.Erik <ekisanon@proton.me>
2026-03-02 15:02:27 +0100

    sched: update module structure and exports

--- src/sched/mod.rs
+++ src/sched/mod.rs
@@ -1,45 +1,41 @@
-//! NØNOS Scheduler Subsystem 
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 pub mod task;
 pub mod runqueue;
 pub mod context;
 pub mod executor;
 pub mod realtime;
-pub mod nonos_scheduler;
+pub mod scheduler;
+mod api;
 
-// Re-export primary scheduler APIs
-pub use nonos_scheduler::{
-    init,
-    get,
-    spawn,
-    run,
-    tick,
-    wakeup,
-    enter,
+pub use scheduler::{
+    init, get, spawn, run, tick, wakeup, enter,
+    add_to_run_queue, remove_from_run_queue, is_in_run_queue,
+    runnable_process_count, get_runnable_pids, get_scheduler_stats,
+    SchedulerStatsSnapshot, sleep_until, wake_process, is_sleeping,
+    get_remaining_sleep, yield_now,
 };
 
-pub fn current_scheduler() -> Option<&'static nonos_scheduler::Scheduler> {
-    get()
-}
-
-pub fn yield_cpu() {
-    // Yield CPU to next task
-    tick();
-}
-
-pub fn current_cpu_id() -> u32 {
-    // Get current CPU ID from architecture-specific code
-    crate::arch::x86_64::cpu::current_cpu_id() as u32
-}
-
+pub use api::{current_scheduler, yield_cpu, schedule, current_cpu_id};
 pub use task::{Task, Priority, CpuAffinity};
 pub use runqueue::RunQueue;
 pub use context::Context;
 pub use executor::{spawn_async, poll_async_tasks, pending_async_tasks};
 pub use realtime::{
-    init as realtime_init,
-    spawn_realtime,
-    run_realtime_tasks,
-    pending_realtime_tasks,
-    has_realtime_tasks,
+    init as realtime_init, spawn_realtime, run_realtime_tasks,
+    pending_realtime_tasks, has_realtime_tasks,
 };
