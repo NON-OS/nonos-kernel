7b0386294a6cf149dc666c47d5e7e90fba39b857
A.Erik <ekisanon@proton.me>
2026-03-02 11:45:24 +0100

    Authentication constants

---
+++ src/modules/auth/constants.rs
@@ -0,0 +1,23 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub const ED25519_SIGNATURE_SIZE: usize = 64;
+pub const ED25519_PUBKEY_SIZE: usize = 32;
+pub const BLAKE3_HASH_SIZE: usize = 32;
+pub const DILITHIUM_SIGNATURE_SIZE: usize = 2420;
+pub const DILITHIUM_PUBKEY_SIZE: usize = 1312;
+pub const MAX_ATTESTATION_CHAIN_DEPTH: usize = 8;
+pub const AUTH_VERSION: u16 = 1;
