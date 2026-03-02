a2b99180d5e7f4aaf27f0b612ccbabf9c358d3f6
A.Erik <ekisanon@proton.me>
2026-03-02 12:57:53 +0100

    nvme: module structure

---
+++ src/storage/nvme/mod.rs
@@ -0,0 +1,26 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub mod driver;
+pub mod state;
+pub mod types;
+
+pub use driver::*;
+pub use state::{
+    init, scan_and_register_nvme_devices, get_controllers, get_namespaces,
+    has_nvme_hardware, get_stats, get_total_capacity,
+};
+pub use types::*;
