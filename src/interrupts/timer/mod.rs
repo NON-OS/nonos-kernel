55d4543d3e1b8c0d4bb8d0e27a25d94c6bb6d934
A.Erik <ekisanon@proton.me>
2026-03-02 13:40:41 +0100

    interrupts/timer: module exports

---
+++ src/interrupts/timer/mod.rs
@@ -0,0 +1,23 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod hooks;
+mod state;
+mod tick;
+
+pub use hooks::{clear_tick_hook, init, set_tick_hook, TickHook};
+pub use state::{get_ticks as tick_count, reset_ticks, TICK_COUNT};
+pub use tick::{on_timer_interrupt, tick};
