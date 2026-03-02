5508483fd21019c6b9e6eb90e7b62a1baccdccf4
A.Erik <ekisanon@proton.me>
2026-03-02 22:06:57 +0100

    shell/commands/cryptography: Add module exports

---
+++ src/shell/commands/cryptography/mod.rs
@@ -0,0 +1,28 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod hash;
+mod random;
+mod genkey;
+mod status;
+mod hmac;
+mod util;
+
+pub use hash::cmd_hash;
+pub use random::cmd_random;
+pub use genkey::cmd_genkey;
+pub use status::cmd_crypto_status;
+pub use hmac::cmd_hmac;
