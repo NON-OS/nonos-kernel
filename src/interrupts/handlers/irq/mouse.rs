7c6c49fc1e7820d495b2142ff994f26b8fb5a0d1
A.Erik <ekisanon@proton.me>
2026-03-02 13:42:36 +0100

    interrupts/handlers: mouse irq

---
+++ src/interrupts/handlers/irq/mouse.rs
@@ -0,0 +1,37 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::interrupts::apic;
+use crate::interrupts::pic;
+use crate::interrupts::stats;
+
+const MOUSE_IRQ_LINE: u8 = 12;
+
+pub fn handle() {
+    crate::input::mouse::handle_interrupt();
+
+    stats::increment_mouse();
+
+    send_eoi();
+}
+
+fn send_eoi() {
+    if apic::is_enabled() {
+        apic::send_eoi();
+    } else {
+        pic::send_eoi(MOUSE_IRQ_LINE);
+    }
+}
