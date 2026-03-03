f7a7eb06e5694b6327281d0e3ebdf299692f84eb
A. Erik <ekisanon@proton.me>
2026-03-03 16:14:28 +0100

    Update runtime module structure

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,4 +1,21 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 pub mod nonos_capsule;
+pub mod nonos_capsule_store;
 pub mod nonos_isolation;
 pub mod nonos_zerostate;
 pub mod nonos_supervisor;
@@ -6,11 +23,10 @@ pub mod nonos_service;
 pub mod nonos_stats;
 pub mod nonos_runtime_task;
 
-// Re-exports for compatibility
 pub use nonos_capsule as capsule;
+pub use nonos_capsule_store as capsule_store;
 pub use nonos_isolation as isolation;
 pub use nonos_zerostate as zerostate;
-
 pub use nonos_supervisor as supervisor;
 pub use nonos_service as service;
 pub use nonos_stats as stats;
