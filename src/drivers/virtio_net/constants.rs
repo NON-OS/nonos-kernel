5afdba8c2e4c8933707d539473f4c74ac5d65154
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:04 +0100

    Refactor VirtIO net constants

--- src/drivers/virtio_net/constants.rs
+++ src/drivers/virtio_net/constants.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! VirtIO network driver constants.
 
 pub const MIN_ETHERNET_FRAME: usize = 60;
 pub const MAX_MTU: usize = 1500;
