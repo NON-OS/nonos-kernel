ab15255cc5d754a574fd4c8628d50e9d07d507ee
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:37 +0100

    Refactor xHCI public API

--- src/drivers/xhci/api.rs
+++ src/drivers/xhci/api.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! xHCI public API.
 
 use super::controller::{get_controller, XHCI_CONTROLLER};
 use super::dma::DmaRegion;
