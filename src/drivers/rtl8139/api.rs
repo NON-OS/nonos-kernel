83de94f582a4eac0e2addbdcd3019089f3c8ffa4
A. Erik <ekisanon@proton.me>
2026-03-03 15:57:52 +0100

    Refactor RTL8139 public API

--- src/drivers/rtl8139/api.rs
+++ src/drivers/rtl8139/api.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-extern crate alloc;
-
 use alloc::sync::Arc;
 use core::sync::atomic::{AtomicBool, Ordering};
 use spin::Mutex;
