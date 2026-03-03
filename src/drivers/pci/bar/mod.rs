3c53cc8770e6bd35e22f68aba6e92c5b55525ca0
A. Erik <ekisanon@proton.me>
2026-03-03 16:05:09 +0100

    Add PCI BAR module structure

---
+++ src/drivers/pci/bar/mod.rs
@@ -0,0 +1,22 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod decode;
+mod info;
+mod validation;
+
+pub use decode::{decode_all_bars, decode_all_bars_unchecked, decode_bar, decode_bar_unchecked};
+pub use info::{bar_type, calculate_bar_alignment, enumerate_bars, is_bar_address_valid, BarInfo};
