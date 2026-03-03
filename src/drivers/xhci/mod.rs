a0f890c3ea93e3cfacb407a492ab4b45954d46d4
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:44 +0100

    Update xHCI module structure

--- src/drivers/xhci/mod.rs
+++ src/drivers/xhci/mod.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! xHCI USB 3.0 host controller driver.
-
 mod api;
 pub mod constants;
 pub mod controller;
