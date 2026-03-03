03f213f9d7081a4ef573430a0f7de9346c88ceba
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:27 +0100

    Refactor HD Audio CORB/RIRB handling

--- src/drivers/audio/controller/corb_rirb.rs
+++ src/drivers/audio/controller/corb_rirb.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! CORB/RIRB command ring buffer and immediate command interface.
 
 use core::ptr;
 use core::sync::atomic::Ordering;
