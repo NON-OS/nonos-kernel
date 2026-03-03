a39e6cdbd6ff977ccc8289f6859f06b79c7132c4
A. Erik <ekisanon@proton.me>
2026-03-03 16:57:57 +0100

    Wire up SOCKS module

---
+++ src/network/socks/mod.rs
@@ -0,0 +1,22 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod client;
+mod server;
+mod protocol;
+
+pub use client::{SocksError, SocksConnection, connect, connect_target, send, recv, close, format_connection_info, error_message};
+pub(crate) use server::{start_socks_server, stop_socks_server};
