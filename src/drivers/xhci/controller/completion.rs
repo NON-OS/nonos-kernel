b48d6b5af9b19c065675aec3fd8eb468f84d315a
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:37 +0100

    Refactor xHCI completion handling

--- src/drivers/xhci/controller/completion.rs
+++ src/drivers/xhci/controller/completion.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! xHCI command and transfer completion handling.
 
 use core::sync::atomic::Ordering;
 
