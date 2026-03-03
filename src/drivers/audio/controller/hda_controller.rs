1c9ea370b2f9e9852ebddc0e9a0e03b6d4b31279
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:28 +0100

    Refactor HD Audio controller implementation

--- src/drivers/audio/controller/hda_controller.rs
+++ src/drivers/audio/controller/hda_controller.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! HD Audio controller driver.
 
 use core::ptr;
 use core::sync::atomic::{AtomicU64, AtomicU8, AtomicBool, Ordering};
