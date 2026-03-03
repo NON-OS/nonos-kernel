4a3a524f77c8a8db57c1b94415967fe757092a2a
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:24 +0100

    Refactor WiFi smoltcp bridge

--- src/drivers/wifi/smol_bridge.rs
+++ src/drivers/wifi/smol_bridge.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! WiFi network stack bridge for smoltcp integration.
 
 extern crate alloc;
 
