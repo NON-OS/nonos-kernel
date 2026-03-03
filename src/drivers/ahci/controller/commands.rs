34f7e39bd7dc3cff693b97aec06305f2ef423807
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:08 +0100

    Refactor AHCI command handling implementation

--- src/drivers/ahci/controller/commands.rs
+++ src/drivers/ahci/controller/commands.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! AHCI command building functions.
 
 use spin::Mutex;
 use alloc::collections::BTreeMap;
