fd020ca922404c068c5e635f0f1927b1023db652
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:37 +0100

    Refactor WiFi transmit path

--- src/drivers/wifi/tx.rs
+++ src/drivers/wifi/tx.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! WiFi transmit path.
 
 
 pub(super) const IEEE80211_FTYPE_DATA: u16 = 0x0008;
