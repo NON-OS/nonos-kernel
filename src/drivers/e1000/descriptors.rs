3412c34004641e2f4c4a344dead2e68395135902
A. Erik <ekisanon@proton.me>
2026-03-03 15:56:09 +0100

    Refactor E1000 descriptor types

--- src/drivers/e1000/descriptors.rs
+++ src/drivers/e1000/descriptors.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! E1000 descriptor structures.
 
 #[repr(C, align(16))]
 #[derive(Clone, Copy, Default)]
