d45343be2a61efa22bd56e204e98360f7ab8c094
A. Erik <ekisanon@proton.me>
2026-03-03 16:05:31 +0100

    Add PCI capability constants

---
+++ src/drivers/pci/constants/capabilities.rs
@@ -0,0 +1,80 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub const CAP_ID_PM: u8 = 0x01;
+pub const CAP_ID_AGP: u8 = 0x02;
+pub const CAP_ID_VPD: u8 = 0x03;
+pub const CAP_ID_SLOT_ID: u8 = 0x04;
+pub const CAP_ID_MSI: u8 = 0x05;
+pub const CAP_ID_CHSWP: u8 = 0x06;
+pub const CAP_ID_PCIX: u8 = 0x07;
+pub const CAP_ID_HT: u8 = 0x08;
+pub const CAP_ID_VNDR: u8 = 0x09;
+pub const CAP_ID_DBG: u8 = 0x0A;
+pub const CAP_ID_CCRC: u8 = 0x0B;
+pub const CAP_ID_SHPC: u8 = 0x0C;
+pub const CAP_ID_SSVID: u8 = 0x0D;
+pub const CAP_ID_AGP3: u8 = 0x0E;
+pub const CAP_ID_SECDEV: u8 = 0x0F;
+pub const CAP_ID_PCIE: u8 = 0x10;
+pub const CAP_ID_MSIX: u8 = 0x11;
+pub const CAP_ID_SATA: u8 = 0x12;
+pub const CAP_ID_AF: u8 = 0x13;
+pub const CAP_ID_EA: u8 = 0x14;
+pub const CAP_ID_FPB: u8 = 0x15;
+
+pub const PCIE_CAP_ID_NULL: u16 = 0x0000;
+pub const PCIE_CAP_ID_AER: u16 = 0x0001;
+pub const PCIE_CAP_ID_VC: u16 = 0x0002;
+pub const PCIE_CAP_ID_DSN: u16 = 0x0003;
+pub const PCIE_CAP_ID_PWR: u16 = 0x0004;
+pub const PCIE_CAP_ID_RCLD: u16 = 0x0005;
+pub const PCIE_CAP_ID_RCILC: u16 = 0x0006;
+pub const PCIE_CAP_ID_RCEC: u16 = 0x0007;
+pub const PCIE_CAP_ID_MFVC: u16 = 0x0008;
+pub const PCIE_CAP_ID_VC9: u16 = 0x0009;
+pub const PCIE_CAP_ID_RCRB: u16 = 0x000A;
+pub const PCIE_CAP_ID_VNDR: u16 = 0x000B;
+pub const PCIE_CAP_ID_CAC: u16 = 0x000C;
+pub const PCIE_CAP_ID_ACS: u16 = 0x000D;
+pub const PCIE_CAP_ID_ARI: u16 = 0x000E;
+pub const PCIE_CAP_ID_ATS: u16 = 0x000F;
+pub const PCIE_CAP_ID_SRIOV: u16 = 0x0010;
+pub const PCIE_CAP_ID_MRIOV: u16 = 0x0011;
+pub const PCIE_CAP_ID_MCAST: u16 = 0x0012;
+pub const PCIE_CAP_ID_PRI: u16 = 0x0013;
+pub const PCIE_CAP_ID_REBAR: u16 = 0x0015;
+pub const PCIE_CAP_ID_DPA: u16 = 0x0016;
+pub const PCIE_CAP_ID_TPH: u16 = 0x0017;
+pub const PCIE_CAP_ID_LTR: u16 = 0x0018;
+pub const PCIE_CAP_ID_SECPCI: u16 = 0x0019;
+pub const PCIE_CAP_ID_PMUX: u16 = 0x001A;
+pub const PCIE_CAP_ID_PASID: u16 = 0x001B;
+pub const PCIE_CAP_ID_LNR: u16 = 0x001C;
+pub const PCIE_CAP_ID_DPC: u16 = 0x001D;
+pub const PCIE_CAP_ID_L1SS: u16 = 0x001E;
+pub const PCIE_CAP_ID_PTM: u16 = 0x001F;
+pub const PCIE_CAP_ID_MPCIE: u16 = 0x0020;
+pub const PCIE_CAP_ID_FRS: u16 = 0x0021;
+pub const PCIE_CAP_ID_RTR: u16 = 0x0022;
+pub const PCIE_CAP_ID_DVSEC: u16 = 0x0023;
+pub const PCIE_CAP_ID_VF_REBAR: u16 = 0x0024;
+pub const PCIE_CAP_ID_DLNK: u16 = 0x0025;
+pub const PCIE_CAP_ID_16GT: u16 = 0x0026;
+pub const PCIE_CAP_ID_LMR: u16 = 0x0027;
+pub const PCIE_CAP_ID_HIER_ID: u16 = 0x0028;
+pub const PCIE_CAP_ID_NPEM: u16 = 0x0029;
+pub const PCIE_CAP_ID_32GT: u16 = 0x002A;
