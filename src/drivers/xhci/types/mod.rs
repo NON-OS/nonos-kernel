332fc3ff3c2c8a0b9c4f8b0971b20c5316a51644
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:44 +0100

    Update xHCI types module structure

--- src/drivers/xhci/types/mod.rs
+++ src/drivers/xhci/types/mod.rs
@@ -22,6 +22,9 @@ mod erst;
 mod input_context;
 mod slot;
 
+#[cfg(test)]
+mod tests;
+
 pub use config::XhciConfig;
 pub use descriptors::UsbDeviceDescriptor;
 pub use device_context::DeviceContext;
@@ -29,52 +32,3 @@ pub use endpoint::{EpContext, EpContextWithPad, EpState};
 pub use erst::ErstEntry;
 pub use input_context::{InputContext, InputControlContext};
 pub use slot::{SlotContext, SlotState};
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use core::mem;
-
-    #[test]
-    fn test_slot_context_size() {
-        assert_eq!(mem::size_of::<SlotContext>(), 32);
-    }
-
-    #[test]
-    fn test_ep_context_size() {
-        assert_eq!(mem::size_of::<EpContext>(), 32);
-    }
-
-    #[test]
-    fn test_device_context_alignment() {
-        assert_eq!(mem::align_of::<DeviceContext>(), 64);
-    }
-
-    #[test]
-    fn test_slot_context_fields() {
-        let mut slot = SlotContext::new();
-        slot.set_speed(4);
-        assert_eq!(slot.speed(), 4);
-
-        slot.set_root_hub_port(3);
-        assert_eq!(slot.root_hub_port(), 3);
-    }
-
-    #[test]
-    fn test_ep_context_dequeue_pointer() {
-        let mut ep = EpContext::new();
-        ep.set_tr_dequeue_pointer(0x1000_0010, true);
-        assert_eq!(ep.tr_dequeue_pointer(), 0x1000_0010);
-        assert!(ep.dcs());
-    }
-
-    #[test]
-    fn test_ep_addr_to_dci() {
-        assert_eq!(DeviceContext::ep_addr_to_dci(0x00), 1);
-        assert_eq!(DeviceContext::ep_addr_to_dci(0x80), 1);
-        assert_eq!(DeviceContext::ep_addr_to_dci(0x01), 2);
-        assert_eq!(DeviceContext::ep_addr_to_dci(0x81), 3);
-        assert_eq!(DeviceContext::ep_addr_to_dci(0x02), 4);
-        assert_eq!(DeviceContext::ep_addr_to_dci(0x82), 5);
-    }
-}
