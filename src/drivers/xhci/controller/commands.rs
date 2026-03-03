0285269ab38dbc6ea451dd58ffe4b70e6a1c0c19
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:37 +0100

    Refactor xHCI command handling

--- src/drivers/xhci/controller/commands.rs
+++ src/drivers/xhci/controller/commands.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! xHCI command operations.
 
 use core::ptr;
 
