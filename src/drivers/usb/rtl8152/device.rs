a6e9bcd515dec9e2962e11353a92058cb0791ce3
A. Erik <ekisanon@proton.me>
2026-03-03 15:58:30 +0100

    Refactor RTL8152 USB device

--- src/drivers/usb/rtl8152/device.rs
+++ src/drivers/usb/rtl8152/device.rs
@@ -1,7 +1,18 @@
 // NONOS Operating System
 // Copyright (C) 2026 NONOS Contributors
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 extern crate alloc;
 
 use alloc::vec::Vec;
@@ -151,11 +162,16 @@ impl Rtl8152Device {
 
         while offset + 4 <= data.len() {
             let pkt_len = u16::from_le_bytes([data[offset], data[offset + 1]]) as usize;
-            let _flags = u16::from_le_bytes([data[offset + 2], data[offset + 3]]);
+            let flags = u16::from_le_bytes([data[offset + 2], data[offset + 3]]);
 
             offset += 4;
 
             if pkt_len == 0 || pkt_len > 1518 {
+                break;
+            }
+
+            let has_error = (flags & 0x8000) != 0;
+            if has_error {
                 break;
             }
 
