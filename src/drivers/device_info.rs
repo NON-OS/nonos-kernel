af36384417d4a7006ea7477780dc6310ac22c305
A. Erik <ekisanon@proton.me>
2026-03-03 15:56:09 +0100

    Refactor device information types

--- src/drivers/device_info.rs
+++ src/drivers/device_info.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Device enumeration and information.
 
 use alloc::vec::Vec;
 
