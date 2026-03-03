df27ff78b11160a79902bdbad2003f83f3fe8ee6
A. Erik <ekisanon@proton.me>
2026-03-03 15:56:28 +0100

    Refactor keyboard constants

--- src/drivers/keyboard/constants.rs
+++ src/drivers/keyboard/constants.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! PS/2 keyboard constants.
-
 pub const KBD_DATA: u16 = 0x60;
 
 pub const KBD_STATUS: u16 = 0x64;
