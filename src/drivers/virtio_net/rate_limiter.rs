6ac322c71f8f2f6724659f54ce96ae487b51da60
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:10 +0100

    Refactor VirtIO net rate limiter

--- src/drivers/virtio_net/rate_limiter.rs
+++ src/drivers/virtio_net/rate_limiter.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Rate limiter for network traffic.
 
 use super::constants::RATE_LIMIT_WINDOW_MS;
 use super::error::VirtioNetError;
