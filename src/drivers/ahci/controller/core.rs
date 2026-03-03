8e49b19831515fce86ebbf0f9189d724623668ef
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:15 +0100

    Refactor AHCI controller core logic

--- src/drivers/ahci/controller/core.rs
+++ src/drivers/ahci/controller/core.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! AHCI controller core implementation.
 
 use alloc::{collections::BTreeMap, string::String, vec::Vec};
 use core::sync::atomic::{AtomicBool, AtomicU32, AtomicU64, Ordering};
