d6e18dfe6e10b7828a733c4352d59c03332891f3
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:15 +0100

    Refactor AHCI controller helper functions

--- src/drivers/ahci/controller/helpers.rs
+++ src/drivers/ahci/controller/helpers.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Low-level MMIO register access and utility functions.
 
 use x86_64::VirtAddr;
 use crate::memory::mmio::{mmio_r32, mmio_w32};
