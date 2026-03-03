9dc4ecdf990d609ad9d02271bd48a68f751a3c5b
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:15 +0100

    Refactor AHCI encryption support

--- src/drivers/ahci/controller/encryption.rs
+++ src/drivers/ahci/controller/encryption.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! AES-256-CTR encryption for data-at-rest.
 
 use spin::Mutex;
 use crate::crypto::aes::Aes256;
