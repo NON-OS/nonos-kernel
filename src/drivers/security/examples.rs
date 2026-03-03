c5f9398dfa67ada71f070b35b71a2ad61812fe6a
A. Erik <ekisanon@proton.me>
2026-03-03 15:58:00 +0100

    Refactor security usage examples

--- src/drivers/security/examples.rs
+++ src/drivers/security/examples.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Example usage of the driver security module
 
 use x86_64::{PhysAddr, VirtAddr};
 
