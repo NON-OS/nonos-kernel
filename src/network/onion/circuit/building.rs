7f4318100f18c19fa3f0c160e50eacd5ff6910c5
A. Erik <ekisanon@proton.me>
2026-03-03 17:01:53 +0100

    Implement circuit building

---
+++ src/network/onion/circuit/building.rs
@@ -0,0 +1,42 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+use alloc::vec::Vec;
+use crate::network::onion::directory::RelayDescriptor;
+use crate::network::onion::crypto::HopCrypto;
+use super::types::CircuitId;
+
+#[derive(Debug)]
+pub(super) struct BuildingCircuit {
+    pub id: CircuitId,
+    pub target_hops: Vec<RelayDescriptor>,
+    pub current_hop: usize,
+    pub state: BuildState,
+    pub crypto_state: Vec<HopCrypto>,
+    pub start_time: u64,
+    pub timeout_ms: u64,
+}
+
+#[derive(Debug, PartialEq)]
+pub(super) enum BuildState {
+    SendingCreate,
+    WaitingCreated,
+    SendingExtend(usize),
+    WaitingExtended(usize),
+    Complete,
+    Failed,
+}
