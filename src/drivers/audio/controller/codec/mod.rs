461f502789c0ef1638732c898202a944cbdcea0d
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:27 +0100

    Update HD Audio codec module exports

--- src/drivers/audio/controller/codec/mod.rs
+++ src/drivers/audio/controller/codec/mod.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! HD Audio codec discovery and path configuration.
-
 mod constants;
 pub(crate) mod control;
 pub(crate) mod discovery;
