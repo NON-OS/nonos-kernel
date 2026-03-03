1a9873f14b35c980a5ea8b9680fc5eeeec67d98e
A. Erik <ekisanon@proton.me>
2026-03-03 15:56:39 +0100

    Update network module structure

--- src/drivers/network/mod.rs
+++ src/drivers/network/mod.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Network driver abstraction layer.
-
 extern crate alloc;
 
 pub mod error;
