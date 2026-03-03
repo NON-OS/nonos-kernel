b482581805a597cd93255c6afa41d7a6bc37a585
A. Erik <ekisanon@proton.me>
2026-03-03 15:56:29 +0100

    Refactor keyboard I/O operations

--- src/drivers/keyboard/io.rs
+++ src/drivers/keyboard/io.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! PS/2 keyboard I/O operations.
 
 use super::constants::*;
 use core::hint::spin_loop;
