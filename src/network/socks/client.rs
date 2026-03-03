f1ee6266ae62bac2eee5b0c4dcf886be7e11b5a8
A. Erik <ekisanon@proton.me>
2026-03-03 16:57:49 +0100

    Implement SOCKS5 client

---
+++ src/network/socks/client.rs
@@ -0,0 +1,131 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum SocksError {
+    ConnectionFailed,
+    AuthFailed,
+    TargetUnreachable,
+    Timeout,
+    ProtocolError,
+    SendFailed,
+    RecvFailed,
+}
+
+pub struct SocksConnection {
+    socket_handle: u32,
+    connected: bool,
+}
+
+pub fn connect(host: &str, port: u16, timeout_ms: u32) -> Result<SocksConnection, SocksError> {
+    use crate::network::tcp;
+
+    let socket = tcp::connect_to(host, port, timeout_ms)
+        .map_err(|_| SocksError::ConnectionFailed)?;
+
+    let handshake = [0x05, 0x01, 0x00];
+    tcp::send_socket(socket, &handshake)
+        .map_err(|_| SocksError::SendFailed)?;
+
+    let mut response = [0u8; 2];
+    tcp::recv_socket(socket, &mut response, timeout_ms)
+        .map_err(|_| SocksError::RecvFailed)?;
+
+    if response[0] != 0x05 || response[1] != 0x00 {
+        return Err(SocksError::AuthFailed);
+    }
+
+    Ok(SocksConnection {
+        socket_handle: socket,
+        connected: true,
+    })
+}
+
+pub fn connect_target(
+    conn: &SocksConnection,
+    host: &str,
+    port: u16,
+    timeout_ms: u32,
+) -> Result<(), SocksError> {
+    use crate::network::tcp;
+
+    let mut request = Vec::with_capacity(7 + host.len());
+    request.push(0x05);
+    request.push(0x01);
+    request.push(0x00);
+    request.push(0x03);
+    request.push(host.len() as u8);
+    request.extend_from_slice(host.as_bytes());
+    request.push((port >> 8) as u8);
+    request.push((port & 0xFF) as u8);
+
+    tcp::send_socket(conn.socket_handle, &request)
+        .map_err(|_| SocksError::SendFailed)?;
+
+    let mut response = [0u8; 10];
+    tcp::recv_socket(conn.socket_handle, &mut response, timeout_ms)
+        .map_err(|_| SocksError::RecvFailed)?;
+
+    if response[0] != 0x05 || response[1] != 0x00 {
+        return Err(SocksError::TargetUnreachable);
+    }
+
+    Ok(())
+}
+
+pub fn send(conn: &SocksConnection, data: &[u8]) -> Result<(), SocksError> {
+    use crate::network::tcp;
+
+    tcp::send_socket(conn.socket_handle, data)
+        .map_err(|_| SocksError::SendFailed)
+}
+
+pub fn recv(conn: &SocksConnection, timeout_ms: u32) -> Result<Vec<u8>, SocksError> {
+    use crate::network::tcp;
+
+    let mut buffer = alloc::vec![0u8; 65536];
+    let len = tcp::recv_socket_available(conn.socket_handle, &mut buffer, timeout_ms)
+        .map_err(|_| SocksError::RecvFailed)?;
+
+    buffer.truncate(len);
+    Ok(buffer)
+}
+
+pub fn close(conn: SocksConnection) {
+    use crate::network::tcp;
+    tcp::close_socket(conn.socket_handle);
+}
+
+pub fn format_connection_info(conn: &SocksConnection) -> String {
+    alloc::format!("SOCKS5[handle={}, connected={}]", conn.socket_handle, conn.connected)
+}
+
+pub fn error_message(err: SocksError) -> String {
+    match err {
+        SocksError::ConnectionFailed => "Connection failed".into(),
+        SocksError::AuthFailed => "Authentication failed".into(),
+        SocksError::TargetUnreachable => "Target unreachable".into(),
+        SocksError::Timeout => "Connection timed out".into(),
+        SocksError::ProtocolError => "Protocol error".into(),
+        SocksError::SendFailed => "Send failed".into(),
+        SocksError::RecvFailed => "Receive failed".into(),
+    }
+}
