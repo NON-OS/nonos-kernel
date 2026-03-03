bd7efab821d520f01106ab446d4ea08b1c0ae69a
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:24 +0100

    Refactor WiFi register definitions

--- src/drivers/wifi/regs.rs
+++ src/drivers/wifi/regs.rs
@@ -14,11 +14,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Intel WiFi hardware register access.
 
 use x86_64::VirtAddr;
 
-pub(super) struct WifiRegs {
+pub(crate) struct WifiRegs {
     base: VirtAddr,
 }
 
