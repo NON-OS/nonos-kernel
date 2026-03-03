1bbf2d97c75c9b3f9f1bc90855e6f5a1a8335b5f
A. Erik <ekisanon@proton.me>
2026-03-03 15:56:29 +0100

    Refactor keyboard buffer implementation

--- src/drivers/keyboard_buffer.rs
+++ src/drivers/keyboard_buffer.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Keyboard input buffer.
 
 use alloc::collections::VecDeque;
 use spin::Mutex;
