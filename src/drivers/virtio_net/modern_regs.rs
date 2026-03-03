93c1861fdca1c2deace238f27215abe622b93906
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:05 +0100

    Refactor VirtIO modern register access

--- src/drivers/virtio_net/modern_regs.rs
+++ src/drivers/virtio_net/modern_regs.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! VirtIO PCI modern register access.
 
 use core::ptr;
 use crate::drivers::pci::{pci_read_config32, PciBar, PciDevice};
