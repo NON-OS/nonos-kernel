3d8ee263b28737b47c89d6d80945b8961e60937b
A. Erik <ekisanon@proton.me>
2026-03-03 15:58:29 +0100

    Refactor USB device abstraction

--- src/drivers/usb/device.rs
+++ src/drivers/usb/device.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! USB device representation.
 
 use super::descriptors::{
     DeviceDescriptor, UsbStringTable, UsbConfiguration, UsbInterfaceInfo,
