c7cec48e7b4e210403806a88a30ad7794af5b49b
A. Erik <ekisanon@proton.me>
2026-03-03 16:07:02 +0100

    Add USB descriptors module structure

---
+++ src/drivers/usb/descriptors/mod.rs
@@ -0,0 +1,29 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod config;
+mod device;
+mod endpoint;
+mod interface;
+mod parse;
+mod strings;
+
+pub use config::{ConfigDescriptorHeader, UsbConfiguration};
+pub use device::DeviceDescriptor;
+pub use endpoint::EndpointDescriptor;
+pub use interface::{InterfaceDescriptor, UsbInterfaceInfo};
+pub use parse::parse_interfaces;
+pub use strings::UsbStringTable;
