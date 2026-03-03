35d61105ed39a7cc6c5e4599312f9d974f8d6e72
A. Erik <ekisanon@proton.me>
2026-03-03 15:56:29 +0100

    Refactor keyboard ring buffer

--- src/drivers/keyboard/ring.rs
+++ src/drivers/keyboard/ring.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Lock-free ring buffers.
 
 use super::event::KeyEvent;
 use core::sync::atomic::{AtomicUsize, Ordering};
