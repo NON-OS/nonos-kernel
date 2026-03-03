175e0344873a468ba84f1c1f97d434afa76a2805
A. Erik <ekisanon@proton.me>
2026-03-03 17:02:17 +0100

    Add crypto constants

---
+++ src/network/onion/crypto/constants.rs
@@ -0,0 +1,34 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+pub const TAP_C_HANDSHAKE_LEN: usize = 186;
+
+pub const TAP_S_HANDSHAKE_LEN: usize = 148;
+
+pub const NTOR_ONIONSKIN_LEN: usize = 84;
+
+pub const NTOR_REPLY_LEN: usize = 64;
+
+pub const CELL_PAYLOAD_SIZE: usize = 509;
+
+pub const RELAY_PAYLOAD_SIZE: usize = 498;
+
+pub const KEY_LEN: usize = 16;
+
+pub const IV_LEN: usize = 16;
+
+pub const DIGEST_LEN: usize = 4;
