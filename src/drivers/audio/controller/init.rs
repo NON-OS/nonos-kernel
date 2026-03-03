c56b5af817ace2e25b52d46ba97bfd7085626cc1
A. Erik <ekisanon@proton.me>
2026-03-03 15:53:28 +0100

    Refactor HD Audio initialization

--- src/drivers/audio/controller/init.rs
+++ src/drivers/audio/controller/init.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! HD Audio controller initialization.
-
 use super::super::error::AudioError;
 use super::super::types::DmaRegion;
 use super::super::constants::*;
