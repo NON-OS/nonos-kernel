2fab148ffada23bb7b1c37f3afb9a910f0d83241
A. Erik <ekisanon@proton.me>
2026-03-03 15:58:48 +0100

    Refactor VGA buffer handling

--- src/drivers/vga/buffer.rs
+++ src/drivers/vga/buffer.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! VGA text buffer.
-
 use super::color::{decode_color, vga_color, Color, DEFAULT_COLOR};
 use super::constants::*;
 use super::io;
