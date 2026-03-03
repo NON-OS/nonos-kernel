cd6ed3b35bd4528e5862477947297aba8d499a00
A. Erik <ekisanon@proton.me>
2026-03-03 15:59:44 +0100

    Refactor xHCI DMA operations

--- src/drivers/xhci/dma.rs
+++ src/drivers/xhci/dma.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! DMA memory management for xHCI driver.
 
 use core::ptr;
 use x86_64::{PhysAddr, VirtAddr};
