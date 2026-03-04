3bead65b8283c727b60b0070936cb445dae4c416
A. Erik <ekisanon@proton.me>
2026-03-04 17:25:55 +0100

    add color definitions

---
+++ src/graphics/framebuffer/colors.rs
@@ -0,0 +1,70 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub const COLOR_BG: u32 = 0xFF080C12;
+pub const COLOR_BG_GRADIENT_TOP: u32 = 0xFF0A1018;
+pub const COLOR_BG_GRADIENT_BOTTOM: u32 = 0xFF050810;
+
+pub const COLOR_PANEL: u32 = 0xFF111820;
+pub const COLOR_PANEL_HOVER: u32 = 0xFF1A2230;
+pub const COLOR_PANEL_ACTIVE: u32 = 0xFF0D1218;
+pub const COLOR_PANEL_BORDER: u32 = 0xFF1E2832;
+
+pub const COLOR_ACCENT: u32 = 0xFF00D4FF;
+pub const COLOR_ACCENT_DIM: u32 = 0xFF0099BB;
+pub const COLOR_ACCENT_GLOW: u32 = 0x4000D4FF;
+
+pub const COLOR_SECONDARY: u32 = 0xFFFFB800;
+pub const COLOR_SECONDARY_DIM: u32 = 0xFFCC9400;
+
+pub const COLOR_TEXT: u32 = 0xFF00D4FF;
+pub const COLOR_TEXT_WHITE: u32 = 0xFFF0F6FC;
+pub const COLOR_TEXT_DIM: u32 = 0xFF6E7A88;
+pub const COLOR_TEXT_MUTED: u32 = 0xFF484F58;
+
+pub const COLOR_TERMINAL_BG: u32 = 0xFF0D1117;
+pub const COLOR_TERMINAL_BORDER: u32 = 0xFF21262D;
+
+pub const COLOR_GREEN: u32 = 0xFF00FF88;
+pub const COLOR_RED: u32 = 0xFFFF4466;
+pub const COLOR_YELLOW: u32 = 0xFFFFB800;
+pub const COLOR_ORANGE: u32 = 0xFFFF8844;
+pub const COLOR_PURPLE: u32 = 0xFFAA66FF;
+
+pub const COLOR_SUCCESS: u32 = 0xFF00FF88;
+pub const COLOR_ERROR: u32 = 0xFFFF4466;
+pub const COLOR_WARNING: u32 = 0xFFFFB800;
+pub const COLOR_INFO: u32 = 0xFF00D4FF;
+
+pub const COLOR_CURSOR: u32 = 0xFF00D4FF;
+pub const COLOR_SELECTION: u32 = 0x4000D4FF;
+
+pub const COLOR_GRID: u32 = 0xFF0F151C;
+pub const COLOR_GRID_ACCENT: u32 = 0xFF141C24;
+pub const COLOR_GLOW_SOFT: u32 = 0x1800D4FF;
+
+pub const COLOR_FG: u32 = COLOR_TEXT;
+pub const COLOR_WHITE: u32 = 0xFFFFFFFF;
+pub const COLOR_BLACK: u32 = 0xFF000000;
+pub const COLOR_BLUE: u32 = COLOR_ACCENT;
+pub const COLOR_GRAY: u32 = 0xFF808080;
+pub const COLOR_DARK_GRAY: u32 = 0xFF404040;
+pub const COLOR_LIGHT_GRAY: u32 = 0xFFC0C0C0;
+pub const COLOR_MENU_BG: u32 = COLOR_PANEL;
+pub const COLOR_DOCK_BG: u32 = COLOR_PANEL;
+pub const COLOR_WINDOW_BG: u32 = COLOR_PANEL;
+pub const COLOR_TITLE_BG: u32 = COLOR_PANEL_ACTIVE;
+pub const COLOR_TITLE_FG: u32 = COLOR_TEXT_WHITE;
