3cca72730a55ff2da1568831e53e794b8fe86035
eKisNonos <ekisanon@proton.me>
2026-03-10 14:00:16 +0100

    tls: fix whitespace

--- src/network/onion/tls/types.rs
+++ src/network/onion/tls/types.rs
@@ -34,7 +34,7 @@ pub(super) enum ContentType {
 pub(super) enum HSType {
     ClientHello = 1,
     ServerHello = 2,
-    EncryptedExtensions = 8,
+    _EncryptedExtensions = 8,
     Certificate = 11,
     CertificateVerify = 15,
     Finished = 20,
