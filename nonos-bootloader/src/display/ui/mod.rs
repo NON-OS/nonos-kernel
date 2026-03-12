02fdc515a168377d5548511cd9e0190a61d90484
eKisNonos <ekisanon@proton.me>
2026-03-12 18:41:59 +0100

    ui module

---
+++ nonos-bootloader/src/display/ui/mod.rs
@@ -0,0 +1,25 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod boxes;
+mod crypto_boxes;
+mod progress;
+mod types;
+
+pub use boxes::{draw_hash_box, draw_stage_box};
+pub use crypto_boxes::{draw_signature_box, draw_zk_box};
+pub use progress::draw_progress_bar;
+pub use types::StageStatus;
