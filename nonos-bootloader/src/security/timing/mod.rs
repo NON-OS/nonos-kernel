b6a5547895264f8a80c1eadea4f6884246df3748
eKisNonos <ekisanon@proton.me>
2026-03-12 18:49:43 +0100

    mod

---
+++ nonos-bootloader/src/security/timing/mod.rs
@@ -0,0 +1,22 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod protect;
+
+pub use protect::{
+    add_fixed_delay, add_random_delay, constant_time_compare, constant_time_eq_u8,
+    constant_time_is_zero, constant_time_select, init_jitter, TimingGuard,
+};
