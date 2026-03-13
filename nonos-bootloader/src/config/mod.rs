80b790f7eb4548539cd4a594d1497584a8de2b00
eKisNonos <ekisanon@proton.me>
2026-03-13 13:26:33 +0100

    Update config mod.rs with policy modules

--- nonos-bootloader/src/config/mod.rs
+++ nonos-bootloader/src/config/mod.rs
@@ -14,14 +14,33 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+mod apply;
+mod apply_hardware;
+mod apply_network;
+mod apply_policy;
+mod apply_security;
+mod boot_method;
 mod config;
-mod enums;
-mod nvram;
-mod types;
+mod fallback;
+mod graphics_mode;
+mod memory_mode;
+mod network_policy;
+pub mod nvram;
+mod policy_network;
+mod policy_security;
+mod policy_system;
+mod security_policy;
+pub mod types;
+mod verification_level;
 
+pub use apply::apply_configuration;
 pub use types::{
     BootloaderConfig, FallbackBehavior, GraphicsMode, MemoryManagementMode, NetworkPolicy,
     PreferredBootMethod, SecurityPolicy, VerificationLevel,
 };
 
 pub use nvram::{load_bootloader_config, save_configuration};
+
+pub use apply_policy::{
+    apply_hardware_settings, apply_memory_settings, apply_network_policy, apply_security_policy,
+};
