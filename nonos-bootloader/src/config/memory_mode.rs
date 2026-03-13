d4573fcfb94b83fea9dd1997b9daa23b459ade4d
eKisNonos <ekisanon@proton.me>
2026-03-13 13:29:55 +0100

    Add memory management mode configuration

---
+++ nonos-bootloader/src/config/memory_mode.rs
@@ -0,0 +1,38 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MemoryManagementMode {
+    Efficient,
+    Secure,
+    Legacy,
+}
+
+impl MemoryManagementMode {
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            Self::Efficient => "EFFICIENT",
+            Self::Secure => "SECURE",
+            Self::Legacy => "LEGACY",
+        }
+    }
+}
+
+impl Default for MemoryManagementMode {
+    fn default() -> Self {
+        Self::Secure
+    }
+}
