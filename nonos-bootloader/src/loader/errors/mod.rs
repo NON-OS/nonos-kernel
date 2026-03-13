f4ba239d54a81c350587ff6f8fbdeb60eef6eb31
eKisNonos <ekisanon@proton.me>
2026-03-13 13:26:19 +0100

    Update errors mod.rs to use split modules

--- nonos-bootloader/src/loader/errors/mod.rs
+++ nonos-bootloader/src/loader/errors/mod.rs
@@ -14,6 +14,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+mod classify;
+mod display;
+mod error;
+
 pub mod types;
 
-pub use types::{LoaderError, LoaderResult};
+pub use error::{LoaderError, LoaderResult};
