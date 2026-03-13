86234ce29c17c8634f3f2cb97615f796de089984
eKisNonos <ekisanon@proton.me>
2026-03-13 13:36:42 +0100

    Update file module exports

--- nonos-bootloader/src/loader/file/mod.rs
+++ nonos-bootloader/src/loader/file/mod.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-pub mod file;
+mod load;
+mod read;
+mod types;
 
-pub use file::{file_exists, load_file_from_esp, load_kernel_binary, FileLoadError, FileResult};
+pub use load::{file_exists, load_file_from_esp, load_kernel_binary};
+pub use types::{FileLoadError, FileResult};
