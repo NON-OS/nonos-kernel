81f7a946c87e92198743e3019b4937f09ffc8c57
eKisNonos <ekisanon@proton.me>
2026-03-15 18:45:02 +0100

    Clean directory module

--- src/network/onion/directory/mod.rs
+++ src/network/onion/directory/mod.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Directory service for Tor network consensus and relay information
 
 mod authorities;
 mod consensus;
