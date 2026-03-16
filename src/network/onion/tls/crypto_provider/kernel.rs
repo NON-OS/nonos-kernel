d1b66b48a7717ff5abf90e496e3150e22c4a23c7
eKisNonos <ekisanon@proton.me>
2026-03-16 13:36:24 +0100

    Modularize TLS cryptographic provider interface

--- src/network/onion/tls/crypto_provider/kernel.rs
+++ src/network/onion/tls/crypto_provider/kernel.rs
@@ -14,42 +14,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-
 use alloc::vec::Vec;
-use spin::Once;
 use crate::network::onion::OnionError;
-use super::types::CipherSuite;
-
-pub trait TlsCrypto: Sync + Send {
-    fn random(&self, out32: &mut [u8; 32]) -> Result<(), OnionError>;
-    fn sha256(&self, data: &[u8], out32: &mut [u8; 32]);
-    fn hmac_sha256(&self, key: &[u8], data: &[u8], out32: &mut [u8; 32]);
-    fn hkdf_extract(&self, salt: &[u8; 32], ikm: &[u8; 32], out32: &mut [u8; 32]);
-    fn hkdf_expand(&self, prk: &[u8; 32], info: &[u8], out: &mut [u8]);
-    fn x25519_keypair(&self) -> Result<([u8; 32], [u8; 32]), OnionError>;
-    fn x25519(&self, sk: &[u8; 32], pk: &[u8; 32]) -> Result<[u8; 32], OnionError>;
-    fn aead_seal(&self, suite: CipherSuite, key: &[u8], nonce: &[u8; 12], aad: &[u8], plaintext: &[u8]) -> Result<Vec<u8>, OnionError>;
-    fn aead_open(&self, suite: CipherSuite, key: &[u8], nonce: &[u8; 12], aad: &[u8], ciphertext: &[u8]) -> Result<Vec<u8>, OnionError>;
-    fn verify_ed25519(&self, pubkey: &[u8], msg: &[u8], sig: &[u8]) -> bool;
-    fn verify_rsa_pss_sha256(&self, spki_der: &[u8], msg: &[u8], sig: &[u8]) -> bool;
-    fn verify_ecdsa_p256_sha256(&self, spki_der: &[u8], msg: &[u8], sig: &[u8]) -> bool;
-}
-
-static TLS_CRYPTO: Once<&'static dyn TlsCrypto> = Once::new();
-
-pub fn init_tls_crypto(provider: &'static dyn TlsCrypto) {
-    TLS_CRYPTO.call_once(|| provider);
-}
-
-#[inline]
-pub(super) fn crypto() -> &'static dyn TlsCrypto {
-    TLS_CRYPTO.call_once(|| &KERNEL_TLS_CRYPTO as &'static dyn TlsCrypto);
-    *TLS_CRYPTO.get().unwrap()
-}
-
-pub fn is_tls_crypto_initialized() -> bool {
-    TLS_CRYPTO.get().is_some()
-}
+use super::super::types::CipherSuite;
+use super::traits::TlsCrypto;
 
 pub struct KernelTlsCrypto;
 pub static KERNEL_TLS_CRYPTO: KernelTlsCrypto = KernelTlsCrypto;
@@ -91,26 +59,16 @@ impl TlsCrypto for KernelTlsCrypto {
 
     fn aead_seal(&self, suite: CipherSuite, key: &[u8], nonce: &[u8; 12], aad: &[u8], plaintext: &[u8]) -> Result<Vec<u8>, OnionError> {
         match suite {
-            CipherSuite::TlsAes128GcmSha256 => {
-                crate::network::onion::nonos_crypto::tls_aes128_gcm_seal(key, nonce, aad, plaintext)
-            }
-            CipherSuite::TlsChacha20Poly1305Sha256 => {
-                crate::network::onion::nonos_crypto::tls_chacha20poly1305_seal(key, nonce, aad, plaintext)
-            }
-        }
-        .map_err(|_| OnionError::CryptoError)
+            CipherSuite::TlsAes128GcmSha256 => crate::network::onion::nonos_crypto::tls_aes128_gcm_seal(key, nonce, aad, plaintext),
+            CipherSuite::TlsChacha20Poly1305Sha256 => crate::network::onion::nonos_crypto::tls_chacha20poly1305_seal(key, nonce, aad, plaintext),
+        }.map_err(|_| OnionError::CryptoError)
     }
 
     fn aead_open(&self, suite: CipherSuite, key: &[u8], nonce: &[u8; 12], aad: &[u8], ciphertext: &[u8]) -> Result<Vec<u8>, OnionError> {
         match suite {
-            CipherSuite::TlsAes128GcmSha256 => {
-                crate::network::onion::nonos_crypto::tls_aes128_gcm_open(key, nonce, aad, ciphertext)
-            }
-            CipherSuite::TlsChacha20Poly1305Sha256 => {
-                crate::network::onion::nonos_crypto::tls_chacha20poly1305_open(key, nonce, aad, ciphertext)
-            }
-        }
-        .map_err(|_| OnionError::CryptoError)
+            CipherSuite::TlsAes128GcmSha256 => crate::network::onion::nonos_crypto::tls_aes128_gcm_open(key, nonce, aad, ciphertext),
+            CipherSuite::TlsChacha20Poly1305Sha256 => crate::network::onion::nonos_crypto::tls_chacha20poly1305_open(key, nonce, aad, ciphertext),
+        }.map_err(|_| OnionError::CryptoError)
     }
 
     fn verify_ed25519(&self, pubkey: &[u8], msg: &[u8], sig: &[u8]) -> bool {
