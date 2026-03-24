8aa39322b3163d9df1ad31ce329e0815742a25ef
eKisNonos <ekisanon@proton.me>
2026-03-24 13:41:32 +0100

    Restructure browser types into modules

---
+++ src/apps/ecosystem/browser/engine/types/document.rs
@@ -0,0 +1,46 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::vec::Vec;
+use super::form::{Link, Form};
+use super::image::Image;
+
+#[derive(Debug, Clone)]
+pub struct Document {
+    pub title: String,
+    pub root: Node,
+    pub links: Vec<Link>,
+    pub forms: Vec<Form>,
+    pub images: Vec<Image>,
+    pub hidden_classes: Vec<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub node_type: NodeType,
+    pub children: Vec<Node>,
+    pub attributes: Vec<(String, String)>,
+}
+
+#[derive(Debug, Clone)]
+pub enum NodeType {
+    Element(String),
+    Text(String),
+    Comment(String),
+}
