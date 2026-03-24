e757e9e9e8550970cd7c4655bc45be246bd67d49
eKisNonos <ekisanon@proton.me>
2026-03-24 15:29:08 +0100

    Add staking UI rendering

---
+++ src/graphics/window/apps/wallet/staking/render.rs
@@ -0,0 +1,70 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use core::sync::atomic::Ordering;
+use crate::graphics::framebuffer::fill_rect;
+use crate::graphics::window::draw_string;
+use super::state::{STAKING_STATE, STAKE_INPUT, STAKE_INPUT_LEN, STAKE_MODE};
+use crate::graphics::window::apps::wallet::render::{format_balance, COLOR_CARD, COLOR_TEXT_DIM, COLOR_TEXT_WHITE, COLOR_ACCENT, COLOR_GREEN};
+use crate::graphics::window::apps::wallet::render_views::draw_rounded_rect;
+
+pub fn draw_staking_view(x: u32, y: u32, w: u32, _h: u32) {
+    draw_string(x + 20, y + 20, b"NOX Staking", COLOR_TEXT_WHITE);
+    let state = STAKING_STATE.lock();
+    for shadow in 0..4u32 { draw_rounded_rect(x + 20 + shadow / 2, y + 50 + shadow + 2, w - 40, 100, 14, ((15 - shadow * 3) << 24) | 0x000000); }
+    draw_rounded_rect(x + 20, y + 50, w - 40, 100, 14, COLOR_CARD);
+    draw_string(x + 36, y + 70, b"Your Staked NOX", COLOR_TEXT_DIM);
+    let (eth, wei) = ((state.staked_amount / 1_000_000_000_000_000_000) as u64, (state.staked_amount % 1_000_000_000_000_000_000 / 1_000_000_000_000_000) as u64);
+    let mut buf = [0u8; 32]; let len = format_balance(&mut buf, eth, wei);
+    draw_string(x + 36, y + 92, &buf[..len], COLOR_TEXT_WHITE);
+    draw_string(x + 36 + (len as u32 + 1) * 8, y + 92, b"NOX", 0xFFBF5AF2);
+    draw_string(x + 36, y + 120, b"Boost:", COLOR_TEXT_DIM);
+    draw_string(x + 90, y + 120, state.boost_display(), COLOR_GREEN);
+    draw_string(x + 140, y + 120, b"APY:", COLOR_TEXT_DIM);
+    let mut apy_buf = [0u8; 8]; let apy_len = format_apy(&mut apy_buf, state.current_apy);
+    draw_string(x + 180, y + 120, &apy_buf[..apy_len], COLOR_GREEN);
+    drop(state);
+    for shadow in 0..4u32 { draw_rounded_rect(x + 20 + shadow / 2, y + 165 + shadow + 2, w - 40, 120, 14, ((15 - shadow * 3) << 24) | 0x000000); }
+    draw_rounded_rect(x + 20, y + 165, w - 40, 120, 14, COLOR_CARD);
+    let mode = STAKE_MODE.load(Ordering::SeqCst);
+    draw_rounded_rect(x + 36, y + 185, 80, 30, 8, if mode == 0 { COLOR_ACCENT } else { 0xFF3A3A3C });
+    draw_string(x + 56, y + 193, b"Stake", if mode == 0 { 0xFF000000 } else { COLOR_TEXT_DIM });
+    draw_rounded_rect(x + 126, y + 185, 80, 30, 8, if mode == 1 { COLOR_ACCENT } else { 0xFF3A3A3C });
+    draw_string(x + 140, y + 193, b"Unstake", if mode == 1 { 0xFF000000 } else { COLOR_TEXT_DIM });
+    draw_string(x + 36, y + 228, b"Amount:", COLOR_TEXT_DIM);
+    draw_rounded_rect(x + 100, y + 222, w - 160, 28, 6, 0xFF1C1C1E);
+    let input = STAKE_INPUT.lock(); let input_len = STAKE_INPUT_LEN.load(Ordering::SeqCst) as usize;
+    draw_string(x + 112, y + 230, &input[..input_len], COLOR_TEXT_WHITE);
+    drop(input);
+    draw_rounded_rect(x + w / 2 - 50, y + 260, 100, 36, 10, if mode == 0 { COLOR_GREEN } else { 0xFFFF3B30 });
+    draw_string(x + w / 2 - 24, y + 270, if mode == 0 { b"Stake" } else { b"Unstake" }, 0xFF000000);
+    let state2 = STAKING_STATE.lock();
+    draw_string(x + 36, y + 310, b"Pool Stats", COLOR_TEXT_DIM);
+    let (pool_eth, pool_wei) = ((state2.total_pool_staked / 1_000_000_000_000_000_000) as u64, (state2.total_pool_staked % 1_000_000_000_000_000_000 / 1_000_000_000_000_000) as u64);
+    let mut pool_buf = [0u8; 32]; let pool_len = format_balance(&mut pool_buf, pool_eth, pool_wei);
+    draw_string(x + 36, y + 330, b"Total Staked:", COLOR_TEXT_DIM);
+    draw_string(x + 150, y + 330, &pool_buf[..pool_len], COLOR_TEXT_WHITE);
+    draw_string(x + 150 + (pool_len as u32 + 1) * 8, y + 330, b"NOX", 0xFFBF5AF2);
+}
+
+fn format_apy(buf: &mut [u8; 8], apy_bps: u32) -> usize {
+    let pct = apy_bps / 100; let mut i = 0;
+    if pct >= 100 { buf[i] = b'0' + ((pct / 100) % 10) as u8; i += 1; }
+    if pct >= 10 { buf[i] = b'0' + ((pct / 10) % 10) as u8; i += 1; }
+    buf[i] = b'0' + (pct % 10) as u8; i += 1;
+    buf[i] = b'%'; i += 1;
+    i
+}
