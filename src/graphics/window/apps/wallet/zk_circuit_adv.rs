49d6b63f5d6493ea189fce6b889d3b8507074a02
eKisNonos <ekisanon@proton.me>
2026-03-24 13:40:55 +0100

    Extract advanced ZK circuit operations

---
+++ src/graphics/window/apps/wallet/zk_circuit_adv.rs
@@ -0,0 +1,54 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::zk_engine::ZKError;
+use crate::zk_engine::circuit::{Circuit, CircuitBuilder, LinearCombination};
+use crate::zk_engine::groth16::FieldElement;
+
+pub(super) fn build_stealth_spend_circuit() -> Result<Circuit, ZKError> {
+    let mut b = CircuitBuilder::new();
+    let stealth = b.alloc_input(Some("stealth_address"));
+    let spend_com = b.alloc_input(Some("spend_pubkey_commitment"));
+    let spend_sec = b.alloc_variable(Some("spend_secret"));
+    let view_sec = b.alloc_variable(Some("view_secret"));
+    let eph_sec = b.alloc_variable(Some("ephemeral_secret"));
+    let shared = b.alloc_variable(Some("shared_secret"));
+    b.enforce_multiplication(view_sec, eph_sec, shared);
+    let derived = b.alloc_variable(Some("stealth_derived"));
+    b.enforce_multiplication(spend_sec, shared, derived);
+    b.enforce_equal(LinearCombination::from_variable(derived), LinearCombination::from_variable(stealth));
+    let com_int = b.alloc_variable(Some("commitment_intermediate"));
+    b.enforce_multiplication(spend_sec, spend_sec, com_int);
+    b.enforce_equal(LinearCombination::from_variable(com_int), LinearCombination::from_variable(spend_com));
+    b.build(6)
+}
+
+pub(super) fn build_balance_sufficiency_circuit() -> Result<Circuit, ZKError> {
+    let mut b = CircuitBuilder::new();
+    let min_req = b.alloc_input(Some("minimum_required"));
+    let bal_com = b.alloc_input(Some("balance_commitment"));
+    let actual = b.alloc_variable(Some("actual_balance"));
+    let blind = b.alloc_variable(Some("blinding_factor"));
+    let diff = b.alloc_variable(Some("difference"));
+    let com_int = b.alloc_variable(Some("commit_intermediate"));
+    b.enforce_multiplication(actual, blind, com_int);
+    b.enforce_equal(LinearCombination::from_variable(com_int), LinearCombination::from_variable(bal_com));
+    let mut diff_lc = LinearCombination::from_variable(actual);
+    diff_lc.add_term(min_req, FieldElement::zero().sub(&FieldElement::one()));
+    b.enforce_equal(LinearCombination::from_variable(diff), diff_lc);
+    b.add_range_constraint(diff, 64);
+    b.build(5)
+}
