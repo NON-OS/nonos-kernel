62a45c53533c3695e92cd5c3732854c477243fdd
eKisNonos <ekisanon@proton.me>
2026-03-24 13:42:10 +0100

    Add ZkSync Era L2 integration

---
+++ src/zksync/types/mod.rs
@@ -0,0 +1,27 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod address;
+mod u256;
+mod primitives;
+mod transaction;
+mod block;
+
+pub use address::Address;
+pub use u256::U256;
+pub use primitives::{BatchNumber, BlockNumber, Nonce, Gas, TxHash};
+pub use transaction::{L2Transaction, TransactionSignature, TransactionStatus, TxFailReason};
+pub use block::{L2Block, Batch, AccountState};
