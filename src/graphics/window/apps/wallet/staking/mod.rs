916a5967c0645c87ad8317ddeffe9ce165b872ef
eKisNonos <ekisanon@proton.me>
2026-03-24 15:28:56 +0100

    Add staking module exports

---
+++ src/graphics/window/apps/wallet/staking/mod.rs
@@ -0,0 +1,25 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod constants;
+mod state;
+mod render;
+mod input;
+mod rpc;
+
+pub(super) use render::draw_staking_view;
+pub(super) use input::handle_staking_click;
+pub(crate) use state::{refresh_staking_data, STAKING_STATE};
