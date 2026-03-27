fb6b3c5f151af5cee46a3c6e2dddd0ca728dac69
eKisNonos <ekisanon@proton.me>
2026-03-27 16:09:10 +0100

    add system settings slider drawing

---
+++ src/graphics/window/settings/system/slider.rs
@@ -0,0 +1,31 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::graphics::framebuffer::{fill_rect, COLOR_ACCENT, COLOR_TEXT_WHITE};
+
+pub(super) fn draw_slider(x: u32, y: u32, w: u32, value: u8, max: u8) {
+    fill_rect(x, y + 8, w, 8, 0xFF374151);
+    let fill_w = ((value as u32) * w) / (max as u32);
+    fill_rect(x, y + 8, fill_w, 8, COLOR_ACCENT);
+    let knob_x = x + fill_w;
+    fill_rect(knob_x.saturating_sub(6), y + 4, 12, 16, 0xFFFFFFFF);
+}
+
+pub(super) fn draw_bg_name(x: u32, y: u32, name: &str) {
+    for (i, ch) in name.bytes().enumerate() {
+        crate::graphics::font::draw_char(x + (i as u32) * 8, y, ch, COLOR_TEXT_WHITE);
+    }
+}
