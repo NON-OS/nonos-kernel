790ff35ba8c03c62826147cc1d738538f7b05373
eKisNonos <ekisanon@proton.me>
2026-03-28 18:49:40 +0100

    memory: add MMU page table management
    Implement page table allocation, walking, and entry
    manipulation for the multi-level page table hierarchy
    used by the MMU.

---
+++ src/memory/mmu/mmu/page_tables.rs
@@ -0,0 +1,40 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use core::arch::asm;
+use x86_64::PhysAddr;
+use super::super::constants::PAGE_TABLE_ENTRIES;
+use super::super::error::{MmuError, MmuResult};
+use super::core::MMU;
+use crate::memory::frame_alloc;
+
+impl MMU {
+    pub(super) fn setup_initial_page_tables(&self) -> MmuResult<()> {
+        let pml4 = self.allocate_page_table_frame()?;
+        let pml4_va = self.frame_to_virt(pml4);
+        unsafe { core::ptr::write_bytes(pml4_va.as_mut_ptr::<u64>(), 0, PAGE_TABLE_ENTRIES); }
+        self.load_page_table(pml4)?;
+        Ok(())
+    }
+
+    pub(super) fn load_page_table(&self, pml4_frame: PhysAddr) -> MmuResult<()> {
+        unsafe { asm!("mov cr3, {}", in(reg) pml4_frame.as_u64(), options(nostack, preserves_flags)); }
+        *self.current_cr3.lock() = pml4_frame.as_u64();
+        self.invalidate_tlb_all();
+        Ok(())
+    }
+
+    pub(super) fn allocate_page_table_frame(&self) -> MmuResult<PhysAddr> {
+        frame_alloc::allocate_frame().ok_or(MmuError::FrameAllocationFailed)
+    }
+}
