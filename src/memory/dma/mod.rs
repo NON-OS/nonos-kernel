8bf2f37be961b73a5cc906b7f82d14b799b6a7a7
eKisNonos <ekisanon@proton.me>
2026-03-28 18:49:17 +0100

    memory: update DMA module exports
    Update the module root for the refactored allocator
    submodule and clean up license header formatting.

--- src/memory/dma/mod.rs
+++ src/memory/dma/mod.rs
@@ -1,19 +1,15 @@
 // NONOS Operating System
 // Copyright (C) 2026 NONOS Contributors
-//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Affero General Public License for more details.
-//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-//! DMA memory management.
 
 extern crate alloc;
 
@@ -27,8 +23,8 @@ pub mod types;
 #[cfg(test)]
 mod tests;
 
+pub use allocator::*;
 pub use constants::*;
 pub use error::{DmaError, DmaResult};
-pub use allocator::*;
 pub use pool::DmaPool;
 pub use types::{DmaConstraints, DmaDirection, DmaRegion, DmaStatsSnapshot, StreamingMapping};
