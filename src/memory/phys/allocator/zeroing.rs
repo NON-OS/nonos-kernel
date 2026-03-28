8de2b10c8e5996df4163e62d6c2f6fa5ef4e08b4
eKisNonos <ekisanon@proton.me>
2026-03-28 18:50:20 +0100

    memory: add frame zeroing on allocation
    Implement automatic zeroing of allocated frames for
    security, preventing information leakage from previously
    used memory.

---
+++ src/memory/phys/allocator/zeroing.rs
@@ -0,0 +1,28 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use core::ptr;
+use super::super::constants::{PAGE_SIZE, PAGE_SIZE_U64};
+use super::super::types::Frame;
+use crate::memory::layout;
+
+pub fn zero_frame(frame: Frame) {
+    let pa = frame.addr();
+    let dm_base = layout::DIRECTMAP_BASE;
+    let dm_size = layout::DIRECTMAP_SIZE;
+    if pa >= dm_size { return; }
+    let va = dm_base.wrapping_add(pa);
+    if va < dm_base { return; }
+    if va.wrapping_add(PAGE_SIZE_U64) > dm_base.wrapping_add(dm_size) { return; }
+    unsafe { ptr::write_bytes(va as *mut u8, 0, PAGE_SIZE); }
+}
