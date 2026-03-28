a9694e9223f5c6767e8e31174109cf8cba3919e1
eKisNonos <ekisanon@proton.me>
2026-03-28 18:49:49 +0100

    memory: update MMU module exports
    Update the module root for the refactored MMU submodule
    and clean up license header formatting.

--- src/memory/mmu/mod.rs
+++ src/memory/mmu/mod.rs
@@ -1,19 +1,15 @@
 // NONOS Operating System
 // Copyright (C) 2026 NONOS Contributors
-//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Affero General Public License for more details.
-//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-//! Memory management unit control.
 
 pub mod constants;
 pub mod error;
@@ -25,8 +21,5 @@ mod tests;
 
 pub use constants::*;
 pub use error::{MmuError, MmuResult};
-pub use mmu::{
-    current_cr3, get_mmu, init_mmu, invalidate_page, map_kernel_memory,
-    mmu_is_initialized as is_initialized, protection_flags, MMU,
-};
+pub use mmu::{current_cr3, get_mmu, init_mmu, invalidate_page, map_kernel_memory, mmu_is_initialized as is_initialized, protection_flags, MMU};
 pub use types::{PagePermissions, PageTableEntry, ProtectionFlags};
