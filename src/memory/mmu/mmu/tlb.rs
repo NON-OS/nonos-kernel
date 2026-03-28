350eb91d26d25b1d5247549315d2e5bd6f18ec48
eKisNonos <ekisanon@proton.me>
2026-03-28 18:49:41 +0100

    memory: add TLB management operations
    Implement TLB invalidation routines for single entries,
    address ranges, and full flushes to maintain consistency
    after page table updates.

---
+++ src/memory/mmu/mmu/tlb.rs
@@ -0,0 +1,30 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use core::arch::asm;
+use x86_64::VirtAddr;
+use super::core::MMU;
+
+impl MMU {
+    pub fn invalidate_tlb_all(&self) {
+        unsafe {
+            let cr3: u64;
+            asm!("mov {}, cr3", out(reg) cr3, options(nostack, preserves_flags));
+            asm!("mov cr3, {}", in(reg) cr3, options(nostack, preserves_flags));
+        }
+    }
+
+    pub fn invalidate_tlb_page(&self, virt_addr: VirtAddr) {
+        unsafe { asm!("invlpg [{}]", in(reg) virt_addr.as_u64(), options(nostack, preserves_flags)); }
+    }
+}
