661d392d3123956e2ef66b9cc1753a7dc48e5400
eKisNonos <ekisanon@proton.me>
2026-03-29 21:41:19 +0200

    split handoff jump into module
    the actual jump to kernel code. needs to set up registers
    correctly and never return. isolated for safety.

---
+++ nonos-bootloader/src/handoff/jump/types.rs
@@ -0,0 +1,30 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct MemoryMapEntry {
+    pub memory_type: u32,
+    pub _pad: u32,
+    pub physical_start: u64,
+    pub virtual_start: u64,
+    pub page_count: u64,
+    pub attribute: u64,
+}
+
+impl MemoryMapEntry {
+    pub const SIZE: usize = core::mem::size_of::<Self>();
+}
