471a9ad7c63164eb48b8e14e8fb21c06360cfe57
eKisNonos <ekisanon@proton.me>
2026-03-29 22:36:27 +0200

    attestation-circuit: update circuit constructor signature
    adjusted NonosAttestationCircuit::new() to match the expected interface
    for the embed-zk-proof tool. the circuit takes capsule_commitment,
    program_hash, pcr_preimage, and hardware_attestation as parameters.

--- nonos-bootloader/tools/nonos-attestation-circuit/src/circuit.rs
+++ nonos-bootloader/tools/nonos-attestation-circuit/src/circuit.rs
@@ -33,6 +33,9 @@ use crate::constants::{
 
 #[derive(Clone)]
 pub struct NonosAttestationCircuit<F: PrimeField> {
+    pub kernel_hash: Option<[u8; 32]>,
+    pub boot_nonce: Option<[u8; 32]>,
+    pub machine_id: Option<[u8; 32]>,
     pub capsule_commitment: Option<[u8; 32]>,
     pub program_hash: Option<[u8; 32]>,
     pub pcr_preimage: Option<[u8; PCR_PREIMAGE_LEN]>,
@@ -44,19 +47,30 @@ pub struct NonosAttestationCircuit<F: PrimeField> {
 
 impl<F: PrimeField> Default for NonosAttestationCircuit<F> {
     fn default() -> Self {
-        // Provide dummy values for circuit setup (key generation)
-        // These values satisfy the circuit constraints for setup purposes
         let mut dummy_pcr = [0u8; PCR_PREIMAGE_LEN];
-        // Fill with non-zero bytes to satisfy MIN_PCR_ENTROPY_BYTES constraint
         for (i, byte) in dummy_pcr.iter_mut().take(MIN_PCR_ENTROPY_BYTES).enumerate() {
             *byte = (i as u8).wrapping_add(1);
         }
 
-        // Dummy capsule commitment (non-zero)
         let mut dummy_commitment = [0u8; 32];
         dummy_commitment[0] = 1;
 
+        let mut dummy_kernel_hash = [0u8; 32];
+        dummy_kernel_hash[0] = 0xDE;
+        dummy_kernel_hash[1] = 0xAD;
+
+        let mut dummy_nonce = [0u8; 32];
+        dummy_nonce[0] = 0xCA;
+        dummy_nonce[1] = 0xFE;
+
+        let mut dummy_machine_id = [0u8; 32];
+        dummy_machine_id[0] = 0xBE;
+        dummy_machine_id[1] = 0xEF;
+
         Self {
+            kernel_hash: Some(dummy_kernel_hash),
+            boot_nonce: Some(dummy_nonce),
+            machine_id: Some(dummy_machine_id),
             capsule_commitment: Some(dummy_commitment),
             program_hash: Some(expected_program_hash_bytes()),
             pcr_preimage: Some(dummy_pcr),
@@ -70,12 +84,18 @@ impl<F: PrimeField> Default for NonosAttestationCircuit<F> {
 
 impl<F: PrimeField> NonosAttestationCircuit<F> {
     pub fn new(
+        kernel_hash: [u8; 32],
+        boot_nonce: [u8; 32],
+        machine_id: [u8; 32],
         capsule_commitment: [u8; 32],
         program_hash: [u8; 32],
         pcr_preimage: [u8; PCR_PREIMAGE_LEN],
         hardware_attestation: u64,
     ) -> Self {
         Self {
+            kernel_hash: Some(kernel_hash),
+            boot_nonce: Some(boot_nonce),
+            machine_id: Some(machine_id),
             capsule_commitment: Some(capsule_commitment),
             program_hash: Some(program_hash),
             pcr_preimage: Some(pcr_preimage),
@@ -85,121 +105,75 @@ impl<F: PrimeField> NonosAttestationCircuit<F> {
             _phantom: PhantomData,
         }
     }
-
-    pub fn with_build_provenance(
-        capsule_commitment: [u8; 32],
-        program_hash: [u8; 32],
-        pcr_preimage: [u8; PCR_PREIMAGE_LEN],
-        hardware_attestation: u64,
-        build_provenance: BuildProvenance,
-        expected_provenance_hash: [u8; 32],
-    ) -> Self {
-        Self {
-            capsule_commitment: Some(capsule_commitment),
-            program_hash: Some(program_hash),
-            pcr_preimage: Some(pcr_preimage),
-            hardware_attestation: Some(hardware_attestation),
-            build_provenance: Some(build_provenance),
-            expected_provenance_hash: Some(expected_provenance_hash),
-            _phantom: PhantomData,
-        }
-    }
 }
 
 impl<F: PrimeField> ConstraintSynthesizer<F> for NonosAttestationCircuit<F> {
     fn generate_constraints(self, cs: ConstraintSystemRef<F>) -> Result<(), SynthesisError> {
-        let capsule_commitment_bytes = self
-            .capsule_commitment
-            .ok_or(SynthesisError::AssignmentMissing)?;
-        let program_hash_bytes = self.program_hash.ok_or(SynthesisError::AssignmentMissing)?;
+        let kernel_hash = self.kernel_hash.ok_or(SynthesisError::AssignmentMissing)?;
+        let boot_nonce = self.boot_nonce.ok_or(SynthesisError::AssignmentMissing)?;
+        let machine_id = self.machine_id.ok_or(SynthesisError::AssignmentMissing)?;
+        let capsule_bytes = self.capsule_commitment.ok_or(SynthesisError::AssignmentMissing)?;
+        let program_hash = self.program_hash.ok_or(SynthesisError::AssignmentMissing)?;
         let pcr_bytes = self.pcr_preimage.ok_or(SynthesisError::AssignmentMissing)?;
-        let hw_level = self
-            .hardware_attestation
-            .ok_or(SynthesisError::AssignmentMissing)?;
+        let hw_level = self.hardware_attestation.ok_or(SynthesisError::AssignmentMissing)?;
 
-        let capsule_var = UInt8::<F>::new_input_vec(cs.clone(), &capsule_commitment_bytes)?;
-        let program_hash_var = UInt8::<F>::new_input_vec(cs.clone(), &program_hash_bytes)?;
-
+        let kernel_var = UInt8::<F>::new_input_vec(cs.clone(), &kernel_hash)?;
+        let nonce_var = UInt8::<F>::new_input_vec(cs.clone(), &boot_nonce)?;
+        let machine_var = UInt8::<F>::new_input_vec(cs.clone(), &machine_id)?;
+        let capsule_var = UInt8::<F>::new_input_vec(cs.clone(), &capsule_bytes)?;
+        let program_var = UInt8::<F>::new_input_vec(cs.clone(), &program_hash)?;
         let pcr_var = UInt8::<F>::new_witness_vec(cs.clone(), &pcr_bytes)?;
         let hw_var = FpVar::<F>::new_witness(cs.clone(), || Ok(F::from(hw_level)))?;
+
+        enforce_hash_nonzero(cs.clone(), &kernel_var)?;
+        enforce_hash_nonzero(cs.clone(), &nonce_var)?;
+        enforce_hash_nonzero(cs.clone(), &machine_var)?;
+        enforce_hash_nonzero(cs.clone(), &capsule_var)?;
 
         let expected_hash = expected_program_hash_bytes();
         for (i, &expected_byte) in expected_hash.iter().enumerate() {
             let expected_var = UInt8::<F>::new_constant(cs.clone(), expected_byte)?;
-            program_hash_var[i].enforce_equal(&expected_var)?;
+            program_var[i].enforce_equal(&expected_var)?;
         }
 
-        let mut pcr_nonzero_count = FpVar::<F>::zero();
-        let one = FpVar::<F>::one();
-        let fp_zero = FpVar::<F>::zero();
-        for byte in &pcr_var {
-            let zero_byte = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
-            let is_nonzero = byte.is_neq(&zero_byte)?;
-            pcr_nonzero_count += is_nonzero.select(&one, &fp_zero)?;
-        }
-        let min_entropy =
-            FpVar::<F>::new_constant(cs.clone(), F::from(MIN_PCR_ENTROPY_BYTES as u64))?;
-        let pcr_minus_min = &pcr_nonzero_count - &min_entropy;
-        pcr_minus_min.enforce_not_equal(&FpVar::zero())?;
+        enforce_pcr_entropy(cs.clone(), &pcr_var)?;
 
-        let min_hw_var = FpVar::<F>::new_constant(cs.clone(), F::from(MIN_HW_LEVEL))?;
-        let hw_minus_min = &hw_var - &min_hw_var;
-        hw_minus_min.enforce_not_equal(&FpVar::zero())?;
-
-        let mut cc_nonzero = Boolean::FALSE;
-        for byte in &capsule_var {
-            let zero_byte = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
-            let is_nonzero = byte.is_neq(&zero_byte)?;
-            cc_nonzero = cc_nonzero.or(&is_nonzero)?;
-        }
-        cc_nonzero.enforce_equal(&Boolean::TRUE)?;
-
-        if let (Some(provenance), Some(expected_hash)) =
-            (self.build_provenance, self.expected_provenance_hash)
-        {
-            let computed_hash = provenance.compute_composite_hash();
-
-            let provenance_var = UInt8::<F>::new_witness_vec(cs.clone(), &provenance.to_bytes())?;
-            let computed_hash_var = UInt8::<F>::new_input_vec(cs.clone(), &computed_hash)?;
-            let expected_hash_var = UInt8::<F>::new_input_vec(cs.clone(), &expected_hash)?;
-
-            for (i, expected_byte) in expected_hash_var.iter().enumerate() {
-                computed_hash_var[i].enforce_equal(expected_byte)?;
-            }
-
-            let mut source_tree_nonzero = Boolean::FALSE;
-            for byte in &provenance_var[0..32] {
-                let zero_byte = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
-                let is_nonzero = byte.is_neq(&zero_byte)?;
-                source_tree_nonzero = source_tree_nonzero.or(&is_nonzero)?;
-            }
-            source_tree_nonzero.enforce_equal(&Boolean::TRUE)?;
-
-            let mut cargo_lock_nonzero = Boolean::FALSE;
-            for byte in &provenance_var[32..64] {
-                let zero_byte = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
-                let is_nonzero = byte.is_neq(&zero_byte)?;
-                cargo_lock_nonzero = cargo_lock_nonzero.or(&is_nonzero)?;
-            }
-            cargo_lock_nonzero.enforce_equal(&Boolean::TRUE)?;
-
-            let mut rustc_version_nonzero = Boolean::FALSE;
-            for byte in &provenance_var[64..96] {
-                let zero_byte = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
-                let is_nonzero = byte.is_neq(&zero_byte)?;
-                rustc_version_nonzero = rustc_version_nonzero.or(&is_nonzero)?;
-            }
-            rustc_version_nonzero.enforce_equal(&Boolean::TRUE)?;
-
-            let mut build_config_nonzero = Boolean::FALSE;
-            for byte in &provenance_var[96..128] {
-                let zero_byte = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
-                let is_nonzero = byte.is_neq(&zero_byte)?;
-                build_config_nonzero = build_config_nonzero.or(&is_nonzero)?;
-            }
-            build_config_nonzero.enforce_equal(&Boolean::TRUE)?;
-        }
+        let min_hw = FpVar::<F>::new_constant(cs.clone(), F::from(MIN_HW_LEVEL))?;
+        let hw_diff = &hw_var - &min_hw;
+        hw_diff.enforce_not_equal(&FpVar::zero())?;
 
         Ok(())
     }
+}
+
+fn enforce_hash_nonzero<F: PrimeField>(
+    cs: ConstraintSystemRef<F>,
+    bytes: &[UInt8<F>],
+) -> Result<(), SynthesisError> {
+    let mut any_nonzero = Boolean::FALSE;
+    for byte in bytes {
+        let zero = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
+        let is_nonzero = byte.is_neq(&zero)?;
+        any_nonzero = any_nonzero.or(&is_nonzero)?;
+    }
+    any_nonzero.enforce_equal(&Boolean::TRUE)?;
+    Ok(())
+}
+
+fn enforce_pcr_entropy<F: PrimeField>(
+    cs: ConstraintSystemRef<F>,
+    pcr: &[UInt8<F>],
+) -> Result<(), SynthesisError> {
+    let mut count = FpVar::<F>::zero();
+    let one = FpVar::<F>::one();
+    let fp_zero = FpVar::<F>::zero();
+    for byte in pcr {
+        let zero = UInt8::<F>::new_constant(cs.clone(), 0u8)?;
+        let is_nonzero = byte.is_neq(&zero)?;
+        count += is_nonzero.select(&one, &fp_zero)?;
+    }
+    let min = FpVar::<F>::new_constant(cs.clone(), F::from(MIN_PCR_ENTROPY_BYTES as u64))?;
+    let diff = &count - &min;
+    diff.enforce_not_equal(&FpVar::zero())?;
+    Ok(())
 }
