2ba2407b336cfa5bcdb572db694d90997f0922d8
eKisNonos <ekisanon@proton.me>
2026-03-29 21:36:42 +0200

    wire up entry module with new helpers
    adds the kernel_main, oom handler, and vga error display
    to the entry module exports.

--- src/entry/mod.rs
+++ src/entry/mod.rs
@@ -1,18 +1,28 @@
 // NONOS Operating System
 // Copyright (C) 2026 NONOS Contributors
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Affero General Public License for more details.
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-pub mod network;
-pub mod security;
-pub mod desktop_loop;
 pub mod context;
+pub mod desktop_loop;
 pub mod fallback;
+pub mod kernel_main;
+pub mod network;
+pub mod oom;
+pub mod security;
+pub mod vga_error;
+
+pub use kernel_main::kernel_main;
+pub use oom::handle_oom;
+pub use vga_error::{early_vga_error, halt_loop};
