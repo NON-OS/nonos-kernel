e708807307c3f42aa79071fd12defa494e3667b3
eKisNonos <ekisanon@proton.me>
2026-03-30 17:31:12 +0200

    crypto: modularize random API into directory structure
    Converted random.rs to random/ module directory with separate
    fill operations, integer generation, and secure byte extraction.

---
+++ src/crypto/core/api/random/mod.rs
@@ -0,0 +1,22 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod api;
+mod keygen;
+mod platform;
+
+pub use api::{secure_random_u32, secure_random_u64, secure_random_u8, fill_random, generate_secure_key_checked};
+pub use keygen::generate_secure_key;
