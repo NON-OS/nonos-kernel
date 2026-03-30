23794df171035afa0e55871987c5ad30b48ceba5
eKisNonos <ekisanon@proton.me>
2026-03-30 17:31:20 +0200

    audio: modularize controller initialization and codec discovery
    Converted init.rs and codec/discovery.rs to module directories
    for cleaner separation of HD Audio controller setup stages and
    codec enumeration logic.

---
+++ src/drivers/audio/controller/init/controller.rs
@@ -0,0 +1,70 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use super::super::super::error::AudioError;
+use super::super::super::types::DmaRegion;
+use super::super::super::constants::*;
+use super::super::helpers::{RegisterAccess, spin_while};
+use super::super::corb_rirb::{stop_corb, stop_rirb};
+use super::super::codec::{self, CodecInfo, CodecPaths};
+use super::types::Capabilities;
+use super::reset::reset_controller;
+use super::capabilities::{read_capabilities, read_codec_mask, find_primary_codec};
+use super::discover::{init_command_buffers, discover_codecs};
+
+pub fn init_controller<T: RegisterAccess>(ctrl: &T, corb: &DmaRegion, rirb: &DmaRegion) -> Result<(Capabilities, u16, Option<u8>, Option<CodecPaths>), AudioError> {
+    reset_controller(ctrl)?;
+    clear_codec_status(ctrl);
+    let caps = read_capabilities(ctrl);
+    crate::log::logger::log_critical(&alloc::format!("HDA: {} output, {} input, {} bidi streams", caps.output_streams, caps.input_streams, caps.bidi_streams));
+    init_command_buffers(ctrl, corb, rirb);
+    let codec_mask = read_codec_mask(ctrl);
+    let primary_codec = find_primary_codec(codec_mask);
+    let codecs = discover_codecs(ctrl, corb, rirb, CORB_ENTRIES, RIRB_ENTRIES, codec_mask);
+    let codec_paths = if let Some(cad) = primary_codec {
+        let primary_codec_info = codecs.iter().find(|c| c.cad == cad);
+        if let Some(codec_info) = primary_codec_info {
+            match codec::discover_paths(ctrl, corb, rirb, CORB_ENTRIES, RIRB_ENTRIES, codec_info) {
+                Ok(paths) => {
+                    if paths.output_count > 0 { if let Err(e) = codec::init_codec_path(ctrl, corb, rirb, CORB_ENTRIES, RIRB_ENTRIES, codec_info) { crate::log::logger::log_critical(&alloc::format!("HDA: Failed to init codec path: {:?}", e)); } }
+                    Some(paths)
+                }
+                Err(e) => { crate::log::logger::log_critical(&alloc::format!("HDA: Failed to discover paths: {:?}", e)); None }
+            }
+        } else { None }
+    } else { None };
+    Ok((caps, codec_mask, primary_codec, codec_paths))
+}
+
+pub fn disable_interrupts<T: RegisterAccess>(ctrl: &T) { let intctl = ctrl.read_reg32(INTCTL); ctrl.write_reg32(INTCTL, intctl & !((1 << 31) | (1 << 30))); }
+
+pub fn shutdown_controller<T: RegisterAccess>(ctrl: &T) -> Result<(), AudioError> {
+    disable_interrupts(ctrl); stop_corb(ctrl); stop_rirb(ctrl);
+    let mut gctl = ctrl.read_reg32(GCTL); gctl &= !GCTL_CRST; ctrl.write_reg32(GCTL, gctl);
+    if !spin_while(|| (ctrl.read_reg32(GCTL) & GCTL_CRST) != 0, SPIN_TIMEOUT_DEFAULT) { return Err(AudioError::CrstClearTimeout); }
+    Ok(())
+}
+
+pub fn clear_codec_status<T: RegisterAccess>(ctrl: &T) { ctrl.write_reg16(STATESTS, 0xFFFF); }
+pub fn is_in_reset<T: RegisterAccess>(ctrl: &T) -> bool { (ctrl.read_reg32(GCTL) & GCTL_CRST) == 0 }
+pub fn is_running<T: RegisterAccess>(ctrl: &T) -> bool { (ctrl.read_reg32(GCTL) & GCTL_CRST) != 0 }
+pub fn read_version<T: RegisterAccess>(ctrl: &T) -> (u8, u8) { let vmin = ctrl.read_reg8(VMIN); let vmaj = ctrl.read_reg8(VMAJ); (vmaj, vmin) }
+pub fn validate_controller<T: RegisterAccess>(ctrl: &T) -> Result<(), AudioError> {
+    if is_in_reset(ctrl) { return Err(AudioError::CrstClearTimeout); }
+    let caps = read_capabilities(ctrl);
+    if caps.output_streams == 0 { return Err(AudioError::NoCodecPresent); }
+    Ok(())
+}
