aa12b78005329361996bff9d6ee1a4c981a84635
eKisNonos <ekisanon@proton.me>
2026-03-30 17:32:28 +0200

    memory/mmio: modularize manager into directory structure
    Converted manager.rs to manager/ module directory separating
    MMIO region allocation, access control, and device mapping.

---
+++ src/memory/mmio/manager/api.rs
@@ -0,0 +1,42 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+use alloc::vec::Vec;
+use spin::Mutex;
+use x86_64::{PhysAddr, VirtAddr};
+use super::core::MmioManager;
+use super::super::error::MmioResult;
+use super::super::stats::MMIO_STATS;
+use super::super::types::{MmioFlags, MmioRegion, MmioStatsSnapshot};
+
+pub(super) static MMIO_MANAGER: Mutex<MmioManager> = Mutex::new(MmioManager::new());
+
+pub fn init() -> MmioResult<()> { MMIO_MANAGER.lock().init() }
+pub fn map_mmio(pa: PhysAddr, size: usize, flags: MmioFlags) -> MmioResult<VirtAddr> { MMIO_MANAGER.lock().map_region(pa, size, flags) }
+pub fn map_device_memory(pa: PhysAddr, size: usize) -> MmioResult<VirtAddr> { map_mmio(pa, size, MmioFlags::device()) }
+pub fn map_framebuffer(pa: PhysAddr, size: usize) -> MmioResult<VirtAddr> { map_mmio(pa, size, MmioFlags::framebuffer()) }
+pub fn unmap_mmio(va: VirtAddr) -> MmioResult<()> { MMIO_MANAGER.lock().unmap_region(va) }
+pub fn get_region_info(va: VirtAddr) -> Option<MmioRegion> { MMIO_MANAGER.lock().find_region(va).copied() }
+pub fn list_regions() -> Vec<MmioRegion> { MMIO_MANAGER.lock().regions().copied().collect() }
+pub fn get_mapped_regions() -> Vec<MmioRegion> { list_regions() }
+pub fn get_stats() -> MmioStatsSnapshot { MMIO_STATS.snapshot() }
+
+pub fn validate_mmio_access(va: VirtAddr, size: usize) -> bool {
+    MMIO_MANAGER.lock().find_region(va).map(|r| va.as_u64() + size as u64 <= r.va.as_u64() + r.size as u64).unwrap_or(false)
+}
+
+pub fn is_mmio_region(va: VirtAddr) -> bool { MMIO_MANAGER.lock().find_region(va).is_some() }
---
+++ src/memory/mmio/manager/io.rs
@@ -0,0 +1,30 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use x86_64::VirtAddr;
+use super::api::MMIO_MANAGER;
+use super::super::constants::*;
+use super::super::error::MmioResult;
+use super::super::ops;
+
+pub unsafe fn read8(va: VirtAddr, offset: usize) -> MmioResult<u8> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_8)?; Ok(ops::read8_at(va.as_u64() + offset as u64)) } }
+pub unsafe fn read16(va: VirtAddr, offset: usize) -> MmioResult<u16> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_16)?; Ok(ops::read16_at(va.as_u64() + offset as u64)) } }
+pub unsafe fn read32(va: VirtAddr, offset: usize) -> MmioResult<u32> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_32)?; Ok(ops::read32_at(va.as_u64() + offset as u64)) } }
+pub unsafe fn read64(va: VirtAddr, offset: usize) -> MmioResult<u64> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_64)?; Ok(ops::read64_at(va.as_u64() + offset as u64)) } }
+pub unsafe fn write8(va: VirtAddr, offset: usize, value: u8) -> MmioResult<()> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_8)?; ops::write8_at(va.as_u64() + offset as u64, value); Ok(()) } }
+pub unsafe fn write16(va: VirtAddr, offset: usize, value: u16) -> MmioResult<()> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_16)?; ops::write16_at(va.as_u64() + offset as u64, value); Ok(()) } }
+pub unsafe fn write32(va: VirtAddr, offset: usize, value: u32) -> MmioResult<()> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_32)?; ops::write32_at(va.as_u64() + offset as u64, value); Ok(()) } }
+pub unsafe fn write64(va: VirtAddr, offset: usize, value: u64) -> MmioResult<()> { unsafe { let m = MMIO_MANAGER.lock(); m.validate_access(va, offset, ACCESS_SIZE_64)?; ops::write64_at(va.as_u64() + offset as u64, value); Ok(()) } }
