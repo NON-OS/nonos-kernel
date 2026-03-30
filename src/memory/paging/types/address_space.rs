948b42792267cc28a708671434df4bd6ac999da2
eKisNonos <ekisanon@proton.me>
2026-03-30 16:38:07 +0200

    memory/paging: split type definitions with address space management
    The paging type system has been restructured to separate page table
    entry definitions from virtual address space management. The address
    space manager now properly isolates per-process page table state.

---
+++ src/memory/paging/types/address_space.rs
@@ -0,0 +1,41 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+use alloc::vec::Vec;
+use x86_64::{PhysAddr, VirtAddr};
+use crate::memory::paging::constants::KERNEL_ASID;
+use super::helpers::get_timestamp;
+
+#[derive(Debug, Clone)]
+pub struct AddressSpace {
+    pub asid: u32,
+    pub cr3_value: PhysAddr,
+    pub mappings: Vec<VirtAddr>,
+    pub process_id: u32,
+    pub creation_time: u64,
+}
+
+impl AddressSpace {
+    pub fn new(asid: u32, cr3_value: PhysAddr, process_id: u32) -> Self {
+        Self { asid, cr3_value, mappings: Vec::new(), process_id, creation_time: get_timestamp() }
+    }
+
+    pub const fn is_kernel(&self) -> bool { self.asid == KERNEL_ASID }
+    pub fn mapping_count(&self) -> usize { self.mappings.len() }
+    pub fn add_mapping(&mut self, va: VirtAddr) { self.mappings.push(va); }
+    pub fn remove_mapping(&mut self, va: VirtAddr) { self.mappings.retain(|&addr| addr != va); }
+}
---
+++ src/memory/paging/types/page_size.rs
@@ -0,0 +1,50 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::memory::paging::constants::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PageSize {
+    Size4KiB,
+    Size2MiB,
+    Size1GiB,
+}
+
+impl PageSize {
+    pub const fn bytes(&self) -> usize {
+        match self {
+            Self::Size4KiB => PAGE_SIZE_4K,
+            Self::Size2MiB => PAGE_SIZE_2M,
+            Self::Size1GiB => PAGE_SIZE_1G,
+        }
+    }
+
+    pub const fn align_mask(&self) -> u64 {
+        match self {
+            Self::Size4KiB => 0xFFF,
+            Self::Size2MiB => 0x1F_FFFF,
+            Self::Size1GiB => 0x3FFF_FFFF,
+        }
+    }
+
+    pub const fn is_aligned(&self, addr: u64) -> bool {
+        addr & self.align_mask() == 0
+    }
+}
+
+impl Default for PageSize {
+    fn default() -> Self { Self::Size4KiB }
+}
