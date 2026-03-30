88a26830aedc1203d896f96e2c3abd47023985b0
eKisNonos <ekisanon@proton.me>
2026-03-30 17:31:26 +0200

    console: modularize types into directory structure
    Converted types.rs to types/ module directory separating color
    definitions, character attributes, and screen buffer structures.

---
+++ src/drivers/console/types/mod.rs
@@ -0,0 +1,27 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod color;
+mod helpers;
+mod vga_cell;
+mod log_level;
+mod stats;
+
+pub use color::Color;
+pub use helpers::{make_color, fg_from_attr, bg_from_attr, set_fg, set_bg};
+pub use vga_cell::VgaCell;
+pub use log_level::LogLevel;
+pub use stats::{ConsoleStats, ConsoleStatsSnapshot};
