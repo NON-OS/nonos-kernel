5a2a7c1b73689503a5b6def8f8811f22e5545e86
eKisNonos <ekisanon@proton.me>
2026-03-30 16:38:17 +0200

    graphics: restructure double buffering with page flip management
    The double buffer implementation has been modularized to separate
    buffer allocation from the page flip synchronization logic. This
    enables tear-free rendering with proper vsync handling.

---
+++ src/graphics/framebuffer/double_buffer/mod.rs
@@ -0,0 +1,23 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod state;
+mod ops;
+mod swap;
+
+pub use state::{init_double_buffer, is_enabled, enable, disable, get_back_buffer_ptr};
+pub use ops::{put_pixel_back, fill_rect_back, clear_back};
+pub use swap::swap_buffers;
