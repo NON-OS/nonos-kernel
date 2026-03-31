4e2f687a751bd1edd5b7090cc32da00e2c30b62c
eKisNonos <ekisanon@proton.me>
2026-03-31 02:29:59 +0200

    apps/wallet: add HD key derivation submodule
    BIP32/BIP44 hierarchical deterministic wallet key management
    with address generation and derivation path handling.

---
+++ src/apps/ecosystem/wallet/keys/types.rs
@@ -0,0 +1,40 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use core::ptr;
+use core::sync::atomic::{compiler_fence, Ordering};
+use crate::crypto::application::bip32::ExtendedPrivateKey;
+
+pub(super) const BIP44_PURPOSE: u32 = 44;
+pub(super) const BIP44_ETH_COIN: u32 = 60;
+
+#[derive(Clone)]
+pub struct WalletKeys {
+    pub(super) master: ExtendedPrivateKey,
+    pub(super) account_key: ExtendedPrivateKey,
+}
+
+impl Drop for WalletKeys {
+    fn drop(&mut self) {
+        unsafe {
+            ptr::write_volatile(&mut self.master as *mut ExtendedPrivateKey, core::mem::zeroed());
+            ptr::write_volatile(&mut self.account_key as *mut ExtendedPrivateKey, core::mem::zeroed());
+        }
+        compiler_fence(Ordering::SeqCst);
+    }
+}
+
+pub(super) fn hex_char(nibble: u8) -> u8 { if nibble < 10 { b'0' + nibble } else { b'a' + (nibble - 10) } }
