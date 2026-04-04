27329e8cabb421531912c1742ade66137298f5b0
eKisNonos <ekisanon@proton.me>
2026-04-04 15:14:05 +0200

    memory: encryption module for secure memory regions

---
+++ src/memory/encryption/detect.rs
@@ -0,0 +1,71 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use super::types::EncryptionCapability;
+
+const CPUID_AMD_SME_LEAF: u32 = 0x8000001F;
+const CPUID_INTEL_TME_LEAF: u32 = 0x13;
+
+pub fn detect_encryption_support() -> EncryptionCapability {
+    let mut cap = EncryptionCapability::none();
+    if is_amd_cpu() { detect_amd_encryption(&mut cap); }
+    else if is_intel_cpu() { detect_intel_encryption(&mut cap); }
+    cap
+}
+
+fn is_amd_cpu() -> bool {
+    let (_, ebx, ecx, edx) = cpuid(0);
+    ebx == 0x68747541 && edx == 0x69746E65 && ecx == 0x444D4163
+}
+
+fn is_intel_cpu() -> bool {
+    let (_, ebx, ecx, edx) = cpuid(0);
+    ebx == 0x756E6547 && edx == 0x49656E69 && ecx == 0x6C65746E
+}
+
+fn detect_amd_encryption(cap: &mut EncryptionCapability) {
+    let (max_ext, _, _, _) = cpuid(0x80000000);
+    if max_ext < CPUID_AMD_SME_LEAF { return; }
+    let (eax, ebx, _, _) = cpuid(CPUID_AMD_SME_LEAF);
+    cap.sme_supported = (eax & 0x01) != 0;
+    cap.sev_supported = (eax & 0x02) != 0;
+    cap.c_bit_position = (ebx & 0x3F) as u8;
+    cap.phys_addr_reduction = ((ebx >> 6) & 0x3F) as u8;
+}
+
+fn detect_intel_encryption(cap: &mut EncryptionCapability) {
+    let (max_leaf, _, _, _) = cpuid(0);
+    if max_leaf < 7 { return; }
+    let (_, _, ecx, _) = cpuid_subleaf(7, 0);
+    cap.tme_supported = (ecx & (1 << 13)) != 0;
+    cap.mktme_supported = cap.tme_supported && max_leaf >= CPUID_INTEL_TME_LEAF;
+    if cap.mktme_supported {
+        let (eax, _, _, _) = cpuid_subleaf(CPUID_INTEL_TME_LEAF, 0);
+        cap.keyid_bits = ((eax >> 4) & 0x0F) as u8;
+    }
+}
+
+pub fn get_encryption_mask(cap: &EncryptionCapability) -> u64 {
+    if cap.c_bit_position > 0 { 1u64 << cap.c_bit_position } else { 0 }
+}
+
+fn cpuid(leaf: u32) -> (u32, u32, u32, u32) {
+    crate::arch::x86_64::cpu::cpuid::cpuid(leaf)
+}
+
+fn cpuid_subleaf(leaf: u32, subleaf: u32) -> (u32, u32, u32, u32) {
+    crate::arch::x86_64::cpu::cpuid::cpuid_count(leaf, subleaf)
+}
---
+++ src/memory/encryption/tme.rs
@@ -0,0 +1,71 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use super::types::EncryptionCapability;
+use super::error::{MemEncryptionError, MemEncryptionResult};
+
+const MSR_IA32_TME_CAPABILITY: u32 = 0x981;
+const MSR_IA32_TME_ACTIVATE: u32 = 0x982;
+const MSR_IA32_MKTME_KEYID_PARTITIONING: u32 = 0x87;
+
+const TME_ENABLE_BIT: u64 = 1 << 1;
+const TME_LOCKED_BIT: u64 = 1 << 0;
+
+pub fn init_tme(cap: &EncryptionCapability) -> MemEncryptionResult<()> {
+    if !cap.tme_supported { return Err(MemEncryptionError::NotSupported); }
+    let tme_cap = rdmsr(MSR_IA32_TME_CAPABILITY);
+    if tme_cap == 0 { return Err(MemEncryptionError::HardwareError); }
+    Ok(())
+}
+
+pub fn enable_tme(_cap: &EncryptionCapability) -> MemEncryptionResult<()> {
+    let activate = rdmsr(MSR_IA32_TME_ACTIVATE);
+    if (activate & TME_LOCKED_BIT) != 0 {
+        if (activate & TME_ENABLE_BIT) != 0 { return Ok(()); }
+        return Err(MemEncryptionError::AlreadyEnabled);
+    }
+    let new_value = activate | TME_ENABLE_BIT | TME_LOCKED_BIT;
+    wrmsr(MSR_IA32_TME_ACTIVATE, new_value);
+    Ok(())
+}
+
+pub fn get_tme_keyid_bits(cap: &EncryptionCapability) -> u8 {
+    cap.keyid_bits
+}
+
+pub fn get_mktme_keyid_partitioning() -> (u32, u32) {
+    let partitioning = rdmsr(MSR_IA32_MKTME_KEYID_PARTITIONING);
+    let num_tme_keys = (partitioning & 0xFFFF) as u32;
+    let num_mktme_keys = ((partitioning >> 16) & 0xFFFF) as u32;
+    (num_tme_keys, num_mktme_keys)
+}
+
+pub fn is_tme_enabled() -> bool {
+    let activate = rdmsr(MSR_IA32_TME_ACTIVATE);
+    (activate & TME_ENABLE_BIT) != 0
+}
+
+fn rdmsr(msr: u32) -> u64 {
+    let (low, high): (u32, u32);
+    unsafe { core::arch::asm!("rdmsr", in("ecx") msr, out("eax") low, out("edx") high); }
+    ((high as u64) << 32) | (low as u64)
+}
+
+fn wrmsr(msr: u32, value: u64) {
+    let low = value as u32;
+    let high = (value >> 32) as u32;
+    unsafe { core::arch::asm!("wrmsr", in("ecx") msr, in("eax") low, in("edx") high); }
+}
