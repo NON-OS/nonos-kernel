a767f0ef0e3e0941c3a4981711493f458ae98af9
eKisNonos <ekisanon@proton.me>
2026-04-06 15:03:47 +0200

    kernel: update root module

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@
 #![no_std]
 #![cfg_attr(not(feature = "std"), feature(alloc_error_handler))]
 #![feature(abi_x86_interrupt)]
+#![feature(c_variadic)]
 #![allow(unsafe_op_in_unsafe_fn)]
 
 #[macro_use]
@@ -29,8 +30,8 @@ fn alloc_error_handler(layout: core::alloc::Layout) -> ! { entry::handle_oom(lay
 pub mod agents; pub mod apps; pub mod arch; pub mod boot; pub mod bus; pub mod capabilities;
 pub mod context; pub mod crypto; pub mod daemon; pub mod display; pub mod drivers; pub mod elf;
 pub mod entry; pub mod fs; pub mod graphics; pub mod input; pub mod interrupts; pub mod ipc;
-pub mod kernel_core; pub mod kernel_selftest; pub mod lang; pub mod locale; pub mod log; pub mod mem;
-pub mod memory; pub mod modules; pub mod monitor; pub mod network; pub mod nox; pub mod npkg;
+pub mod kernel_core; pub mod kernel_selftest; pub mod lang; pub mod libc; pub mod locale; pub mod log;
+pub mod mem; pub mod memory; pub mod modules; pub mod monitor; pub mod network; pub mod nox; pub mod npkg;
 pub mod persistence; pub mod process; pub mod runtime; pub mod sched; pub mod sdk;
 pub mod security; pub mod services; pub mod shell; pub mod smp; pub mod storage; pub mod sys;
 pub mod syscall; pub mod test; pub mod ui; pub mod usercopy; pub mod userspace; pub mod vault;
