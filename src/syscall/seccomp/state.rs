f94b6415143006eef28017246ade335a2e14bbd8
eKisNonos <ekisanon@proton.me>
2026-04-06 14:51:17 +0200

    seccomp: add process seccomp state tracking

---
+++ src/syscall/seccomp/state.rs
@@ -0,0 +1,79 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+
+use alloc::collections::BTreeMap;
+use alloc::vec::Vec;
+use spin::Mutex;
+use super::types::{SECCOMP_MODE_DISABLED, SECCOMP_MODE_STRICT, SECCOMP_MODE_FILTER};
+use super::filter::SeccompFilter;
+
+#[derive(Clone)]
+pub struct SeccompState {
+    pub mode: u32,
+    pub filters: Vec<SeccompFilter>,
+}
+
+impl Default for SeccompState {
+    fn default() -> Self {
+        Self { mode: SECCOMP_MODE_DISABLED, filters: Vec::new() }
+    }
+}
+
+static PROCESS_SECCOMP: Mutex<BTreeMap<u32, SeccompState>> = Mutex::new(BTreeMap::new());
+
+pub fn get_mode(pid: u32) -> u32 {
+    PROCESS_SECCOMP.lock().get(&pid).map(|s| s.mode).unwrap_or(SECCOMP_MODE_DISABLED)
+}
+
+pub fn set_strict_mode(pid: u32) -> Result<(), i32> {
+    let mut map = PROCESS_SECCOMP.lock();
+    let state = map.entry(pid).or_default();
+    if state.mode != SECCOMP_MODE_DISABLED { return Err(1); }
+    state.mode = SECCOMP_MODE_STRICT;
+    Ok(())
+}
+
+pub fn add_filter(pid: u32, filter: SeccompFilter) -> Result<(), i32> {
+    filter.validate()?;
+    let mut map = PROCESS_SECCOMP.lock();
+    let state = map.entry(pid).or_default();
+    if state.mode == SECCOMP_MODE_STRICT { return Err(1); }
+    state.mode = SECCOMP_MODE_FILTER;
+    state.filters.push(filter);
+    Ok(())
+}
+
+pub fn get_filters(pid: u32) -> Vec<SeccompFilter> {
+    PROCESS_SECCOMP.lock().get(&pid).map(|s| s.filters.clone()).unwrap_or_default()
+}
+
+pub fn clone_seccomp(parent_pid: u32, child_pid: u32) {
+    let map = PROCESS_SECCOMP.lock();
+    if let Some(parent_state) = map.get(&parent_pid) {
+        let mut map = PROCESS_SECCOMP.lock();
+        map.insert(child_pid, parent_state.clone());
+    }
+}
+
+pub fn clear_seccomp(pid: u32) {
+    PROCESS_SECCOMP.lock().remove(&pid);
+}
+
+pub fn filter_count(pid: u32) -> usize {
+    PROCESS_SECCOMP.lock().get(&pid).map(|s| s.filters.len()).unwrap_or(0)
+}
