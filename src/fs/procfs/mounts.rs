3f526f3209d53ace05ea022c9e5d3a4cff260422
eKisNonos <ekisanon@proton.me>
2026-04-06 15:10:01 +0200

    procfs: add /proc/mounts

---
+++ src/fs/procfs/mounts.rs
@@ -0,0 +1,80 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+
+use alloc::string::String;
+use alloc::format;
+use alloc::vec::Vec;
+
+pub fn read_mounts() -> String {
+    let mounts = crate::fs::vfs::get_mounts();
+    let mut output = String::new();
+    for mount in mounts {
+        output.push_str(&format!(
+            "{} {} {} {} {} {}\n",
+            mount.device,
+            mount.mountpoint,
+            mount.fstype,
+            format_mount_options(&mount.options),
+            mount.dump_freq,
+            mount.pass_num
+        ));
+    }
+    output
+}
+
+fn format_mount_options(options: &[MountOption]) -> String {
+    if options.is_empty() {
+        return String::from("rw");
+    }
+    let mut opts = Vec::new();
+    for opt in options {
+        opts.push(opt.to_string());
+    }
+    opts.join(",")
+}
+
+#[derive(Debug, Clone)]
+pub enum MountOption {
+    ReadWrite,
+    ReadOnly,
+    NoExec,
+    NoSuid,
+    NoDev,
+    Sync,
+    Async,
+    Relatime,
+    NoAtime,
+    Defaults,
+}
+
+impl MountOption {
+    pub fn to_string(&self) -> &'static str {
+        match self {
+            Self::ReadWrite => "rw",
+            Self::ReadOnly => "ro",
+            Self::NoExec => "noexec",
+            Self::NoSuid => "nosuid",
+            Self::NoDev => "nodev",
+            Self::Sync => "sync",
+            Self::Async => "async",
+            Self::Relatime => "relatime",
+            Self::NoAtime => "noatime",
+            Self::Defaults => "defaults",
+        }
+    }
+}
