d1ca19e2cfac1d982d45ab5b74ab736daa991c7f
eKisNonos <ekisanon@proton.me>
2026-04-06 15:14:13 +0200

    sysfs: add mod

---
+++ src/fs/sysfs/devices/mod.rs
@@ -0,0 +1,48 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod pci;
+mod platform;
+mod system;
+
+pub use pci::{init_pci_devices, register_pci_device};
+pub use platform::{init_platform_devices, register_platform_device};
+pub use system::{init_system_devices, get_cpu_device, get_node_device};
+
+use super::kobject::{register_kobject, KobjectType};
+
+static mut DEVICES_INO: u64 = 200;
+
+pub fn init_devices_subsystem() {
+    unsafe {
+        DEVICES_INO = 200;
+    }
+    pci::init_pci_devices();
+    platform::init_platform_devices();
+    system::init_system_devices();
+}
+
+pub fn get_devices_ino() -> u64 {
+    unsafe { DEVICES_INO }
+}
+
+pub fn register_device(name: &str, parent: u64) -> u64 {
+    register_kobject(name, KobjectType::Device, parent)
+}
+
+pub fn register_root_device(name: &str) -> u64 {
+    register_kobject(name, KobjectType::Device, get_devices_ino())
+}
