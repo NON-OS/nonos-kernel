9f5a3d5efc97d00c6fcfd6e8c377d4bb87626fe1
eKisNonos <ekisanon@proton.me>
2026-04-06 15:10:53 +0200

    procfs: add /proc/[pid]/cwd symlink

---
+++ src/fs/procfs/pid/cwd.rs
@@ -0,0 +1,41 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+extern crate alloc;
+
+use alloc::string::String;
+
+pub fn read_pid_cwd(pid: i32) -> Result<String, i32> {
+    let proc = crate::process::get_process(pid).ok_or(-3)?;
+    if proc.cwd.is_empty() {
+        return Ok(String::from("/"));
+    }
+    Ok(proc.cwd.clone())
+}
+
+pub fn get_cwd_inode(pid: i32) -> Result<u64, i32> {
+    let proc = crate::process::get_process(pid).ok_or(-3)?;
+    Ok(proc.cwd_inode)
+}
+
+pub fn get_cwd_dev(pid: i32) -> Result<(u32, u32), i32> {
+    let proc = crate::process::get_process(pid).ok_or(-3)?;
+    Ok((proc.cwd_dev_major, proc.cwd_dev_minor))
+}
+
+pub fn set_pid_cwd(pid: i32, path: &str) -> Result<(), i32> {
+    crate::process::set_cwd(pid, path)
+}
