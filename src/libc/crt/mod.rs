592dd0abe2aded7b0fe5de2a5e536b0303916893
eKisNonos <ekisanon@proton.me>
2026-04-06 15:16:46 +0200

    libc: add mod

---
+++ src/libc/crt/mod.rs
@@ -0,0 +1,21 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub mod crt0;
+pub mod crti;
+
+pub use crt0::{crt0_start, setup_stack, call_main};
+pub use crti::{crti_init, crti_fini, init_array_call, fini_array_call};
