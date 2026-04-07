1ff0325deefffab4ef94bd7d65fbc506da445e57
eKisNonos <ekisanon@proton.me>
2026-04-07 18:06:26 +0200

    process: initialization routines for process subsystem

--- src/process/core/init.rs
+++ src/process/core/init.rs
@@ -14,6 +14,29 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+use super::table::types::{PROCESS_TABLE, CURRENT_PID};
+use super::types::{ProcessState, Priority};
+use super::table::create_process;
+use core::sync::atomic::Ordering;
+
 pub(crate) fn init_system_processes() {
-    crate::log::info!("[PROCESS] System processes initialized");
+    match create_process("init", ProcessState::Ready, Priority::Normal) {
+        Ok(pid) => {
+            CURRENT_PID.store(pid, Ordering::SeqCst);
+            crate::log::info!("[PROCESS] Init process created with PID {}", pid);
+        }
+        Err(e) => crate::log::error!("[PROCESS] Failed to create init: {}", e),
+    }
+    let _ = create_process("kthreadd", ProcessState::Ready, Priority::High);
+    let _ = create_process("ksoftirqd", ProcessState::Ready, Priority::High);
+}
+
+pub fn get_init_process() -> Option<alloc::sync::Arc<super::pcb::ProcessControlBlock>> {
+    PROCESS_TABLE.find_by_pid(1)
+}
+
+pub fn reparent_orphans(dead_pid: u32) {
+    for child in PROCESS_TABLE.get_children_of(dead_pid) {
+        child.ppid.store(1, Ordering::Release);
+    }
 }
