427407eb8bda86073e931d2680f760b2c10c8a49
eKisNonos <ekisanon@proton.me>
2026-04-08 19:03:04 +0200

    syscall/capsule: implement load exec kill handlers

---
+++ src/syscall/capsule/load.rs
@@ -0,0 +1,65 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::capsule::{self, UnlockToken, CapsuleId};
+
+pub const SYS_CAPSULE_LOAD: usize = 500;
+pub const SYS_CAPSULE_EXEC: usize = 501;
+pub const SYS_CAPSULE_KILL: usize = 502;
+
+pub fn sys_capsule_load(data_ptr: usize, data_len: usize, token_ptr: usize) -> i64 {
+    let data = match crate::usercopy::copy_from_user(data_ptr, data_len) {
+        Ok(d) => d,
+        Err(_) => return -1,
+    };
+    let token_bytes = match crate::usercopy::copy_from_user_fixed::<113>(token_ptr) {
+        Ok(t) => t,
+        Err(_) => return -1,
+    };
+    let token = parse_token(&token_bytes);
+    match capsule::load(&data, token) {
+        Ok(id) => id as i64,
+        Err(_) => -1,
+    }
+}
+
+pub fn sys_capsule_exec(id: CapsuleId, data_ptr: usize, data_len: usize) -> i64 {
+    let data = match crate::usercopy::copy_from_user(data_ptr, data_len) {
+        Ok(d) => d,
+        Err(_) => return -1,
+    };
+    match capsule::execute(id, &data) {
+        Ok(pid) => pid as i64,
+        Err(_) => -1,
+    }
+}
+
+pub fn sys_capsule_kill(id: CapsuleId) -> i64 {
+    capsule::registry::remove(id);
+    0
+}
+
+fn parse_token(data: &[u8; 113]) -> UnlockToken {
+    let mut token = [0u8; 32];
+    let mut capsule_id = [0u8; 32];
+    let mut manifest_hash = [0u8; 32];
+    token.copy_from_slice(&data[0..32]);
+    capsule_id.copy_from_slice(&data[32..64]);
+    manifest_hash.copy_from_slice(&data[64..96]);
+    let approved_caps = u64::from_le_bytes(data[96..104].try_into().unwrap());
+    let expires_at = u64::from_le_bytes(data[104..112].try_into().unwrap());
+    UnlockToken { token, capsule_id, manifest_hash, approved_caps, expires_at }
+}
