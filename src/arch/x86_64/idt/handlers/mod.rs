a6c39393fde01941545fe04f91c9f4dac714f491
eKisNonos <ekisanon@proton.me>
2026-04-08 15:57:45 +0200

    idt: remove dead isr.rs aggregator file

--- src/arch/x86_64/idt/handlers/mod.rs
+++ src/arch/x86_64/idt/handlers/mod.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-mod isr;
 mod isr_exceptions;
 mod isr_irqs;
 mod common;
