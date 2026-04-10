4256b14c563a1fa16266ace1d464e17818de3509
eKisNonos <ekisanon@proton.me>
2026-04-10 15:51:22 +0200

    browser/js: export microtask struct for event loop

--- src/apps/ecosystem/browser/js/event_loop/mod.rs
+++ src/apps/ecosystem/browser/js/event_loop/mod.rs
@@ -1,10 +1,10 @@
-mod microtask;
+pub mod microtask;
 mod timers;
 mod tick;
 
 #[cfg(test)]
 mod tests;
 
-pub use microtask::MicrotaskQueue;
+pub use microtask::{MicrotaskQueue, Microtask};
 pub use timers::{TimerStore, TimerId};
 pub use tick::event_loop_tick;
