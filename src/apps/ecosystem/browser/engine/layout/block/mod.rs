0e62515fe3a810ec19d92049348b6a2c9280be36
eKisNonos <ekisanon@proton.me>
2026-04-10 15:50:02 +0200

    browser/layout: export collapse_margins for block layout

--- src/apps/ecosystem/browser/engine/layout/block/mod.rs
+++ src/apps/ecosystem/browser/engine/layout/block/mod.rs
@@ -1,6 +1,9 @@
 mod layout;
-mod width;
-mod height;
-mod margin_collapse;
+pub mod width;
+pub mod height;
+pub mod margin_collapse;
 
 pub use layout::layout_block;
+pub use width::calculate_block_width;
+pub use height::calculate_block_height;
+pub use margin_collapse::collapse_margins;
