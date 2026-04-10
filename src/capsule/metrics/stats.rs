00a771efd19e156b2c26acd6723da349726f9f8c
eKisNonos <ekisanon@proton.me>
2026-04-10 16:00:39 +0200

    capsule: update metrics stats module

--- src/capsule/metrics/stats.rs
+++ src/capsule/metrics/stats.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::capsule::CapsuleId;
-
 #[derive(Debug, Clone, Copy, Default)]
 pub struct CapsuleStats {
     pub cpu_ns: u64,
