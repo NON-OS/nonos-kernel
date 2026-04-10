f76068cb20600f4af4245fa613d9ac3db6a57cc8
eKisNonos <ekisanon@proton.me>
2026-04-10 15:49:15 +0200

    browser/css: export matches_attribute for attribute selector matching

--- src/apps/ecosystem/browser/engine/css/selector/mod.rs
+++ src/apps/ecosystem/browser/engine/css/selector/mod.rs
@@ -1,8 +1,9 @@
 mod types;
 mod specificity;
 pub mod match_node;
-mod match_attr;
+pub mod match_attr;
 
-pub use types::Selector;
+pub use types::{Selector, SimpleSelector, AttributeSelector, AttributeOp};
 pub use specificity::Specificity;
 pub use match_node::matches_selector;
+pub use match_attr::matches_attribute;
