f604255f848a41e997e6931b946338d983badd06
eKisNonos <ekisanon@proton.me>
2026-04-10 15:51:20 +0200

    browser/js: export prototype populate functions

--- src/apps/ecosystem/browser/js/prototype/mod.rs
+++ src/apps/ecosystem/browser/js/prototype/mod.rs
@@ -1,9 +1,9 @@
 pub mod chain;
 mod create;
 pub mod builtin_protos;
-mod string_proto;
-mod array_proto;
-mod number_proto;
+pub mod string_proto;
+pub mod array_proto;
+pub mod number_proto;
 
 #[cfg(test)]
 mod tests;
@@ -11,3 +11,6 @@ mod tests;
 pub use chain::{ProtoChain, ProtoObject};
 pub use create::{create_with_proto, object_create};
 pub use builtin_protos::BuiltinPrototypes;
+pub use string_proto::populate as populate_string_proto;
+pub use array_proto::populate as populate_array_proto;
+pub use number_proto::populate as populate_number_proto;
