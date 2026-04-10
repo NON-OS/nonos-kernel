74973cabcfa90f892836fa1dac261fa421afa5ea
eKisNonos <ekisanon@proton.me>
2026-04-10 15:48:28 +0200

    browser/http2: export hpack encoder and decoder modules

--- src/apps/ecosystem/browser/navigate/http2/hpack/mod.rs
+++ src/apps/ecosystem/browser/navigate/http2/hpack/mod.rs
@@ -3,7 +3,7 @@ mod dynamic_table;
 mod encode;
 mod decode;
 
-pub use static_table::lookup_static;
+pub use static_table::{lookup_static, find_static, find_static_name};
 pub use dynamic_table::DynamicTable;
 pub use encode::encode_headers;
 pub use decode::decode_headers;
