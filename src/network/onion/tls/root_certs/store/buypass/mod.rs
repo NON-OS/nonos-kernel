d4d2d43e3a999b9d491472f57c6e88d099248bc4
senseix21 <mahdix2118@protonmail.com>
2026-04-11 12:35:20 +0600

    Phase 3: add Buypass root CAs (2 CAs)

---
+++ src/network/onion/tls/root_certs/store/buypass/mod.rs
@@ -0,0 +1,9 @@
+mod buypass_class_2_root_ca;
+mod buypass_class_3_root_ca;
+
+use super::super::types::TrustedRootCa;
+
+pub static BUYPASS_ROOTS: &[TrustedRootCa] = &[
+    buypass_class_2_root_ca::ROOT,
+    buypass_class_3_root_ca::ROOT,
+];
