2d078aea5ed9e43edd0f427c3fd0e52bf9918551
eKisNonos <ekisanon@proton.me>
2026-04-25 15:48:47 +0200

    devices: add dual GPU detection and safe exit mode
    Detect optimus laptops by counting GOP handles, triggers safe
    exit for machines with ExitBootServices issues. Split device
    enumeration into storage, network, graphics, PCI modules.

---
+++ nonos-bootloader/src/hardware/devices/mod.rs
@@ -0,0 +1,23 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+mod enumerate;
+mod gpu;
+mod safe_exit;
+
+pub use enumerate::{enumerate_graphics, enumerate_network, enumerate_pci, enumerate_storage};
+pub use gpu::detect_dual_gpu;
+pub use safe_exit::needs_safe_exit;
---
+++ nonos-bootloader/src/hardware/devices/safe_exit.rs
@@ -0,0 +1,22 @@
+// NØNOS Operating System
+// Copyright (C) 2026 NØNOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use uefi::prelude::*;
+use super::gpu::detect_dual_gpu;
+
+pub fn needs_safe_exit(system_table: &mut SystemTable<Boot>) -> bool {
+    detect_dual_gpu(system_table)
+}
