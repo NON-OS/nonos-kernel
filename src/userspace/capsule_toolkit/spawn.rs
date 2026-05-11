e7619cb9f9b563acdfb7a43c7728f51a406b31de
senseix21 <mahdix2118@protonmail.com>
2026-05-11 14:58:32 +0600

    fix(userspace): add toolkit spawn glue

---
+++ src/userspace/capsule_toolkit/spawn.rs
@@ -0,0 +1,45 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use super::embed::TOOLKIT_ELF;
+use crate::capabilities::Capability;
+use crate::kernel_core::process_spawn::capsule_spawn::{self, CapsuleSpec, SpawnError};
+
+const SERVICE_NAME: &str = "toolkit";
+const SERVICE_PORT: u32 = 4610;
+const REPLY_INBOX: &str = "endpoint.toolkit.reply";
+const REPLY_PORT: u32 = 4611;
+
+pub fn spawn_toolkit_capsule() -> Result<(), SpawnError> {
+    if TOOLKIT_ELF.is_empty() {
+        return Err(SpawnError::FeatureDisabled);
+    }
+    let mut caps_bits = 0u64;
+    for cap in [Capability::CoreExec, Capability::Memory, Capability::Debug, Capability::IPC] {
+        caps_bits |= cap.bit();
+    }
+    let spec = CapsuleSpec {
+        name: SERVICE_NAME,
+        service_port: SERVICE_PORT,
+        reply_inbox: REPLY_INBOX,
+        reply_port: REPLY_PORT,
+        elf: TOOLKIT_ELF,
+        caps_bits,
+        debug_tag: b"[TOOLKIT-DEBUG] load_elf_executable error:",
+    };
+    let _ = capsule_spawn::spawn(&spec)?;
+    Ok(())
+}
