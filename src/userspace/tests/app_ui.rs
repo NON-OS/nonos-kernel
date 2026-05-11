041dec421b7a4fa630650ea571a02f7d1f0cc69e
senseix21 <mahdix2118@protonmail.com>
2026-05-11 15:19:51 +0600

    fix(tests): add app-ui exit cleanup regressions

---
+++ src/userspace/tests/app_ui.rs
@@ -0,0 +1,36 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::test::TestResult;
+
+const ABOUT_APP_SRC: &str = include_str!("../../../userland/capsule_about/src/main.rs");
+
+pub(crate) fn test_about_app_exit_cleanup_markers() -> TestResult {
+    if !ABOUT_APP_SRC.contains("mk_exit(0)") {
+        return Err("about app must call mk_exit(0) on parked IPC path");
+    }
+    if !ABOUT_APP_SRC.contains("ipc parked") {
+        return Err("about app must emit ipc parked marker before exit");
+    }
+    Ok(())
+}
+
+pub(crate) fn test_about_app_no_global_mut_state() -> TestResult {
+    if ABOUT_APP_SRC.contains("static mut") {
+        return Err("about app must not keep global mutable state");
+    }
+    Ok(())
+}
