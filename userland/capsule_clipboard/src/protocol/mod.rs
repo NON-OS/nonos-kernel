b14dde1e9947b419660c5f39def68fffd4e3913d
senseix21 <mahdix2118@protonmail.com>
2026-05-17 15:34:29 +0600

    clipboard: export distinct parse errnos — protocol/mod.rs [1.1]

--- userland/capsule_clipboard/src/protocol/mod.rs
+++ userland/capsule_clipboard/src/protocol/mod.rs
@@ -7,7 +7,7 @@ mod ops;
 
 pub use decode::parse;
 pub use encode::{response_header, write_status};
-pub use errno::{E_BAD_OP, E_INVAL, E_RANGE};
+pub use errno::{E_BAD_LEN, E_BAD_MAGIC, E_BAD_OP, E_BAD_VERSION, E_INVAL, E_RANGE};
 pub use header::{Request, HDR_LEN, MAGIC, VERSION};
 pub use limits::{IPC_PAYLOAD_MAX, MAX_DEPTH, MAX_ENTRY_BYTES, MAX_TOTAL_BYTES, STATUS_LEN};
 pub use ops::{OP_CLEAR, OP_COPY, OP_HEALTHCHECK, OP_HISTORY_GET, OP_HISTORY_LIST, OP_PASTE};
