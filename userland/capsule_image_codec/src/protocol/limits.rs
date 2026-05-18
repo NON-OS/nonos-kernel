e355c677379248dcf44099ddae9101831c4b658a
senseix21 <mahdix2118@protonmail.com>
2026-05-18 14:59:53 +0600

    image_codec: import capsule crate (eK base + 1.1 typed-errno) — userland/capsule_image_codec/src/protocol/limits.rs [1.3]

---
+++ userland/capsule_image_codec/src/protocol/limits.rs
@@ -0,0 +1,20 @@
+// NONOS Operating System
+// Copyright (C) 2026 NONOS Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+pub const IPC_PAYLOAD_MAX: usize = 131072;
+pub const STATUS_LEN: usize = 4;
+pub const DECODE_LZ4_PREFIX_LEN: usize = 8;
+pub const DECODE_RESP_LEN: usize = 32;
